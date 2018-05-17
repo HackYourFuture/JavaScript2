f9f3621ec1b96f3689a220d0f2e9e662290992e9
Husam Lahm <37015049+ThreePalmTrees@users.noreply.github.com>
2018-05-17 21:44:15 +0200

    added comments

--- Week2/homework/map_filter.js
+++ Week2/homework/map_filter.js
@@ -3,6 +3,6 @@
 const numbers = [1, 2, 3, 4];
 
 let doubledOddNumbers = numbers
-  .filter(number => number % 2 === 1)
-  .map(oddNumber => oddNumber * 2);
+  .filter(number => number % 2 === 1) // filtering and keeping only the odd numbers
+  .map(oddNumber => oddNumber * 2); // multiplying the filtered numbers (odd) by two
 console.log(doubledOddNumbers);
