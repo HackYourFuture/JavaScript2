080c26fc5155d1a6b6845c3d49688db3cbed11c5
AllouhM <mahmoud.allouh0@gmail.com>
2018-05-17 23:16:27 +0200

    Add homework week2

--- Week2/homework/maartjes_work.js
+++ Week2/homework/maartjes_work.js
@@ -43,5 +43,18 @@ const tuesday = [
 ];
 
 const tasks = monday.concat(tuesday);
+// console.log(tasks);
+
 
 // Add your code here
+
+const perHour = 20;
+const totalEarned = tasks.filter(twoHoursOrMore => twoHoursOrMore.duration >= 120).reduce((total, task) =>
+  total + (task.duration / 60 * perHour), 0);
+
+// way 2, here I started with map.
+// const perHour = 20;
+// const totalEarned = tasks.map(toHours => toHours.duration / 60).filter(twoHoursOrMore => twoHoursOrMore >= 2)
+//   .reduce((total, task) => total + (task * perHour), 0);
+
+console.log('Maartje should receive for the performed tasks' + ' ' + '€ ' + totalEarned.toFixed(2));
--- Week2/homework/map_filter.js
+++ Week2/homework/map_filter.js
@@ -1,5 +1,11 @@
 'use strict';
 
-const numbers = [1, 2, 3, 4];
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; // I added some more numbers for testing.
 
 // Add your code here
+
+
+const doubledOddNumbers = numbers.filter(number => number % 2 !== 0).map(number => number * 2);
+
+console.log(doubledOddNumbers);
+
