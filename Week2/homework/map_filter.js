19acaa5c075196fd023d34fd4757c0dc809e9a99
threepalmtrees <chrosamofficial@gmail.com>
2018-05-17 21:40:16 +0200

    added homework week2

--- Week2/homework/map_filter.js
+++ Week2/homework/map_filter.js
@@ -1,5 +1,8 @@
-'use strict';
+"use strict";
 
 const numbers = [1, 2, 3, 4];
 
-// Add your code here
+let doubledOddNumbers = numbers
+  .filter(number => number % 2 === 1)
+  .map(oddNumber => oddNumber * 2);
+console.log(doubledOddNumbers);
