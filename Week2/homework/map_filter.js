87a90b1a42c700a3f1558ac99574dbdbf4f97505
AllouhM <mahmoud.allouh0@gmail.com>
2018-05-18 02:07:05 +0200

    delet homework2_week2

--- Week2/homework/map_filter.js
+++ Week2/homework/map_filter.js
@@ -1,11 +1,7 @@
 'use strict';
 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; // I added some more numbers for testing.
+const numbers = [1, 2, 3, 4];
 
 // Add your code here
 
-
-const doubledOddNumbers = numbers.filter(number => number % 2 !== 0).map(number => number * 2);
-
-console.log(doubledOddNumbers);
 
