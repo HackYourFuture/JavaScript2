9ca56759a6678f08e5cae5379d1715145f8ee5b7
neveenatik <nevine.atike@gmail.com>
2018-05-20 21:18:37 +0200

    Corrected homework based on Hardith's feedback

--- Week1/homework/app.js
+++ Week1/homework/app.js
@@ -1,115 +1,108 @@
 'use strict';
-{
+function main() {
+
   const books = {
-    book1: {
-      key: 'forty_rules_of_love',
+    forty_rules_of_love: {
       bookTitle: 'Forty rules of love',
       language: 'Turkish',
-      author: 'Elif Shabak'
+      author: 'Elif Shabak',
     },
-    book2: {
-      key: 'azazel',
+    azazel: {
       bookTitle: 'Azazel',
       language: 'Arabic',
-      author: 'Youssef Zeidan'
+      author: 'Youssef Zeidan',
 
     },
-    book3: {
-      key: 'alchemist',
+    alchemist: {
       bookTitle: 'The Alchemist',
       language: 'Portuguese',
-      author: 'Paulo Coelho'
+      author: 'Paulo Coelho',
     },
-    book4: {
-      key: 'love_time_of_cholera',
+    love_time_of_cholera: {
       bookTitle: 'Love in the time of cholera',
       language: 'Spanish',
-      author: 'Gabriel García Márquez'
+      author: 'Gabriel García Márquez',
     },
-    book5: {
-      key: 'aleph',
+    aleph: {
       bookTitle: 'Aleph',
       language: 'Potuguese',
-      author: 'Paulo Coelho'
+      author: 'Paulo Coelho',
     },
-    book6: {
-      key: 'flesh_memory',
+    flesh_memory: {
       bookTitle: 'Memory in the Flesh',
       language: 'Arabic',
-      author: 'Ahlam Mosteghanemi'
+      author: 'Ahlam Mosteghanemi',
     },
-    book7: {
-      key: 'hundred_years_of_solitude',
+    hundred_years_of_solitude: {
       bookTitle: 'One hundred years of solitude',
       language: 'Spanish',
-      author: 'Gabriel García Márquez'
+      author: 'Gabriel García Márquez',
     },
-    book8: {
-      key: 'manuscript_found_in_accra',
+    manuscript_found_in_accra: {
       bookTitle: 'Manuscript found in Accra',
       language: 'Portuguese',
-      author: 'Paulo Coelho'
+      author: 'Paulo Coelho',
     },
-    book9: {
-      key: 'brida',
+    brida: {
       bookTitle: 'Brida',
       language: 'Portuguese',
-      author: 'Paulo Coelho'
+      author: 'Paulo Coelho',
     },
-    book10: {
-      key: 'religion_history',
+    religion_history: {
       bookTitle: 'Religion history and philosophy',
       language: 'Arabic',
-      author: 'Taha al-Hashimi'
-    }
+      author: 'Taha al-Hashimi',
+    },
   };
 
-  function pageContentCreator(parentObject) {
+  let bookImages = {
+    forty_rules_of_love: 'images\\forty_rules_of_love.jpg',
+    azazel: 'images\\azazel.jpg',
+    alchemist: 'images\\alchemist.jpg',
+    love_time_of_cholera: 'images\\love_time_of_cholera.jpg',
+    aleph: 'images\\aleph.jpg',
+    flesh_memory: 'images\\flesh_memory.jpg',
+    hundred_years_of_solitude: 'images\\hundred_years_of_solitude.jpg',
+    manuscript_found_in_accra: 'images\\manuscript_found_in_accra.jpg',
+    brida: 'images\\brida.jpg',
+    religion_history: 'images\\religion_history.jpg',
+  };
 
-    const pageHeader = document.createElement('h1');
-    pageHeader.innerHTML = '10 books I have read!';
-    pageHeader.style.textAlign = 'center';
-    const content = document.getElementById('book-list');
-    content.appendChild(pageHeader);
+  const pageHeader = document.createElement('h1');
+  pageHeader.innerHTML = '10 books I have read!';
+  const content = document.getElementById('book-list');
+  content.appendChild(pageHeader);
 
-    const divWrapper = document.createElement('div');
-    divWrapper.setAttribute('id', 'div-wrapper');
-    content.appendChild(divWrapper);
+  const contentDiv = document.createElement('div');
+  contentDiv.setAttribute('class', 'div-wrapper');
+  content.appendChild(contentDiv);
 
-    for (const childObject in parentObject) {
-      const wrapper = document.createElement('div');
-      wrapper.setAttribute('id', parentObject[childObject].key);
-      wrapper.setAttribute('class', 'wrapper');
-      divWrapper.appendChild(wrapper);
+  for (const key in books) {
+    const wrapper = document.createElement('div');
+    wrapper.setAttribute('id', key);
+    wrapper.setAttribute('class', 'wrapper');
+    contentDiv.appendChild(wrapper);
 
-      const header = document.createElement('h2');
-      header.innerHTML = parentObject[childObject]['bookTitle'];
-      wrapper.appendChild(header);
+    const header = document.createElement('h2');
+    header.innerHTML = books[key]['bookTitle'];
+    wrapper.appendChild(header);
 
-      const list = document.createElement('ul');
-      for (let i = 2; i < Object.keys(parentObject[childObject]).length; i++) {
-        list.innerHTML += '<li> ' + Object.keys(parentObject[childObject])[i] + ' :\t' + Object.values(parentObject[childObject])[i] + '</li>';
-        list.style.listStyle = 'none';
-        wrapper.appendChild(list);
-      }
-
-      const image = document.createElement('img');
-      image.src = './images/' + parentObject[childObject].key + '.jpg';
-      image.width = 175;
-      image.setAttribute('alt', 'book cover');
-      wrapper.appendChild(image);
+    const list = document.createElement('ul');
+    for (let i = 1; i < Object.keys(books[key]).length; i++) {
+      list.innerHTML += '<li> ' + Object.keys(books[key])[i] + ' :\t' + Object.values(books[key])[i] + '</li>';
+      wrapper.appendChild(list);
     }
 
+    const image = document.createElement('img');
+    image.src = getSource(key, bookImages);
+    image.setAttribute('alt', 'book cover');
+    wrapper.appendChild(image);
   }
 
-  window.addEventListener('load', pageContentCreator(books));
-
-  function ImageIdEncapsulation() {
-    for (const object in books) {
-      this[Object.values(books[object])[0]] = './images/' + books[object][0] + '.jpg';
-    }
-  }
-
-  const objectBookIdsAndImagePaths = new ImageIdEncapsulation();
-  console.log(Object.keys(objectBookIdsAndImagePaths), Object.values(objectBookIdsAndImagePaths));
 }
+
+function getSource(bookId, bookImages) {
+  return bookImages[bookId]
+}
+
+window.addEventListener('load', main());
