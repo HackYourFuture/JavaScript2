dafba100577f1ad139665241e19fe15a5d47aa76
amirno1 <amir.noshir@gmail.com>
2018-05-27 16:38:39 +0200

    homework2 added

--- Week2/homework/maartjes_work.js
+++ Week2/homework/maartjes_work.js
@@ -44,4 +44,12 @@ const tuesday = [
 
 const tasks = monday.concat(tuesday);
 
-// Add your code here
+
+const salary = tasks.map(x => x.duration / 60)
+  .filter(x => x >= 2)
+  .map(x => x * 10)
+  .reduce((acc, val) => acc + val).toFixed(2);
+
+//lets say she earns 10 euro per hour
+
+console.log('Maartje earns ', salary, '€');
