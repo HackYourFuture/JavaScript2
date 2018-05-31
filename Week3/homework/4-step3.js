dc77e57dca772b063100fb319d98cfb05edd381d
Knar <qenpoghos@gmail.com>
2018-05-31 13:07:03 +0100

    Added more freeCodeCamp solutions to the 4-step3.js file

--- Week3/homework/4-step3.js
+++ Week3/homework/4-step3.js
@@ -35,4 +35,39 @@ const carInst3 = function (wheels, seats, engines) {
 const myCarInst3 = new carInst3(3, 1, 2);
 console.log(myCarInst3);
 
+//Instruction 4 ->Verify an Object's Constructor with instanceof
+
+myCarInst3 instanceof carInst3;
+
+// Instruction 5 -> Add the own properties of myCarInst3 to the array ownProps, without using Object.keys() method
+
+const ownProps = [];
+
+for (const prop in myCarInst3) {
+    if (myCarInst3.hasOwnProperty(prop)) {
+        ownProps.push(prop);
+    }
+}
+
+console.log(ownProps);
+
+// Instruction 6 ->Use Prototype Properties to Reduce Duplicate Code
+
+carInst3.prototype.type = 'Electric';
+
+// Instruction 7 -> Iterate Over All Properties
+
+const ownProperties = [];
+const prototypeProps = [];
+
+for (const prop in myCarInst3) {
+    if (myCarInst3.hasOwnProperty(prop)) {
+        ownProperties.push(prop);
+    } else {
+        prototypeProps.push(prop);
+    }
+}
+
+console.log(ownProperties);
+console.log(prototypeProps);
 
