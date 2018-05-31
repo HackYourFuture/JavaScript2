4143fe6c04f3265ecfd299a5503788dd99cc211d
Wael-Alhomsi <waelhom2@gmail.com>
2018-05-31 21:22:58 +0200

    added homework-week3

--- Week3/homework/4-step3.js
+++ Week3/homework/4-step3.js
@@ -1,4 +1,17 @@
 'use strict';
 // paste your freeCodeCamp solutions in here
 
-//Sorry but for some reason I could not retrieve the freecodecamp web page for this step. 
+// This is an alternative to the original exercise. The original one exists no more on freecodecamp.
+
+// The new one is suggested by Jim. The link is: https://learn.freecodecamp.org/javascript-algorithms-and-data-structures/object-oriented-programming/use-a-constructor-to-create-objects
+
+function Dog() {
+    this.name = "Rupert";
+    this.color = "brown";
+    this.numLegs = 4;
+}
+// Add your code below this line
+const hound = new Dog();
+
+console.log(hound);
+
