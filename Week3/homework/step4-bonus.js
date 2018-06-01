01754ea14009f9b0c10d759894fa1d886a8f0b50
Wael-Alhomsi <waelhom2@gmail.com>
2018-06-01 02:01:32 +0200

    added homework-week3 part2

--- Week3/homework/step4-bonus.js
+++ Week3/homework/step4-bonus.js
@@ -3,8 +3,17 @@
 const values = ['a', 'b', 'c', 'd', 'a', 'e', 'f', 'c'];
 
 // Add your function here. Try and come up with a good name for this function
+function removeDuplicates(arr) {
+    const modifiedArray = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (modifiedArray.includes(arr[i]) === false) {
+            modifiedArray.push(arr[i]);
+        }
+    }
+    return modifiedArray;
+}
 
 // Replace `yourFunction` with the name of the function you just created
-const uniqueValues = yourFunction(values);
+const uniqueValues = removeDuplicates(values);
 
 console.log(uniqueValues);
