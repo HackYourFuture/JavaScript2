5dcaeddaec55018490d36750a2a2983429a8c893
zmagzoub <zaid.majzoub1@gmail.com>
2018-06-01 21:44:01 +0200

    added homework week3

--- Week3/homework/3-step3.js
+++ Week3/homework/3-step3.js
@@ -2,24 +2,38 @@
 
 // use a 'for' loop
 function repeatStringNumTimesWithFor(str, num) {
-  // add your code here
-  return str;
+    // add your code here
+    let repeatedString = '';
+    for (let i = num; i > 0; i--) {
+        repeatedString += str;
+    }
+    return repeatedString;
 }
 
 console.log('for', repeatStringNumTimesWithFor('abc', 3));
 
 // use a 'while' loop
 function repeatStringNumTimesWithWhile(str, num) {
-  // add your code here
-  return str;
+    // add your code here
+    let repeatedString = '';
+    while (num > 0) {
+        repeatedString += str;
+        num--;
+    }
+    return repeatedString;
 }
 
 console.log('while', repeatStringNumTimesWithWhile('abc', 3));
 
 // use a 'do...while' loop
 function repeatStringNumTimesWithDoWhile(str, num) {
-  // add your code here
-  return str;
+    // add your code here
+    let repeatedString = '';
+    do {
+        repeatedString += str;
+        num--;
+    } while (num > 0)
+    return repeatedString;
 }
 
 console.log('while', repeatStringNumTimesWithDoWhile('abc', 3));
--- Week3/homework/6-step3.js
+++ Week3/homework/6-step3.js
@@ -4,3 +4,14 @@ const arr2d = [[1, 2], [3, 4], [5, 6]];
 const arr3d = [[[1, 2], [3, 4]], [[5, 6], [7, 8]]];
 
 // add your solution here, or add a comment on how you would tackle this problem
+// this solution is only works for arr3d
+function printArray(a) {
+    for (var i = 0; i < a.length; i++) {
+        for (var z = 0; z < a[i].length; z++) {
+            for (var y = 0; y < a[i][z].length; y++)
+                console.log(a[i][z][y]);
+        }
+    }
+}
+
+printArray(arr3d)
