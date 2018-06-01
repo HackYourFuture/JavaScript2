f9db166bce3967c3fa481b90765011c3eb2576b1
neveenatik <nevine.atike@gmail.com>
2018-06-01 02:29:45 +0200

    Added homework week3

--- Week3/homework/2-step3.js
+++ Week3/homework/2-step3.js
@@ -3,6 +3,25 @@
 function threeFive(startIndex, stopIndex, threeCallback, fiveCallback) {
   const values = [];
   // Add your code here
+
+  for (startIndex; startIndex <= stopIndex; startIndex++) {
+    values.push(startIndex);
+  }
+  console.log('Array created is: [' + values + ']');
+  for (let i = 0; i < values.length; i++) {
+    if (values[i] % 3 === 0) {
+      threeCallback(values[i]);
+    }
+    if (values[i] % 5 === 0) {
+      fiveCallback(values[i]);
+    }
+  }
+}
+function sayThree(num) {
+  console.log(num + ' is divisible by three');
+}
+function sayFive(num) {
+  console.log(num + ' is divisible by five');
 }
 
 threeFive(10, 15, sayThree, sayFive);
--- Week3/homework/7-step3.js
+++ Week3/homework/7-step3.js
@@ -1,25 +1,26 @@
-'use strict';
-
-const x = 9;
+const x = 9; // x is assigned a primitive value
 function f1(val) {
   val = val + 1;
   return val;
 }
+f1(x); // a copy of the variable x is passed into the function and processed
+console.log(x); // we are logging the "original" value of x
 
-f1(x);
-
-console.log(x);
-
-
-const y = { x: 9 };
+const y = { x: 9 }; // y is assigned an object
 function f2(val) {
   val.x = val.x + 1;
   return val;
 }
-
-f2(y);
-
-console.log(y);
+f2(y); // a reference to the object y is passed into the function and processed
+console.log(y); // the reference to y is logged to console
 
 // Add your explanation as a comment here
+/*
+In sum, ALL objects interact by reference in Javascript 
+so when passing to a function they all point to the same location 
+so when you change one object you change them all. 
+This is a stark difference compared to pass by value that copies the value into two separate spots in memory effectively 
+making them entirely separate entities despite one initially being set equal to the other.
+https://codeburst.io/javascript-passing-by-value-vs-reference-explained-in-plain-english-8d00fd06a47c
+*/
 
