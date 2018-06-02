7eccb63b284beea6079175f02c8823114caa90d4
Knar <qenpoghos@gmail.com>
2018-06-02 17:00:11 +0100

    edited 6-steps3.js

--- Week3/homework/6-step3.js
+++ Week3/homework/6-step3.js
@@ -5,10 +5,12 @@ const arr3d = [[[1, 2], [3, 4]], [[5, 6], [7, 8]]];
 
 // add your solution here, or add a comment on how you would tackle this problem
 
-const printAllItems = arr2d.map(function (item, index) {
-    return index + ". " + item;
-}).join('\n');
+const printAllItems = (arr) =>
+    arr.map((item, index) => {
+        return index + ". " + item;
+    }).join('\n');
 
-console.log(printAllItems);
+console.log(printAllItems(arr2d));
+console.log(printAllItems(arr3d));
 
 
