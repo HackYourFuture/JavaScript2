ccf555f540b85bc72629f3088b3e677cf5fb4ff0
mehmetsaidcoskun <39768263+mehmetsaidcoskun@users.noreply.github.com>
2018-07-18 20:56:23 +0200

    2.1 and 2.2

--- Week1/homework/app.js
+++ Week1/homework/app.js
@@ -1,11 +1,17 @@
 'use strict';
 {
   const bookTitles = [
-    // Replace with your own book titles
-    'harry_potter_chamber_secrets'
+    "crime_and_punisment",
+    "zorba",
+    "siddhartha",
+    "white_fang",
+    "poor_folk",
+    "white_nights",
+    "sorrows_yougn_werther",
+    "sweet_orange_tree",
+    "what_men_live_by",
+    "the_alchemist"
   ];
 
-
-  // Replace with your own code
-  console.log(bookTitles);
+  console.log(bookTitles)
 }
