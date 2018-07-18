fcda1ed94006ed83e95b7a234b9a6dde50e0c7f2
mehmetsaidcoskun <39768263+mehmetsaidcoskun@users.noreply.github.com>
2018-07-18 21:14:03 +0200

    2.4 new object named bookShelf

--- Week1/homework/app.js
+++ Week1/homework/app.js
@@ -28,4 +28,17 @@
   }
 
   printBookTitles("div1", bookTitles);
+
+  const bookShelf = {
+    "crime_and_punisment": { author: "dostoyevski", language: "russian" },
+    "zorba": { author: "nikos kazancakis", language: "greek" },
+    "siddhartha": { author: "hermann hesse", language: "german" },
+    "white_fang": { author: "jack london", language: "english" },
+    "poor_folk": { author: "dostoyevski", language: "russian" },
+    "white_nights": { author: "dostoyevski", language: "russian" },
+    "sorrows_yougn_werther": { author: "goethe", language: "german" },
+    "sweet_orange_tree": { author: "vasconcelos", language: "portuguese" },
+    "what_men_live_by": { author: "tolstoy", language: "russian" },
+    "the_alchemist": { author: "paulo coelho", language: "portuguese" }
+  };
 }
