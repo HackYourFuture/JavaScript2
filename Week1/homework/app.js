8bd336c175d4865d4d6c7d5dec2b23b12c5e1833
ward <vakpko@gmail.com>
2018-07-18 14:10:46 +0200

    added books and loaded app.js

--- Week1/homework/app.js
+++ Week1/homework/app.js
@@ -1,11 +1,15 @@
-'use strict';
-{
-  const bookTitles = [
-    // Replace with your own book titles
-    'harry_potter_chamber_secrets'
-  ];
+'use strict'; {
+    const bookTitles = [
+        "edgar_po_tales",
+        "charles_dickens_oliver_twist",
+        "oscar wilde_the_Picture_of_dorian_gray",
+        "alexander_duma_monte_cristo",
+        "shakespeare_hamlet",
+        "shakespeare_king_lir",
+        "ibsen_nora",
+        "friedrich_schiller_intrigue_and_love",
+        "jules_verne_search_of_the_castaways,"
+    ];
 
-
-  // Replace with your own code
-  console.log(bookTitles);
+    console.log(bookTitles);
 }
