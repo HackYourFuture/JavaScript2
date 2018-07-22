0eeef2d0a8f074e7212b289fb2dd9829d8646754
karamGhazzi <Karam-g1993@hotmail.com>
2018-07-22 03:54:00 +0200

    the last part of my home work

--- Week1/homework/app.js
+++ Week1/homework/app.js
@@ -1,11 +1,23 @@
-'use strict';
-{
-  const bookTitles = [
+const bookTitles = {
     // Replace with your own book titles
-    'harry_potter_chamber_secrets'
-  ];
+    'titles': ["eat_cake_be_brave", "cottage_by_the_sea", "the_sinners", "the_other_woman", "measure_what_matters", "liars_leakers_and_liberals", "becoming", "you_are_on_an_airplane", "believe_me", "whiskey_in_a_teacup"],
+    'languages': ["english", "spanish", "arabic", "english", "polish", "arabic", "russian", "armenian", "syrian", "hebrew"],
+    'author': ["ahmad", "abdallah", "hisham", "koko", "kiki", "roula", "nana", "sally", "stella", "nina"],
+};
+
+// Replace with your own code
+
+var list = document.createElement('section');
+list.setAttribute('id', 'bookLists');
+var ul = document.createElement('ul');
+document.body.appendChild(list);
+list.appendChild(ul);
+for (var i = 0; i < 10; i++) {
+    var li = document.createElement('li');
+    ul.appendChild(li);
+    var smth = '| title: ' + bookTitles.titles[i] + '| language: ' + bookTitles.languages[i] + '| author: ' + bookTitles.author[i]
+
+    li.innerHTML += smth
 
 
-  // Replace with your own code
-  console.log(bookTitles);
 }
