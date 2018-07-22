a6af14bde31808773038f4d060ec24c62508f0e1
karamGhazzi <Karam-g1993@hotmail.com>
2018-07-22 03:56:42 +0200

    last add

--- Week1/homework/app.js
+++ Week1/homework/app.js
@@ -1,23 +1,118 @@
-const bookTitles = {
+const bookTitles2 = [
     // Replace with your own book titles
-    'titles': ["eat_cake_be_brave", "cottage_by_the_sea", "the_sinners", "the_other_woman", "measure_what_matters", "liars_leakers_and_liberals", "becoming", "you_are_on_an_airplane", "believe_me", "whiskey_in_a_teacup"],
-    'languages': ["english", "spanish", "arabic", "english", "polish", "arabic", "russian", "armenian", "syrian", "hebrew"],
-    'author': ["ahmad", "abdallah", "hisham", "koko", "kiki", "roula", "nana", "sally", "stella", "nina"],
+    "Court of the Stone Children",
+    "Alice's Adventures in Wonderland",
+    "Through the Looking Glass",
+    'What Am I?',
+    'The City of Gold & Lead',
+    'The Pool of Fire',
+    'The White Mountains',
+    'The Dark is Rising',
+    'Charlie and the Chocolate Factory',
+    'James and the Giant Peach'
+];
+
+const bookTitles = {
+    0: {
+        title: "Court of the Stone Children",
+        language: "English",
+        author: "Cameron, Eleanor",
+
+    },
+    1: {
+        title: "Alice's Adventures in Wonderland",
+        language: "English",
+        author: "Carroll, Lewis",
+
+    },
+    2: {
+        title: "Through the Looking Glass",
+        language: "English",
+        author: "Carroll, Lewis",
+
+    },
+    3: {
+        title: "What Am I?",
+        language: "English",
+        author: "Charles, N. N.",
+
+    },
+    4: {
+        title: "The City of Gold & Lead",
+        language: "English",
+        author: "Christopher, John",
+
+    },
+    5: {
+        title: "The Pool of Fire",
+        language: "English",
+        author: "Christopher, John",
+
+    },
+    6: {
+        title: "The White Mountains",
+        language: "English",
+        author: "Christopher, John",
+
+    },
+    7: {
+        title: "The Dark is Rising",
+        language: "English",
+        author: "Cooper, Susan",
+
+    },
+    8: {
+        title: "Charlie and the Chocolate Factory",
+        language: "English",
+        author: "Dahl, Roald",
+
+    },
+    9: {
+        title: "James and the Giant Peach",
+        language: "English",
+        author: "Dahl, Roald",
+
+    }
 };
 
+
 // Replace with your own code
+function bookInfo() {
+    var list = document.createElement("section");
+    list.setAttribute("id", "bookLists");
+    var ul = document.createElement("ul");
+    document.body.appendChild(list);
+    list.appendChild(ul);
+    for (var i = 0; i < Object.keys(bookTitles).length; i++) {
+        var li = document.createElement("li");
+        var h1 = document.createElement("h1");
+        h1.setAttribute("class", "heading");
+        var p = document.createElement("p");
+        p.setAttribute("class", "language");
 
-var list = document.createElement('section');
-list.setAttribute('id', 'bookLists');
-var ul = document.createElement('ul');
-document.body.appendChild(list);
-list.appendChild(ul);
-for (var i = 0; i < 10; i++) {
-    var li = document.createElement('li');
-    ul.appendChild(li);
-    var smth = '| title: ' + bookTitles.titles[i] + '| language: ' + bookTitles.languages[i] + '| author: ' + bookTitles.author[i]
+        var p2 = document.createElement("p");
+        p2.setAttribute("class", "author");
 
-    li.innerHTML += smth
+        var img = document.createElement("img");
+        img.setAttribute("class", "imglist");
 
 
+
+
+        ul.appendChild(li);
+        li.appendChild(h1);
+        li.appendChild(img);
+        img.setAttribute("src", "booksimg/" + [i] + ".jpg");
+        li.appendChild(p);
+        li.appendChild(p2);
+
+
+        var img = "|images: " + bookTitles[i];
+        h1.innerHTML += bookTitles[i].title;
+
+        p.innerHTML += bookTitles[i].language;
+        p2.innerHTML += bookTitles[i].author;
+
+    }
 }
+bookInfo();
