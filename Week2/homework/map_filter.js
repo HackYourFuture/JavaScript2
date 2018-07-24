b493e0efb27ca83b3466013aa20d0409ef9c2949
mehmetsaidcoskun <39768263+mehmetsaidcoskun@users.noreply.github.com>
2018-07-24 17:48:37 +0200

    filtering odd nums and doubling them

--- Week2/homework/map_filter.js
+++ Week2/homework/map_filter.js
@@ -3,3 +3,15 @@
 const numbers = [1, 2, 3, 4];
 
 // Add your code here
+
+function pickAndDoubleOdds(arr) {
+
+    let myOdds = arr.filter(num => num % 2 === 1);
+    //this is for filtering odd numbers in parameter array
+
+    return myOdds.map(odd => odd *= 2);
+    //this is for doubling odd numbers previously chosen
+}
+
+console.log(pickAndDoubleOdds(numbers));
+// display the result
