9bc764c057f1d2587d3e69accc99d0601fc49d5e
Aɴᴅʀᴇᴡ <acimanx@gmail.com>
2018-07-25 17:22:06 +0200

    Upgraded

--- Week2/homework/map_filter.js
+++ Week2/homework/map_filter.js
@@ -1,5 +1,5 @@
 'use strict';
 
-const numbers = [1, 2, 3, 4];
-
-// Add your code here
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const newNumbers = numbers.filter(evenGoAway => 0 != evenGoAway % 2).map(multiplyIt => 2 * multiplyIt);
+console.log("The doubled numbers are:", newNumbers);
