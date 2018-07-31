09f5614d0f25ec07eac456f6ef2098439554dc4a
mehmetsaidcoskun <39768263+mehmetsaidcoskun@users.noreply.github.com>
2018-07-31 21:09:46 +0200

    listUniqueOnes

--- Week3/homework/step4-bonus.js
+++ Week3/homework/step4-bonus.js
@@ -1,10 +1,31 @@
 'use strict';
 
-const values = ['a', 'b', 'c', 'd', 'a', 'e', 'f', 'c'];
+const values = ['a', 'b', 'c', 'd', 'a', 'e', 'f', 'c', []];
 
-// Add your function here. Try and come up with a good name for this function
+// Add your function here. Try and come up with a good name 
+//for this function
 
-// Replace `yourFunction` with the name of the function you just created
-const uniqueValues = yourFunction(values);
+function listUniqueOnes(arr) {
+    arr.sort();
+
+    let myUniques = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (typeof arr[i] != "string" || "number") {
+            return "This array contains non-string or non-number element!"
+        };
+
+        if (arr[i] !== arr[i + 1]) {
+            myUniques.push(arr[i]);
+        }
+    }
+
+    return myUniques;
+}
+
+// Replace `yourFunction` with the name of the function you 
+//just created
+
+const uniqueValues = listUniqueOnes(values);
 
 console.log(uniqueValues);
