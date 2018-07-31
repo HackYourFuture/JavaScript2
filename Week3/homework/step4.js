d28e9ad57a0c522e9091d0f29088c91ab0574003
mehmetsaidcoskun <39768263+mehmetsaidcoskun@users.noreply.github.com>
2018-07-31 20:47:35 +0200

    closure example-createBase

--- Week3/homework/step4.js
+++ Week3/homework/step4.js
@@ -2,7 +2,14 @@
 
 // Add your code here
 
+//This is created by me!
+const createBase = (x) => { return (y) => { return x + y } };
+
 const addSix = createBase(6);
 
 addSix(10); // returns 16
 addSix(21); // returns 27
+
+//I added logs in order to check whether the function runs or not.
+console.log(addSix(10));
+console.log(addSix(21));
