18e5ac780bdeec8337f3ff6c43fab9f3f3185044
mehmetsaidcoskun <39768263+mehmetsaidcoskun@users.noreply.github.com>
2018-07-31 17:47:52 +0200

    pass by value or reference

--- Week3/homework/7-step3.js
+++ Week3/homework/7-step3.js
@@ -1,6 +1,11 @@
 'use strict';
 
+console.log("pass by value!")
+
 const x = 9;
+
+console.log("before running function, x = " + x);
+
 function f1(val) {
   val = val + 1;
   return val;
@@ -8,10 +13,17 @@ function f1(val) {
 
 f1(x);
 
-console.log(x);
+console.log("after running function, still x = " + x);
 
+
+//now you will see the difference
+
+console.log("\npass by reference!")
 
 const y = { x: 9 };
+
+console.log("before running function, y.x = " + y.x)
+
 function f2(val) {
   val.x = val.x + 1;
   return val;
@@ -19,7 +31,14 @@ function f2(val) {
 
 f2(y);
 
-console.log(y);
+console.log("after running function, y.x = " + y.x);
 
 // Add your explanation as a comment here
 
+//In Pass by Value
+//Changing the argument inside the function DOESN'T 
+//affect the variable passed from outside the function.
+
+//In Pass by Reference
+//Changing the argument inside the function affect
+//the variable passed from outside the function.
