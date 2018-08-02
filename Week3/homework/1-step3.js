9b5df9d2de8f6f345a4e537754fb7e15d1ece21b
mehmetsaidcoskun <39768263+mehmetsaidcoskun@users.noreply.github.com>
2018-08-02 19:16:05 +0200

    js2-week3-homework

--- Week3/homework/1-step3.js
+++ Week3/homework/1-step3.js
@@ -2,6 +2,11 @@
 
 function foo(func) {
   // What to do here? 
+
+  //write a function that takes another function 
+  //as an argument and runs it.
+
+  return func();
 }
 
 function bar() {
--- Week3/homework/5-step3.js
+++ Week3/homework/5-step3.js
@@ -1,2 +1,22 @@
 'use strict';
 // paste your freeCodeCamp solutions in here
+
+//Modify function multiplyAll so that it multiplies the 
+//product variable by each number in the sub - arrays of arr
+
+function multiplyAll(arr) {
+    let product = 0;
+    // Only change code below this line
+
+    for (let i = 0; i < arr.length; i++) {
+        for (let k = 0; k < arr[i].length; k++) {
+            product += arr[i][k];
+        }
+    }
+
+    // Only change code above this line
+    return product;
+}
+
+// Modify values below to test your code
+console.log(multiplyAll([[1, 2], [3, 4], [5, 6, 7]]));
