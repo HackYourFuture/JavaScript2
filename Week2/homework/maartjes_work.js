b08e181d858fceaf6364696f100b29b8d506708c
mehmetsaidcoskun <39768263+mehmetsaidcoskun@users.noreply.github.com>
2018-08-02 19:13:04 +0200

    js2-week2 homework

--- Week2/homework/maartjes_work.js
+++ Week2/homework/maartjes_work.js
@@ -45,3 +45,34 @@ const tuesday = [
 const tasks = monday.concat(tuesday);
 
 // Add your code here
+
+
+//Step-1: Map the tasks to durations in hours.
+const workHour = tasks.map(myElement =>
+  (Math.round(100 * (myElement.duration / 60))) / 100);
+//60 is used to show duration in hour.
+//100 is used to round with two decimals such as 0.33 or 1.23
+
+
+//Step-2: Filter out everything that took less than two hours
+//(i.e., remove from the collection)
+const morethan2Hours = workHour.filter(hour => hour >= 2);
+// durations which are less than 2, are gone.
+
+
+//Step-3: Multiply the each duration by a per-hour rate for billing
+//(you can decide yourself what Maartje should earn per hour) 
+//and sum it all up.
+const totalHour = morethan2Hours.reduce((per, total) => total += per);
+
+const earningAmount = `€ ${Math.round((totalHour * 24.5) * 100) / 100}`;
+
+console.log(earningAmount);
+//it will return € 457.17
+
+
+
+
+//Output a formatted Euro amount, rounded to Euro cents,
+//e.g: € 12.34.
+
