b0041e39754c72ce6440389769f30b948eaeaeef
whitepiolin <whitepiolin@gmail.com>
2018-08-04 23:25:55 +0200

    hw step4

--- Week3/homework/step4-bonus.js
+++ Week3/homework/step4-bonus.js
@@ -3,8 +3,18 @@
 const values = ['a', 'b', 'c', 'd', 'a', 'e', 'f', 'c'];
 
 // Add your function here. Try and come up with a good name for this function
+console.log(values);
 
+function removeDuplicate(arr){
+  let uniqueArray = arr.filter(function(elem, index) {
+      return index == arr.indexOf(elem);
+  });
+  return uniqueArray
+}
 // Replace `yourFunction` with the name of the function you just created
-const uniqueValues = yourFunction(values);
+
+const uniqueValues = removeDuplicate(values);
 
 console.log(uniqueValues);
+
+
--- Week3/homework/step4.js
+++ Week3/homework/step4.js
@@ -2,7 +2,15 @@
 
 // Add your code here
 
+function createBase(x) {
+    return function (y) {
+        return x + y;
+    };
+ }
+
 const addSix = createBase(6);
 
 addSix(10); // returns 16
 addSix(21); // returns 27
+
+
