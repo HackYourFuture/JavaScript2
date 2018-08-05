784ae5e0624e254c10adb096c7eca622af9c28bd
karamGhazzi <Karam-g1993@hotmail.com>
2018-08-05 03:53:45 +0200

    part of my homework3

--- Week3/homework/2-step3.js
+++ Week3/homework/2-step3.js
@@ -1,8 +1,30 @@
 'use strict';
 
 function threeFive(startIndex, stopIndex, threeCallback, fiveCallback) {
-  const values = [];
-  // Add your code here
+    const values = [];
+    // Add your code here
+    let myArr = stopIndex - startIndex;
+    for (let i = 0; i <= myArr; i++) {
+        values[i] = startIndex;
+        startIndex++;
+    }
+    values.forEach((value) => {
+        if (value % 3 === 0 && value % 5 === 0) {
+            threeCallback();
+            fiveCallback();
+        } else if (value % 3 === 0) {
+            threeCallback();
+        } else if (value % 5 === 0) {
+            fiveCallback();
+        }
+    });
 }
 
+function sayThree() {
+    console.log("3");
+}
+
+function sayFive() {
+    console.log("5");
+}
 threeFive(10, 15, sayThree, sayFive);
--- Week3/homework/3-step3.js
+++ Week3/homework/3-step3.js
@@ -1,25 +1,52 @@
 'use strict';
 
 // use a 'for' loop
-function repeatStringNumTimesWithFor(str, num) {
-  // add your code here
-  return str;
+function repeatStringNumTimesWithFor(str, nr) {
+    // add your code here
+    let reString = "";
+    if (nr >= 0) {
+        for (let i = 0; i < nr; i++) {
+            reString += str;
+        }
+        return reString;
+    } else {
+        return reString;
+    }
 }
 
 console.log('for', repeatStringNumTimesWithFor('abc', 3));
 
 // use a 'while' loop
-function repeatStringNumTimesWithWhile(str, num) {
-  // add your code here
-  return str;
+function repeatStringNumTimesWithWhile(str, nr) {
+    // add your code here
+    let reString = "";
+    if (nr >= 0) {
+        while (nr) {
+            reString += str;
+            nr--;
+        }
+        return reString;
+    } else {
+        return reString;
+    }
 }
 
 console.log('while', repeatStringNumTimesWithWhile('abc', 3));
 
 // use a 'do...while' loop
-function repeatStringNumTimesWithDoWhile(str, num) {
-  // add your code here
-  return str;
+function repeatStringNumTimesWithDoWhile(str, nr) {
+    // add your code here
+    let reString = "";
+    if (nr >= 0) {
+        do {
+            reString += str, nr--;
+
+        }
+        while (nr);
+        return reString
+    } else {
+        return reString;
+    }
 }
 
 console.log('while', repeatStringNumTimesWithDoWhile('abc', 3));
