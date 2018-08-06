9d06927be92274d7128f9477062a26d57208a37f
karamGhazzi <Karam-g1993@hotmail.com>
2018-08-06 03:51:53 +0200

    my homwork js2 week 2 correction

--- Week2/homework/maartjes_work.js
+++ Week2/homework/maartjes_work.js
@@ -1,47 +1,49 @@
 'use strict';
 
-const monday = [
-  {
-    name: 'Write a summary HTML/CSS',
-    duration: 180
-  },
-  {
-    name: 'Some web development',
-    duration: 120
-  },
-  {
-    name: 'Fix homework for class10',
-    duration: 20
-  },
-  {
-    name: 'Talk to a lot of people',
-    duration: 200
-  }
+const monday = [{
+        name: 'Write a summary HTML/CSS',
+        duration: 180
+    },
+    {
+        name: 'Some web development',
+        duration: 120
+    },
+    {
+        name: 'Fix homework for class10',
+        duration: 20
+    },
+    {
+        name: 'Talk to a lot of people',
+        duration: 200
+    }
 ];
 
-const tuesday = [
-  {
-    name: 'Keep writing summary',
-    duration: 240
-  },
-  {
-    name: 'Some more web development',
-    duration: 180
-  },
-  {
-    name: 'Staring out the window',
-    duration: 10
-  },
-  {
-    name: 'Talk to a lot of people',
-    duration: 200
-  },
-  {
-    name: 'Look at application assignments new students',
-    duration: 40
-  }
+const tuesday = [{
+        name: 'Keep writing summary',
+        duration: 240
+    },
+    {
+        name: 'Some more web development',
+        duration: 180
+    },
+    {
+        name: 'Staring out the window',
+        duration: 10
+    },
+    {
+        name: 'Talk to a lot of people',
+        duration: 200
+    },
+    {
+        name: 'Look at application assignments new students',
+        duration: 40
+    }
 ];
 
 const tasks = monday.concat(tuesday);
 
 // Add your code here
+
+const salaryPerHour = tasks.map(element => element.duration / 60).filter(element => element >= 2).reduce((total, element) => total + (element * 18.70), 0);
+const resultOutput = "€ " + salaryPerHour.toFixed(2);
+console.log(resultOutput);
