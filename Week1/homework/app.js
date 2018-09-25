b392245fa43f7d8cabdcce5d073a7576fdccbfd0
ZerSolo <yosiami15@gmail.com>
2018-09-26 00:30:57 +0200

    create list of my fav. books

--- Week1/homework/app.js
+++ Week1/homework/app.js
@@ -1,11 +1,21 @@
 'use strict'; {
   const bookTitles = [
-    // Replace with your own book titles
-    'How to make a life', 'this is it'
+
+    'how_to_Make_Life', 
+    'the_sign_and_seal', 
+    'the_book_of_genesius', 
+    'reveletion', 
+    'lukas_gospel', 
+    'the_tradition_of_orthodox', 
+    'the_next_goal',  
+    'astronomy_in_christian_life', 
+    'my_goal', 
+    'my_future'
   ];
 
-
-
-  console.log("This is te book titles" + bookTitles);
-
+  console.log("These are my book titles" + bookTitles);
+  
 }
+const allLink = document.getElementsByTagName("a")
+const firstLink = allLink[0];
+console.log(allLink);
