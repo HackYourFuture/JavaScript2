5bbf678d3f209639f0c99da2cc598c1ea2609e62
karamGhazzi <Karam-g1993@hotmail.com>
2018-09-30 03:07:30 +0200

    js2 wk1

--- Week1/homework/app.js
+++ Week1/homework/app.js
@@ -1,11 +1,108 @@
 'use strict';
-{
-  const bookTitles = [
-    // Replace with your own book titles
-    'harry_potter_chamber_secrets'
-  ];
+
+const bookTitles2 = {
+  court_of_the_stone_children: {
+    title: "Court of the Stone Children",
+    language: "English",
+    author: "Cameron, Eleanor",
+
+  },
+  alices_adventures_in_wonderland: {
+    title: "Alice's Adventures in Wonderland",
+    language: "English",
+    author: "Carroll, Lewis",
+
+  },
+  through_the_looking_glass: {
+    title: "Through the Looking Glass",
+    language: "English",
+    author: "Carroll, Lewis",
+
+  },
+  what_am_i: {
+    title: "What Am I?",
+    language: "English",
+    author: "Charles, N. N.",
+
+  },
+  the_city_of_gold_and_lead: {
+    title: "The City of Gold & Lead",
+    language: "English",
+    author: "Christopher, John",
+
+  },
+  the_pool_of_fire: {
+    title: "The Pool of Fire",
+    language: "English",
+    author: "Christopher, John",
+
+  },
+  the_white_mountains: {
+    title: "The White Mountains",
+    language: "English",
+    author: "Christopher, John",
+
+  },
+  the_dark_is_rising: {
+    title: "The Dark is Rising",
+    language: "English",
+    author: "Cooper, Susan",
+
+  },
+  charlie_and_the_chocolate_factory: {
+    title: "Charlie and the Chocolate Factory",
+    language: "English",
+    author: "Dahl, Roald",
+
+  },
+  james_and_the_giant_peach: {
+    title: "James and the Giant Peach",
+    language: "English",
+    author: "Dahl, Roald",
+
+  }
+};
 
 
-  // Replace with your own code
-  console.log(bookTitles);
+// Replace with your own code
+function createAndAppend(name, parent) {
+  const elm = document.createElement(name);
+  parent.appendChild(elm);
+  return elm;
+}
+function booksInformation() {
+  const list = document.createElement("section");
+  list.setAttribute("id", "bookList");
+  const ul = createAndAppend("ul", list);
+  document.body.appendChild(list);
+  for (let i = 0; i < Object.keys(bookTitles2).length; i++) {
+    const myObject = Object.keys(bookTitles2);
+    const bookName = myObject[i];
+    const bookDetails = bookTitles2[bookName];
+
+    const li = createAndAppend("li", ul);
+    const h1 = createAndAppend("h1", li);
+    h1.setAttribute("class", "heading");
+
+    const img = createAndAppend("img", li);
+    img.setAttribute("class", "imglist");
+    img.setAttribute("src", "booksimg/" + [i] + ".jpg");
+
+    const p = createAndAppend("p", li);
+    p.setAttribute("class", "language");
+
+    const p2 = createAndAppend("p", li);
+    p2.setAttribute("class", "author");
+
+    // ul.appendChild(li);
+    // li.appendChild(h1);
+    // li.appendChild(img);
+    // li.appendChild(p);
+    // li.appendChild(p2);
+
+    h1.innerHTML += bookDetails['title'];
+    p.innerHTML += bookDetails['language'];
+    p2.innerHTML += bookDetails['author'];
+
+  }
 }
