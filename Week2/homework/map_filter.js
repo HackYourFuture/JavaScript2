2051f73dec4834b53b9affd14d257bf225f8c2c0
Karam <ra4mos@yahoo.com>
2018-10-03 19:54:39 +0200

    js hw week2

--- Week2/homework/map_filter.js
+++ Week2/homework/map_filter.js
@@ -1,5 +1,24 @@
 'use strict';
 
 const numbers = [1, 2, 3, 4];
+/*
+const newNumbers = [];
 
-// Add your code here
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] % 2 !== 0) {
+    newNumbers.push(numbers[i] * 2);
+  }
+}
+console.log('The doubled numbers are', newNumbers);
+*/
+function isOdd(num) {
+  return num % 2 !== 0;
+}
+const filteredNumber = (numbers.filter(isOdd));
+function double(num) {
+  return num * 2;
+}
+
+console.log('The doubled numbers are', filteredNumber.map(double));
+
+
