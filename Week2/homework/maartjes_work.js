f2045c7833ac1b583b90336e17961531a354ca23
heboshka <heboshka@hotmail.com>
2018-10-03 22:33:34 +0200

    Hiba HW JS2 Week2

--- Week2/homework/maartjes_work.js
+++ Week2/homework/maartjes_work.js
@@ -1,47 +1,64 @@
 'use strict';
 
 const monday = [
-  {
-    name: 'Write a summary HTML/CSS',
-    duration: 180
-  },
-  {
-    name: 'Some web development',
-    duration: 120
-  },
-  {
-    name: 'Fix homework for class10',
-    duration: 20
-  },
-  {
-    name: 'Talk to a lot of people',
-    duration: 200
-  }
+    {
+        name: 'Write a summary HTML/CSS',
+        duration: 180
+    },
+    {
+        name: 'Some web development',
+        duration: 120
+    },
+    {
+        name: 'Fix homework for class10',
+        duration: 20
+    },
+    {
+        name: 'Talk to a lot of people',
+        duration: 200
+    }
 ];
 
 const tuesday = [
-  {
-    name: 'Keep writing summary',
-    duration: 240
-  },
-  {
-    name: 'Some more web development',
-    duration: 180
-  },
-  {
-    name: 'Staring out the window',
-    duration: 10
-  },
-  {
-    name: 'Talk to a lot of people',
-    duration: 200
-  },
-  {
-    name: 'Look at application assignments new students',
-    duration: 40
-  }
+    {
+        name: 'Keep writing summary',
+        duration: 240
+    },
+    {
+        name: 'Some more web development',
+        duration: 180
+    },
+    {
+        name: 'Staring out the window',
+        duration: 10
+    },
+    {
+        name: 'Talk to a lot of people',
+        duration: 200
+    },
+    {
+        name: 'Look at application assignments new students',
+        duration: 40
+    }
 ];
 
 const tasks = monday.concat(tuesday);
 
-// Add your code here
+const tasksInHours = tasks.map(newTasks => {
+    const newDuration = newTasks.duration / 60;
+    return {
+        name: newTasks.name, duration: newDuration
+    }
+});
+
+console.log(tasksInHours);
+
+const tasksTakesMoreThanTwoHours = tasksInHours.filter(twoHoursTasks => {
+    return twoHoursTasks.duration >= 2
+});
+console.log(tasksTakesMoreThanTwoHours);
+
+let hourRate = 18;
+let taskRate = tasksInHours.reduce((rate, hour) => rate + (hour.duration * hourRate), 0);
+console.log("Maartje should earn : € " + taskRate);
+
--- Week2/homework/map_filter.js
+++ Week2/homework/map_filter.js
@@ -1,5 +1,7 @@
 'use strict';
 
 const numbers = [1, 2, 3, 4];
-
-// Add your code here
+const oddNumbers = numbers.filter(num => num % 2 !== 0);
+console.log(oddNumbers);
+const doubleNum = oddNumbers.map(num => num * 2);
+console.log(doubleNum);
