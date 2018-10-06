2722b5df56ce1ab5e3ba0cb8cd0de1c71a58209a
RimSebai <rimsebai79@gmail.com>
2018-10-06 12:35:50 +0300

    my homework week1 JS

--- Week2/homework/maartjes_work.js
+++ Week2/homework/maartjes_work.js
@@ -44,4 +44,69 @@ const tuesday = [
 
 const tasks = monday.concat(tuesday);
 
-// Add your code here
+//console.clear()
+const numbers = [1, 2, 3, 4];
+let newNumbers = [];
+
+// for (let i = 0; i < numbers.length; i++) {
+//   if (numbers[i] % 2 !== 0) {
+//     newNumbers.push(numbers[i] * 2);
+//   }
+// }
+newNumbers = numbers.filter(el => el % 2 !== 0).map(el => el*2)
+
+console.log('The doubled numbers are', newNumbers);
+
+const monday = [
+  {
+    name: 'Write a summary HTML/CSS',
+    duration: 180
+  },
+  {
+    name: 'Some web development',
+    duration: 120
+  },
+  {
+    name: 'Fix homework for class10',
+    duration: 20
+  },
+  {
+    name: 'Talk to a lot of people',
+    duration: 200
+  }
+];
+
+const tuesday = [
+  {
+    name: 'Keep writing summary',
+    duration: 240
+  },
+  {
+    name: 'Some more web development',
+    duration: 180
+  },
+  {
+    name: 'Staring out the window',
+    duration: 10
+  },
+  {
+    name: 'Talk to a lot of people',
+    duration: 200
+  },
+  {
+    name: 'Look at application assignments new students',
+    duration: 40
+  }
+];
+
+const tasks = monday.concat(tuesday);
+const Maartje = tasks.map(obj => obj.duration/60).filter(el => el>=2)
+.reduce(
+    (sum, hours) =>  hours +sum 
+)*30.34
+
+console.log(Maartje.toFixed(2)+' €')
+
+
+
+
