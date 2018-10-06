7906ac27c7f872096cdfca4beba9686c64185d42
RimSebai <rimsebai79@gmail.com>
2018-10-06 12:56:11 +0300

    my homework week2 JS

--- Week2/homework/map_filter.js
+++ Week2/homework/map_filter.js
@@ -3,3 +3,14 @@
 const numbers = [1, 2, 3, 4];
 
 // Add your code here
+
+let newNumbers = [];
+
+// for (let i = 0; i < numbers.length; i++) {
+//   if (numbers[i] % 2 !== 0) {
+//     newNumbers.push(numbers[i] * 2);
+//   }
+// }
+newNumbers = numbers.filter(el => el % 2 !== 0).map(el => el * 2)
+
+console.log('The doubled numbers are', newNumbers);
