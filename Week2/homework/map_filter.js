f845a34e05799ec5476380ad76aefe9100c78db9
Karam <ra4mos@yahoo.com>
2018-10-11 10:31:34 +0200

    deleted conflicting hw

--- Week2/homework/map_filter.js
+++ Week2/homework/map_filter.js
@@ -1,24 +1,3 @@
 'use strict';
 
 const numbers = [1, 2, 3, 4];
-/*
-const newNumbers = [];
-
-for (let i = 0; i < numbers.length; i++) {
-  if (numbers[i] % 2 !== 0) {
-    newNumbers.push(numbers[i] * 2);
-  }
-}
-console.log('The doubled numbers are', newNumbers);
-*/
-function isOdd(num) {
-  return num % 2 !== 0;
-}
-const filteredNumber = (numbers.filter(isOdd));
-function double(num) {
-  return num * 2;
-}
-
-console.log('The doubled numbers are', filteredNumber.map(double));
-
-
