7db66d6ef0b156095ba8a1ac2a013ed6a79bd0b8
RimSebai <40184200+RimSebai@users.noreply.github.com>
2018-10-12 21:02:40 +0200

    Update step4.js

--- Week3/homework/step4.js
+++ Week3/homework/step4.js
@@ -1,8 +1,16 @@
 'use strict';
 
 // Add your code here
+function createBase(x) {
+    return function (z) {
+      sum = x + z;
+      return sum;
+    }
+  }
+  const addSix = createBase(6);
 
-const addSix = createBase(6);
+  console.log(addSix(10));
+  console.log(addSix(21));
 
 addSix(10); // returns 16
 addSix(21); // returns 27
