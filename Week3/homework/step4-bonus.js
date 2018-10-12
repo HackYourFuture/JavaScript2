99d571c68d6564080df8c2999c53e46d735d16e3
RimSebai <40184200+RimSebai@users.noreply.github.com>
2018-10-12 21:04:02 +0200

    Update step4-bonus.js

--- Week3/homework/step4-bonus.js
+++ Week3/homework/step4-bonus.js
@@ -3,8 +3,17 @@
 const values = ['a', 'b', 'c', 'd', 'a', 'e', 'f', 'c'];
 
 // Add your function here. Try and come up with a good name for this function
+uniqueArray = values.filter(function duplicateRemover (item, pos) {
+    return values.indexOf(item) == pos;
+})
+console.log(uniqueArray);
 
 // Replace `yourFunction` with the name of the function you just created
-const uniqueValues = yourFunction(values);
+const uniqueValues = duplicateRemover(values);
 
 console.log(uniqueValues);
+
+//another solution
+
+[... new Set(values)];
+console.log([... new Set(values)]);
