620c6a0ab1eb2505c49a3887f02747c3d247d2a4
Mohamad <mohamad.aezaldin@gmail.com>
2018-10-13 09:43:53 +0200

    adding the homework of week3

--- Week3/homework/step4-bonus.js
+++ Week3/homework/step4-bonus.js
@@ -3,8 +3,12 @@
 const values = ['a', 'b', 'c', 'd', 'a', 'e', 'f', 'c'];
 
 // Add your function here. Try and come up with a good name for this function
+function createSet(arr) {
+  const arrSet = new Set(arr);
+  return arrSet;
+}
 
 // Replace `yourFunction` with the name of the function you just created
-const uniqueValues = yourFunction(values);
+const uniqueValues = createSet(values);
 
 console.log(uniqueValues);
