2a12c067f89c5dfe2e2015292bd2bffe945c8b81
Seif Tabaja <saif_tabaja@hotmail.com>
2018-10-29 23:04:24 +0100

    week2 homework done

--- Week2/homework/maartjes_work.js
+++ Week2/homework/maartjes_work.js
@@ -43,5 +43,19 @@ const tuesday = [
 ];
 
 const tasks = monday.concat(tuesday);
+//console.log(tasks);
+
 
 // Add your code here
+// change from mins to hour, then filter out all values less than 2.
+const setFilteredHours = tasks.map(i => i.duration / 60).filter(i => i >= 2)
+//console.log(setFilteredHours);
+
+
+// totalPayment = (map) an hour * 30 => returns [ 90, 60, 100, 120, 90, 100. 
+// Then(reduce) the sum of all values is 560 euros
+let payPerHour = 50;
+const totalPayment = setFilteredHours.map(i => i * payPerHour).reduce((total, value) => total + value)
+console.log("the total amount of money is: " + "€" + totalPayment.toFixed(2));
+
+//toFixed(x): set the number of ints after the coma in case of floats
--- Week2/homework/map_filter.js
+++ Week2/homework/map_filter.js
@@ -3,3 +3,16 @@
 const numbers = [1, 2, 3, 4];
 
 // Add your code here
+
+//filters out all the even numbers ==> returns [1,3]
+const oddsFilter = numbers.filter(i => i % 2 != 0)
+//doubles the odd numbers only (1,3)
+const doubled = oddsFilter.map(i => {
+  if (i % 2 != 0) { return i * 2; }
+  else { return i; }
+});
+
+console.log("the thrown away numbers are: " + oddsFilter);
+console.log("The doubled numbers are: " + doubled);
+
+
