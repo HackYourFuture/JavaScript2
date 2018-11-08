af16e6d3d7c5cc8a4ee1b8061dfdaa480737097e
Yaacob <yyaacob0@gmail.com>
2018-11-08 17:16:21 +0100

    adding books, adding DOM elements and adding the function that generates a list

--- Week1/homework/app.js
+++ Week1/homework/app.js
@@ -1,11 +1,37 @@
 'use strict';
+
 {
   const bookTitles = [
     // Replace with your own book titles
-    'harry_potter_chamber_secrets'
+    'I_am_number_four',
+    'The_power_of_six',
+    'The_rise_of_nine',
+    'The_fall_of_five',
+    'The_revenge_of_seven',
+    'The_fate_of_ten',
+    'United_as_one',
+    'Twee_verhalen_van_sherlock_holmes',
+    'The_psychoanalyst',
+    'In_the_heat_of_summer' 
   ];
+ 
+  function generatingBookList () { 
 
+    // Creating the DOM element. 
 
-  // Replace with your own code
-  console.log(bookTitles);
-}
+    let generatingP = document.createElement('p');
+    let generatingUl = document.createElement('ul');
+    generatingP.appendChild(generatingUl);
+
+    // Creating the function
+
+    for (let i = 0; i<bookTitles.length; i++) {
+        let list = document.createElement('li');
+        generatingUl.appendChild(list);
+        list.innerHTML= bookTitles[i];
+    }
+    console.log(generatingP);
+    }
+    generatingBookList();
+    }
+    
