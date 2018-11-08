4419b6dadd668cb0859acc1d234ccf337bb339d7
Yaacob <yyaacob0@gmail.com>
2018-11-08 17:29:28 +0100

    lowercased all the books name

--- Week1/homework/app.js
+++ Week1/homework/app.js
@@ -3,16 +3,16 @@
 {
   const bookTitles = [
     // Replace with your own book titles
-    'I_am_number_four',
-    'The_power_of_six',
-    'The_rise_of_nine',
-    'The_fall_of_five',
-    'The_revenge_of_seven',
-    'The_fate_of_ten',
-    'United_as_one',
-    'Twee_verhalen_van_sherlock_holmes',
-    'The_psychoanalyst',
-    'In_the_heat_of_summer' 
+    'i_am_number_four',
+    'the_power_of_six',
+    'the_rise_of_nine',
+    'the_fall_of_five',
+    'the_revenge_of_seven',
+    'the_fate_of_ten',
+    'united_as_one',
+    'twee_verhalen_van_sherlock_holmes',
+    'the_psychoanalyst',
+    'in_the_heat_of_summer' 
   ];
  
   function generatingBookList () { 
