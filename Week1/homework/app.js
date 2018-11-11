6b10fe94c7fd6e72d7265db411a16a2aa6305559
Burhan Khuzam <burhankhuzam@gmail.com>
2018-11-11 12:29:04 +0100

    some homework progress

--- Week1/homework/app.js
+++ Week1/homework/app.js
@@ -1,11 +1,27 @@
-'use strict';
-{
-  const bookTitles = [
-    // Replace with your own book titles
-    'harry_potter_chamber_secrets'
-  ];
+// 'use strict';
+// {
+const bookTitles = ['becoming', 'those_who_knew', 'good_and_mad', 'the_missy_middle',
+  'lake_success', 'fashion_climbing', 'crudo', 'winners_take_all',
+  'french_exit', 'how_to_be_famous'];
 
-
-  // Replace with your own code
-  console.log(bookTitles);
+for (i = 0; i < bookTitles.length; i++) {
+  console.log("I love this book: " + bookTitles[i]);
 }
+
+
+
+  // var ul = document.createElement("ul");
+  // document.body.appendChild(ul);
+
+  // for (var i = 1; i <= 10; i++) {
+  //   var li = document.createElement("li");
+  //   li.className = "file";
+
+  //   var a = document.createElement("a");
+  //   a.innerHTML = "Subfile " + i;
+
+  //   li.appendChild(a);
+  //   ul.appendChild(li);
+  // }
+
+// }
