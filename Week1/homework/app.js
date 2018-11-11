ed37c665f762ca357206b444cd98893ebe8e0998
mohamad monir <mohamadmonirhamid@gmail.com>
2018-11-11 12:31:47 +0100

    js2 wk 1

--- Week1/homework/app.js
+++ Week1/homework/app.js
@@ -2,10 +2,28 @@
 {
   const bookTitles = [
     // Replace with your own book titles
-    'harry_potter_chamber_secrets'
+    "harry_potter_chamber_secrets",
+    "tell_of_two_cities",
+    "hamlet",
+    "merchant_of_venice",
+    "hotel",
+    "the_sound_and_the_fury",
+    "for_whom_the_bell_tolls",
+    "the_old_man_and_the_sea",
+    "palace_walk", 
+    "mirror"
   ];
 
 
-  // Replace with your own code
-  console.log(bookTitles);
+ // Replace with your own code
+ 
+  function addId() {
+    const addUl = document.createElement("ul");
+    for (let i = 0; i < bookTitles.length; i++) {
+      let items = document.createElement("li");
+    }
+  }
+  
 }
+console.log(addId(bookTitles));
+
