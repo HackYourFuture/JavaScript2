635d248a58759e227cd758eee020fbfd28c3e1ab
F.Mohamed <mohamedfatuma980@gmail.com>
2018-11-14 17:42:01 +0100

    week2 maartjes_work.js and map_filter.js

--- Week2/homework/maartjes_work.js
+++ Week2/homework/maartjes_work.js
@@ -45,3 +45,30 @@ const tuesday = [
 const tasks = monday.concat(tuesday);
 
 // Add your code here
+
+//task duration in .... minutes
+const taskDurationMinutes = tasks.map(function(task) {
+  return task.duration;
+})
+
+console.log(taskDurationMinutes); //[ 180, 120, 20, 200, 240, 180, 10, 200, 40 ]
+
+// task duration in ... hours
+const taskDuartionHours = taskDurationMinutes.map(function(workHours){
+  return workHours / 60;
+}) 
+console.log(taskDuartionHours);//[ 3, 2, 0.33, 3.33, 4, 3, 0.17, 3.33, 0.67]
+
+//filter the durations which are less than 2hr
+const earningHours = taskDuartionHours.filter(function(payHours) {
+    return payHours >= 2;
+})
+console.log(earningHours);//[ 3, 2, 3.33, 4, 3, 3.33]
+
+//total earning .. let earning per hour is 10
+const earning = earningHours
+    .map(hours => hours * 10)
+    .reduce ((sum, pay) => sum + pay);
+
+    console.log(earning);//186.66666666666669
+    console.log(earning.toFixed(2)); //186.67
--- Week2/homework/map_filter.js
+++ Week2/homework/map_filter.js
@@ -3,3 +3,21 @@
 const numbers = [1, 2, 3, 4];
 
 // Add your code here
+
+//using filter and map
+const newNumbers = numbers
+    .filter(function(number) {
+    return number % 2 !==0;
+})
+    .map(function(odd) {
+        return odd * 2; 
+    })
+    
+console.log(newNumbers); //[ 2, 6 ]
+
+//filter, map and =>
+const newNumbers = numbers
+      .filter(number => number % 2 !== 0)
+      .map(odd => odd * 2);
+
+console.log(newNumbers); // [ 2, 6 ]
