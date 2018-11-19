34435a9ffae34fd00055dafe9625fdd416634f43
Yaacob <yyaacob0@gmail.com>
2018-11-19 18:53:38 +0100

    first part of the homework week2

--- Week2/homework/map_filter.js
+++ Week2/homework/map_filter.js
@@ -3,3 +3,14 @@
 const numbers = [1, 2, 3, 4];
 
 // Add your code here
+
+// const newNumbers = [];
+// for (let i = 0; i < numbers.length; i++) {
+//     if (numbers[i] % 2 !== 0) {
+//       newNumbers.push(numbers[i] * 2);
+//     }
+//   }
+//   console.log(newNumbers);
+
+const newNum = numbers.filter(numbers => numbers % 2 !== 0).map(numbers => numbers * 2);
+console.log(newNum);
