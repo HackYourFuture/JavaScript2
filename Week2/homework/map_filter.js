bef55f48aed185c0c050a58044443bf657e922c1
Burhan Khuzam <burhankhuzam@gmail.com>
2018-11-21 21:47:12 +0100

    JS2 Week 2 Homework

--- Week2/homework/map_filter.js
+++ Week2/homework/map_filter.js
@@ -1,5 +1,13 @@
-'use strict';
-
 const numbers = [1, 2, 3, 4];
+// const newNuxmbers = [];
 
-// Add your code here
+// for (let i = 0; i < numbers.length; i++) {
+//   if (numbers[i] % 2 !== 0) {
+//     newNumbers.push(numbers[i] * 2);
+//   }
+// }
+
+let newNumbers = numbers
+    .filter(number => number % 2 !== 0) 
+    .map(number => number * 2);
+console.log('The doubled numbers are', newNumbers);
