df5bde3593802778be8984e6b76986e98c2e1eb0
soumaia <soumaiamamo@gmail.com>
2018-11-23 12:16:03 +0100

    my home work javascript 2 ,week2

--- Week2/homework/maartjes_work.js
+++ Week2/homework/maartjes_work.js
@@ -45,3 +45,30 @@ const tuesday = [
 const tasks = monday.concat(tuesday);
 
 // Add your code here
+ let durationInHours = tasks.map(hours => hours.duration / 60 ); 
+ console.log(durationInHours);
+
+ // (9) [3, 2, 0.3333333333333333, 3.3333333333333335, 4, 3,
+ // 0.16666666666666666, 3.3333333333333335, 0.6666666666666666]
+
+ let moreThanTwo = durationInHours.filter(hours => hours > 2 );
+ console.log(moreThanTwo);
+
+//(5) [3, 3.3333333333333335, 4, 3, 3.3333333333333335] 
+
+let salary = moreThanTwo.map(hour => hour * 10 );
+
+let amount = salary.reduce((getsum , total) => getsum + total );
+console.log(amount);
+
+//166.66666666666669
+
+let theSalary = '€ ' +  amount.toFixed(2);
+
+console.log(theSalary);
+
+//€166.67
+
+
+
+
--- Week2/homework/map_filter.js
+++ Week2/homework/map_filter.js
@@ -2,4 +2,15 @@
 
 const numbers = [1, 2, 3, 4];
 
-// Add your code here
+
+const newNumber = numbers.filter( number => number % 2 !== 0);
+console.log(newNumber);
+ 
+const mapping = newNumber.map(number =>number * 2);
+console.log(mapping);
+
+
+console.log('The doubled numbers are', mapping);
+
+
+
