d079cfcb4e3dc2c44e5790ac1679b12d779a52ed
7more10 <42109947+7more10@users.noreply.github.com>
2018-11-24 19:43:57 +0100

    Hallo! This my homework for week2 JS2. Alek

--- Week2/homework/map_filter.js
+++ Week2/homework/map_filter.js
@@ -1,5 +1,69 @@
-'use strict';
-
 const numbers = [1, 2, 3, 4];
 
-// Add your code here
+for (let i = 0; i < numbers.length; i++) {
+const newNumbers = numbers.filter(number => number % 2);
+  const finalNumbers = newNumbers.map(number => number * 2);
+  console.log('The final numbers are ' + finalNumbers);
+  }
+  
+  const monday = [
+    {
+      name: 'Write a summary HTML/CSS',
+      duration: 180
+    },
+    {
+      name: 'Some web development',
+      duration: 120
+    },
+    {
+      name: 'Fix homework for class10',
+      duration: 20
+    },
+    {
+      name: 'Talk to a lot of people',
+      duration: 200
+    }
+  ];
+  
+  const tuesday = [
+    {
+      name: 'Keep writing summary',
+      duration: 240
+    },
+    {
+      name: 'Some more web development',
+      duration: 180
+    },
+    {
+      name: 'Staring out the window',
+      duration: 10
+    },
+    {
+      name: 'Talk to a lot of people',
+      duration: 200
+    },
+    {
+      name: 'Look at application assignments new students',
+      duration: 40
+    }
+  ];
+  
+  const tasks = monday.concat(tuesday);
+  console.log(tasks);
+
+  const durationInHours = tasks.map(tasks => tasks.duration / 60);
+  console.log(durationInHours);
+
+  const lessThanTwoTasks = durationInHours.filter(hour => hour >= 2);
+  console.log(lessThanTwoTasks);
+
+  const ratePerHourInEuro = 10;
+ 
+  function arraySum(array){
+    let sum = 0;
+    for(let i = 0; i < array.length; i++){
+    sum += array[i] * ratePerHourInEuro;
+    }
+    console.log(sum.toFixed(2));
+    }
+    arraySum(lessThanTwoTasks);
