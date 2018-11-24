9e4a82df1495c411e3c6c4db33faa4269f1a9428
kaniwar103 <kaniwar103@gamil.com>
2018-11-24 21:25:23 +0100

    js2 homework week2

--- Week2/homework/maartjes_work.js
+++ Week2/homework/maartjes_work.js
@@ -45,3 +45,18 @@ const tuesday = [
 const tasks = monday.concat(tuesday);
 
 // Add your code here
+// Map the tasks to durations in hours.
+const hours = tasks.map(function(taskHour){
+  return taskHour.duration / 60;
+})
+console.log(hours);//[ 3,2,0.33,3.33,4,3,0.16,3.33,0.66 ]
+
+// Filter out everything that took less than two hours
+const earningHours = hours.filter(function(earningHour){
+return earningHour >= 2;
+})
+console.log(earningHours);//[ 3, 2, 3.33, 4, 3, 3.33 ]
+
+// Multiply the duration per-hour rate and sum it all up.
+const totalEarning = earningHours.map(hours => hours * 9)
+.reduce((money, payment) => money + payment);
--- Week2/homework/map_filter.js
+++ Week2/homework/map_filter.js
@@ -3,3 +3,10 @@
 const numbers = [1, 2, 3, 4];
 
 // Add your code here
+const numbers = [1, 2, 3, 4];
+const newNumbers = numbers.filter(function(number){
+    return (number % 2 !== 0);
+}).map(function(number){
+    return number * 2;
+});
+console.log("The doubled numbers are", newNumbers); // [2, 6]
