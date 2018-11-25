87b0b163794768968acf6b8f64ebfc2d45bc4522
keyyaali <kbbaloch48@gmail.com>
2018-11-25 16:17:16 +0100

    this is my week2 hw second file

--- Week2/homework/maartjes_work.js
+++ Week2/homework/maartjes_work.js
@@ -1,24 +1,22 @@
-'use strict';
-
-const monday = [
-  {
-    name: 'Write a summary HTML/CSS',
+const tasks = monday.concat(tuesday);
+// Add your code here
+{
+  name: 'Write a summary HTML/CSS',
     duration: 180
-  },
-  {
-    name: 'Some web development',
+},
+{
+  name: 'Some web development',
     duration: 120
-  },
-  {
-    name: 'Fix homework for class10',
+},
+{
+  name: 'Fix homework for class10',
     duration: 20
-  },
-  {
-    name: 'Talk to a lot of people',
+},
+{
+  name: 'Talk to a lot of people',
     duration: 200
-  }
-];
-
+}
+  ];
 const tuesday = [
   {
     name: 'Keep writing summary',
@@ -41,7 +39,10 @@ const tuesday = [
     duration: 40
   }
 ];
-
 const tasks = monday.concat(tuesday);
-
-// Add your code here
+let twoDaysIncome = tasks
+  .map(task => task.duration / 60)
+  .filter(task => task >= 2)
+  .map(total => total * 60)
+  .reduce((a, b) => a + b, 0);
+console.log("Maartje earned " + twoDaysIncome + ".00 Euro on Monday and Tuesday!");
