b1a1765e150592d974446734be7564b7554c7121
kfth <nieuwleven33@gmail.com>
2019-01-06 12:51:46 +0100

    homework week1

--- Week1/homework/app.js
+++ Week1/homework/app.js
@@ -3,7 +3,8 @@
 {
   const bookTitles = [
     // Replace with your own book titles
-    'harry_potter_chamber_secrets'
+    'harry_potter_chamber_secrets',
+    'xxx'
   ];
 
   // Replace with your own code
