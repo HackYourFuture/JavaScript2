b2bd744bdab58b8a853e2411ced5542dff0fcb53
ammarvehbi <merakli.vehbi@gmail.com>
2019-01-06 12:51:32 +0100

    homework week1

--- Week1/homework/app.js
+++ Week1/homework/app.js
@@ -3,7 +3,8 @@
 {
   const bookTitles = [
     // Replace with your own book titles
-    'harry_potter_chamber_secrets'
+    'harry_potter_chamber_secrets',
+    'xxxxx'
   ];
 
   // Replace with your own code
