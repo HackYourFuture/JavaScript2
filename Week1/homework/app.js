53619b89ade00d363cd28a15ee63601ac71b9e52
ufukcalimli <ufk.calimli@gmail.com>
2019-01-06 12:51:36 +0100

    Homework week1

--- Week1/homework/app.js
+++ Week1/homework/app.js
@@ -3,7 +3,8 @@
 {
   const bookTitles = [
     // Replace with your own book titles
-    'harry_potter_chamber_secrets'
+    'harry_potter_chamber_secrets',
+    'lotr_return_the_king'
   ];
 
   // Replace with your own code
