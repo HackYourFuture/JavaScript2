4a3eb0444af3da93ae3c61fda31f4d316b9d901e
ahmetbibi <ahmetbibi@gmail.com>
2019-01-07 20:38:08 +0100

    I editted the homework week1

--- Week2/homework/maartjes_work.js
+++ Week2/homework/maartjes_work.js
@@ -3,60 +3,59 @@
 const monday = [
   {
     name: 'Write a summary HTML/CSS',
-    duration: 180
+    duration: 180,
   },
   {
     name: 'Some web development',
-    duration: 120
+    duration: 120,
   },
   {
     name: 'Fix homework for class10',
-    duration: 20
+    duration: 20,
   },
   {
     name: 'Talk to a lot of people',
-    duration: 200
-  }
+    duration: 200,
+  },
 ];
 
 const tuesday = [
   {
     name: 'Keep writing summary',
-    duration: 240
+    duration: 240,
   },
   {
     name: 'Some more web development',
-    duration: 180
+    duration: 180,
   },
   {
     name: 'Staring out the window',
-    duration: 10
+    duration: 10,
   },
   {
     name: 'Talk to a lot of people',
-    duration: 200
+    duration: 200,
   },
   {
     name: 'Look at application assignments new students',
-    duration: 40
-  }
+    duration: 40,
+  },
 ];
 
 const tasks = monday.concat(tuesday);
 
 // Add your code here
-// console.log(tasks);
-// let a = tasks[1].duration;
-let durationInHours = tasks.map(minute => minute.duration / 60);
 
-let moreThan2Hours = durationInHours.filter(newArray => newArray >= 2);
+const durationInHours = tasks.map(minute => minute.duration / 60);
+
+const moreThan2Hours = durationInHours.filter(newArray => newArray >= 2);
 
 // He earns €15.5 for one hour.
 
 const wageForOneHour = 15.5;
 
-let wageForTotalHours = moreThan2Hours.map(wage => wage * wageForOneHour);
+const wageForTotalHours = moreThan2Hours.map(wage => wage * wageForOneHour);
 
-let totalWage = Number(wageForTotalHours.reduce((total, num) => total + num).toFixed(2));
+const totalWage = Number(wageForTotalHours.reduce((total, num) => total + num).toFixed(2));
 
-console.log("Total wage that Maartjes earn: " + totalWage);
+console.log('Total wage that Maartjes earn: ' + totalWage);
