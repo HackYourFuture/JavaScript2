e7ed0338e65dde4833525f8ce0ce9cda14443c7e
alquinex <alquinex2010@hotmail.com>
2019-01-08 11:24:29 +0100

    homework week1

--- Week1/homework/app.js
+++ Week1/homework/app.js
@@ -3,7 +3,8 @@
 {
   const bookTitles = [
     // Replace with your own book titles
-    'harry_potter_chamber_secrets'
+    'harry_potter_chamber_secrets',
+    "Today's excercise"
   ];
 
   // Replace with your own code
