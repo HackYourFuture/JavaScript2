ecff2c1b8d565884a9fb7b22d18984b1ac988c3b
ahmetbibi <ahmetbibi@gmail.com>
2019-01-12 01:25:35 +0100

    I made some changes after reviewed

--- Week1/homework/app.js
+++ Week1/homework/app.js
@@ -3,16 +3,16 @@
 {
   // First part of homework. Getting elements from array.
 
-  // const bookTitles = ['animal_farm', 'nineteen_eighty_four', 'burmese_days', 'last_witness',
-  //   'les_miserables', 'burning', 'reckoning', 'murder_orient_express',
-  //   'man_brown_suit', 'ice_cold'];
+  const bookIds = ['animal_farm', 'nineteen_eighty_four', 'burmese_days', 'last_witness',
+    'les_miserables', 'burning', 'reckoning', 'murder_orient_express',
+    'man_brown_suit', 'ice_cold'];
 
-  // var myBookList = function () {
+  // function createBookList() {
   //   var ulTag = document.createElement('ul');
   //   document.body.appendChild(ulTag);
-  //   for (let i in bookTitles) {
+  //   for (let i in bookIds) {
   //     var liTag = document.createElement('li');
-  //     var liTagText = document.createTextNode(bookTitles[i]);
+  //     var liTagText = document.createTextNode(bookIds[i]);
   //     liTag.appendChild(liTagText);
   //     ulTag.appendChild(liTag);
   //   }
@@ -22,52 +22,62 @@
     animal_farm: {
       title: 'Animal Farm',
       language: 'English',
-      author: 'George Orwell'
+      author: 'George Orwell',
+      alt: 'This is the cover of Animal Farm book'
     },
     nineteen_eighty_four: {
       title: '1984',
       language: 'English',
-      author: 'George Orwell'
+      author: 'George Orwell',
+      alt: 'This is the cover of 1984 book'
     },
     burmese_days: {
       title: 'Burmese Days',
       language: 'English',
-      author: 'George Orwell'
+      author: 'George Orwell',
+      alt: 'This is the cover of Burmese Days book'
     },
     last_witness: {
       title: 'The Last Witness',
       language: 'English',
-      author: 'Glenn Meade'
+      author: 'Glenn Meade',
+      alt: 'This is the cover of The Last Witness book'
     },
     les_miserables: {
       title: 'Les Miserables',
       language: 'French',
-      author: 'Victor Hugo'
+      author: 'Victor Hugo',
+      alt: 'This is the cover of Les Miserables book'
     },
     burning: {
       title: 'The Burning',
       language: 'English',
-      author: 'Jane Casey'
+      author: 'Jane Casey',
+      alt: 'This is the cover of The Burning book'
     },
     reckoning: {
       title: 'The Reckoning',
       language: 'English',
-      author: 'Jane Casey'
+      author: 'Jane Casey',
+      alt: 'This is the cover of The Reckoning book'
     },
     murder_orient_express: {
       title: 'Murder on the Orient Express',
       language: 'English',
-      author: 'Agatha Christie'
+      author: 'Agatha Christie',
+      alt: 'This is the cover of Murder on the Orient Express book'
     },
     man_brown_suit: {
       title: 'The Man in the Brown Suit',
       language: 'English',
-      author: 'Agatha Christie'
+      author: 'Agatha Christie',
+      alt: 'This is the cover of The Man in the Brown Suit book'
     },
     ice_cold: {
       title: 'Ice Cold',
       language: 'English',
-      author: 'Glenn Meade'
+      author: 'Tess Gerritsen',
+      alt: 'This is the cover of Ice Cold book'
     }
   };
 
@@ -93,14 +103,14 @@
     const ulTag2 = document.createElement('ul');
     document.body.appendChild(ulTag2);
 
-    const keysOfBookInfo = Object.keys(bookInfo);
-    for (const i of keysOfBookInfo) {
+    for (const keys of bookIds) {
+      const book = bookInfo[keys];
       const liTag2 = document.createElement('li');
       const attribute = document.createAttribute('id');
-      attribute.value = i;
+      attribute.value = keys;
       liTag2.setAttributeNode(attribute);
       const headings = document.createElement('h2');
-      const headingsText = document.createTextNode(bookInfo[i].title);
+      const headingsText = document.createTextNode(book.title);
       headings.appendChild(headingsText);
 
       const aboutLanguage = document.createElement('p');
@@ -108,7 +118,7 @@
       const aboutLanguageSpanText = document.createTextNode('Language: ');
       aboutLanguageSpan.appendChild(aboutLanguageSpanText);
       aboutLanguage.appendChild(aboutLanguageSpan);
-      const aboutLanguageText = document.createTextNode(bookInfo[i].language);
+      const aboutLanguageText = document.createTextNode(book.language);
       aboutLanguage.appendChild(aboutLanguageText);
 
       const aboutAuthor = document.createElement('p');
@@ -116,28 +126,37 @@
       const aboutAuthorSpanText = document.createTextNode('Author: ');
       aboutAuthorSpan.appendChild(aboutAuthorSpanText);
       aboutAuthor.appendChild(aboutAuthorSpan);
-      const aboutAuthorText = document.createTextNode(bookInfo[i].author);
+      const aboutAuthorText = document.createTextNode(book.author);
       aboutAuthor.appendChild(aboutAuthorText);
 
       liTag2.appendChild(headings);
       liTag2.appendChild(aboutLanguage);
       liTag2.appendChild(aboutAuthor);
       ulTag2.appendChild(liTag2);
-
-      const idOfLi = document.getElementsByTagName('li')[i].getAttribute('id');
-      const keysOfBookCovers = Object.keys(bookCovers);
-      const valuesOfBookCovers = Object.values(bookCovers);
-
-      for (let j = 0; j < keysOfBookCovers.length; j++) {
-        if (idOfLi === keysOfBookCovers[j]) {
-          const imgTag = document.createElement('img');
-          imgTag.setAttribute('src', valuesOfBookCovers[j]);
-          imgTag.setAttribute('alt', bookInfo[i].title);
-          liTag2.appendChild(imgTag);
-        }
-      }
     }
   }
 
-  window.onload = createHtml;
+  function addImage() {
+
+    const keysOfBookCovers = Object.keys(bookCovers);
+
+    for (let keys of keysOfBookCovers) {
+      const book = bookCovers[keys];
+      const bookAlt = bookInfo[keys].alt;
+      let liTag2 = document.getElementsByTagName('li')[keys];
+
+      const imgTag = document.createElement('img');
+      imgTag.setAttribute('src', book);
+      imgTag.setAttribute('alt', bookAlt);
+      liTag2.appendChild(imgTag);
+
+    }
+  }
+
+  function main() {
+    createHtml();
+    addImage();
+  }
+
+  window.addEventListener('load', main);
 }
