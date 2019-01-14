e66ce1e9a5828f7e03edd7b4fbc110c05a4961d1
ammarvehbi <merakli.vehbi@gmail.com>
2019-01-14 17:10:17 +0100

    Homework Week2

--- Week2/homework/maartjes-work.js
+++ Week2/homework/maartjes-work.js
@@ -46,16 +46,29 @@ const maartjesTasks = monday.concat(tuesday);
 const maartjesHourlyRate = 20;
 
 function computeEarnings(tasks, hourlyRate) {
-  // Replace this comment and the next line with your code
-  console.log(tasks, hourlyRate);
+  const result =
+    // step 1.2.4
+    Math.floor(
+      100 *
+        tasks
+          // step 1.2.1
+          .map(x => x.duration / 60)
+          // step 1.2.2
+          .filter(x => x >= 2)
+          // step 1.2.3
+          .map(x => x * hourlyRate)
+          .reduce((accumulator, number) => accumulator + number)
+    ) / 100;
+  return result;
 }
 
 // eslint-disable-next-line no-unused-vars
 const earnings = computeEarnings(maartjesTasks, maartjesHourlyRate);
+console.log(earnings);
 
 // add code to convert `earnings` to a string rounded to two decimals (euro cents)
 
-console.log(`Maartje has earned €${'replace this string with the earnings rounded to euro cents'}`);
+console.log(`Maartje has earned €${earnings}`);
 
 // Do not change or remove anything below this line
 module.exports = {
--- Week2/homework/map-filter.js
+++ Week2/homework/map-filter.js
@@ -1,8 +1,8 @@
 'use strict';
 
 function doubleOddNumbers(numbers) {
-  // Replace this comment and the next line with your code
-  console.log(numbers);
+  // step 1.1
+  return numbers.filter(x => x % 2 !== 0).map(x => x * 2);
 }
 
 const myNumbers = [1, 2, 3, 4];
