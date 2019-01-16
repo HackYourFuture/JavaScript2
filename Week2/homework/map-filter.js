5b437b7bf27db8d094464c297dea204533c36517
YaseirAlkhwalda <yaseir.alkhwalda@gmail.com>
2019-01-16 01:27:45 +0100

    homework-week2

--- Week2/homework/map-filter.js
+++ Week2/homework/map-filter.js
@@ -1,8 +1,7 @@
 'use strict';
 
 function doubleOddNumbers(numbers) {
-  // Replace this comment and the next line with your code
-  console.log(numbers);
+  return numbers.filter(number => number % 2 !== 0).map(number => number * 2);
 }
 
 const myNumbers = [1, 2, 3, 4];
