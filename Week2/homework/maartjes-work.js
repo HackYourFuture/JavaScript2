b72af68a995d664aea78eb17ea2cab3ce9604ec4
Aya92syr <black.angel.35197@gmail.com>
2019-01-25 15:35:32 +0100

    Week2HomeWork

--- Week2/homework/maartjes-work.js
+++ Week2/homework/maartjes-work.js
@@ -46,16 +46,22 @@ const maartjesTasks = monday.concat(tuesday);
 const maartjesHourlyRate = 20;
 
 function computeEarnings(tasks, hourlyRate) {
-  // Replace this comment and the next line with your code
-  console.log(tasks, hourlyRate);
-}
+return tasks 
+.map( hour => hour.duration / 60)
+.filter(hour => hour > 2)
+.map (hour => hour * hourlyRate)
+.reduce ((x, y) => x + y, 0);
+ 
+ } 
+ //console.log(tasks, hourlyRate);
+
 
 // eslint-disable-next-line no-unused-vars
 const earnings = computeEarnings(maartjesTasks, maartjesHourlyRate);
 
 // add code to convert `earnings` to a string rounded to two decimals (euro cents)
 
-console.log(`Maartje has earned €${'replace this string with the earnings rounded to euro cents'}`);
+console.log(`Maartje has earned €${earnings.toFixed(2)}`);
 
 // Do not change or remove anything below this line
 module.exports = {
--- Week2/homework/map-filter.js
+++ Week2/homework/map-filter.js
@@ -1,14 +1,19 @@
 'use strict';
 
 function doubleOddNumbers(numbers) {
-  // Replace this comment and the next line with your code
-  console.log(numbers);
-}
+  const newNumbers = myNumbers.filter(myNumbers => myNumbers % 2 !==0 );
+  
+  
+   const doublenewNumbers = newNumbers.map(newNumbers => newNumbers * 2)
+   return doublenewNumbers;
+}  
+  
 
 const myNumbers = [1, 2, 3, 4];
 console.log(doubleOddNumbers(myNumbers));
 
 // Do not change or remove anything below this line
+
 module.exports = {
   myNumbers,
   doubleOddNumbers
