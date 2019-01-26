102d8a93a8f7ba70524bc3c72d4e4169aad03bdb
Jim Cramer <remarcmij@gmail.com>
2019-01-26 18:15:54 +0100

    Fix ESLint directives

--- Week3/homework/step2-3.js
+++ Week3/homework/step2-3.js
@@ -2,7 +2,7 @@
 
 // Use a 'for' loop
 function repeatStringNumTimesWithFor(str, num) {
-  // eslint-disable-next-line
+  // eslint-disable-next-line prefer-const
   let result = '';
 
   // Replace this comment and the next line with your code
@@ -15,7 +15,7 @@ console.log('for', repeatStringNumTimesWithFor('abc', 3));
 
 // Use a 'while' loop
 function repeatStringNumTimesWithWhile(str, num) {
-  // eslint-disable-next-line
+  // eslint-disable-next-line prefer-const
   let result = '';
 
   // Replace this comment and the next line with your code
@@ -28,7 +28,7 @@ console.log('while', repeatStringNumTimesWithWhile('abc', 3));
 
 // Use a 'do...while' loop
 function repeatStringNumTimesWithDoWhile(str, num) {
-  // eslint-disable-next-line
+  // eslint-disable-next-line prefer-const
   let result = '';
 
   // Replace this comment and the next line with your code
