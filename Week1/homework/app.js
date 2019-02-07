51a3a76207e6c3e9a733f66319f543a84b39a43e
Braian T <42869449+BraianT@users.noreply.github.com>
2019-02-07 10:36:21 -0500

    Dom Manipulation
    Javascript2 week1
    Books

--- Week1/homework/app.js
+++ Week1/homework/app.js
@@ -1,149 +1,147 @@
-'use strict';
-{
+"use strict";
 
+//1.1
+{
   const bookTitles = [
     // Replace with your own book titles
-    'a_tale_of_two_cities', 
-    'the_lord_of_the_rings', 
-    'the_little_prince', 
-    'the_da_vinci_code',
-    'the_hobbit',
-    'alice_adventures_in_wonderland',
-    'dream_of_the_redchamber', 
-    'and_then_there_were_none', 
-    'the_lion_the_witch_and_the_wardrobe', 
-    'the_adventures_of_pinocchio'
+    "a_tale_of_two_cities",
+    "the_lord_of_the_rings",
+    "the_little_prince",
+    "the_da_vinci_code",
+    "the_hobbit",
+    "alice_adventures_in_wonderland",
+    "dream_of_the_redchamber",
+    "and_then_there_were_none",
+    "the_lion_the_witch_and_the_wardrobe",
+    "the_adventures_of_pinocchio"
   ];
+  //console.log(bookTitles);
 
-  // Replace with your own code
+  //1.3 Make a function that generates a ul with li elements for each book ID in the array using a for loop.
+  const rootDiv = document.getElementById("root");
 
-  function makeList(arr) {
-
-    let ul = document.createElement('ul');
-    ul.setAttribute('id','bookTitles');
-    document.body.appendChild(ul)
+  function generateList(arr) {
+    let list = document.createElement("ul");
+    rootDiv.appendChild(list);
 
     for (let i = 0; i < arr.length; i++) {
-      let li = document.createElement('li');
-      li.setAttribute('id', i);
-      li.innerHTML = arr[i];
-      ul.appendChild(li);
-      
+      let listItems = document.createElement("li");
+      listItems.setAttribute("id", arr[i]);
+      listItems.innerText = arr[i];
+      list.appendChild(listItems);
+      //console.log(listItems);
     }
-  };
+  }
+  //generateList(bookTitles);
 
-  //makeList(bookTitles);
-
+  //1.4
   let bookInformation = {
-    'a_tale_of_two_cities' : {
-      titleOfTheBook: 'A Tale Of Two Cities',
-      authorOfTheBook: 'Charles Dickens',
-      languageOfTheBook: 'English',
+    a_tale_of_two_cities: {
+      title: "A Tale Of Two Cities",
+      author: "Charles Dickens",
+      language: "English"
     },
-    'the_lord_of_the_rings' : {
-      titleOfTheBook: 'The Lord Of The Rings',
-      authorOfTheBook: 'J. R. R. Tolkien',
-      languageOfTheBook: 'English',
-    }, 
-    'the_little_prince': {
-      titleOfTheBook: 'The Little Prince',
-      authorOfTheBook: 'Antoine de Saint-Exupéry',
-      languageOfTheBook: 'English',
-    }, 
-    'the_da_vinci_code' : {
-      titleOfTheBook: 'The Da Vinci Code',
-      authorOfTheBook: 'Dan Brown',
-      languageOfTheBook: 'English',
+    the_lord_of_the_rings: {
+      title: "The Lord Of The Rings",
+      author: "J. R. R. Tolkien",
+      language: "English"
     },
-    'the_hobbit' : {
-      titleOfTheBook: 'The Hobbit',
-      authorOfTheBook: 'J. R. R. Tolkien',
-      languageOfTheBook: 'English',
+    the_little_prince: {
+      title: "The Little Prince",
+      author: "Antoine de Saint-Exupéry",
+      language: "English"
     },
-    'alice_adventures_in_wonderland' : {
-      titleOfTheBook: 'Alice\'s Adventures In Wonderland',
-      authorOfTheBook: 'Lewis Carroll',
-      languageOfTheBook: 'English',
+    the_da_vinci_code: {
+      title: "The Da Vinci Code",
+      author: "Dan Brown",
+      language: "English"
     },
-    'dream_of_the_redchamber' : {
-      titleOfTheBook: 'Dream Of The Redchamber',
-      authorOfTheBook: 'Cao Xueqin',
-      languageOfTheBook: 'English',
-    }, 
-    'and_then_there_were_none' : {
-      titleOfTheBook: 'And Then There Were None',
-      authorOfTheBook: 'Agatha Christie',
-      languageOfTheBook: 'English',
-    }, 
-    'the_lion_the_witch_and_the_wardrobe' : {
-      titleOfTheBook: 'The Lion The Witch And The Wardrobe',
-      authorOfTheBook: 'C. S. Lewis',
-      languageOfTheBook: 'English',
-    }, 
-    'the_adventures_of_pinocchio' : {
-      titleOfTheBook: 'The Adventures Of Pinocchio',
-      authorOfTheBook: 'Carlo Collodi',
-      languageOfTheBook: 'English',
+    the_hobbit: {
+      title: "The Hobbit",
+      author: "J. R. R. Tolkien",
+      language: "English"
     },
-
-  }
-
-  
-  function showBooksInfo(obj) {
-
-    let listedBooks = document.createElement('ul');
-    document.body.appendChild(listedBooks)
-      for (let i=0; i<Object.keys(obj).length; i++){
-        let listedBooksLi = document.createElement('li');
-        listedBooksLi.setAttribute('id', Object.keys(obj)[i]);
-        listedBooks.appendChild(listedBooksLi);
-  
-        let titleOfTheBook = document.createElement('h1');
-        titleOfTheBook.innerHTML = obj[Object.keys(obj)[i]].titleOfTheBook;
-  
-        let authorOfTheBook = document.createElement('h2');
-        authorOfTheBook.innerHTML = obj[Object.keys(obj)[i]].authorOfTheBook;
-
-        let languageOfTheBook = document.createElement('h3');
-        languageOfTheBook.innerHTML = obj[Object.keys(obj)[i]].languageOfTheBook;
-  
-        listedBooksLi.appendChild(titleOfTheBook);
-        listedBooksLi.appendChild(authorOfTheBook);
-        listedBooksLi.appendChild(languageOfTheBook);
+    alice_adventures_in_wonderland: {
+      title: "Alice's Adventures In Wonderland",
+      author: "Lewis Carroll",
+      language: "English"
+    },
+    dream_of_the_redchamber: {
+      title: "Dream Of The Redchamber",
+      author: "Cao Xueqin",
+      language: "English"
+    },
+    and_then_there_were_none: {
+      title: "And Then There Were None",
+      author: "Agatha Christie",
+      language: "English"
+    },
+    the_lion_the_witch_and_the_wardrobe: {
+      title: "The Lion The Witch And The Wardrobe",
+      author: "C. S. Lewis",
+      language: "English"
+    },
+    the_adventures_of_pinocchio: {
+      title: "The Adventures Of Pinocchio",
+      author: "Carlo Collodi",
+      language: "English"
     }
   };
-  showBooksInfo(bookInformation);
 
-  let coverPic = {
-    a_tale_of_two_cities : "./img/two_cities.png", 
-    the_lord_of_the_rings : "./img/The_Lord.png", 
-    the_little_prince : "./img/Litte_Prince.png", 
-    the_da_vinci_code : "./img/DaVinciCode.jpg",
-    the_hobbit : "./img/Thehobbit.png",
-    alice_adventures_in_wonderland : "./img/Alice_Adventures.jpg",
-    dream_of_the_redchamber : "./img/Dream_Chamber.jpg", 
-    and_then_there_were_none : "./img/There_were_None.jpg", 
-    the_lion_the_witch_and_the_wardrobe : "./img/Lion_Witch_Wardobe.jpg", 
-    the_adventures_of_pinocchio : "./img/Pinocchio.jpg",
-  }
+  function generateListModification(obj) {
+    let list = document.createElement("ul");
+    rootDiv.appendChild(list);
 
+    for (let i = 0; i < Object.keys(obj).length; i++) {
+      let listItem = document.createElement("li");
+      listItem.setAttribute("id", Object.keys(obj)[i]);
+      list.appendChild(listItem);
 
+      let listTitle = document.createElement("h2");
+      listTitle.innerText = "Title: " + obj[Object.keys(obj)[i]].title;
+      list.appendChild(listTitle);
 
-  let test = Object.keys(obj)
+      let listAuthor = document.createElement("h3");
+      listAuthor.innerText = "Author: " + obj[Object.keys(obj)[i]].author;
+      list.appendChild(listAuthor);
 
-  console.log(test);
+      let listLanguage = document.createElement("h4");
+      listLanguage.innerText = "Language: " + obj[Object.keys(obj)[i]].language;
+      list.appendChild(listLanguage);
 
-  
-  function viewImages(obj){
-//   console.log(obj);
-  for (let i = 0; i < Object.keys(obj).length; i++){
-    let li = document.getElementById(Object.keys(obj)[i]);
-    let img = document.createElement('img');
-    let key = Object.keys(obj)[i];
-    img.setAttribute("src", obj[key]);
-    img.setAttribute("alt", "books cover");
-    li.appendChild(img);
+      let sepLine = document.createElement("hr");
+      listLanguage.appendChild(sepLine);
+      //console.log(listItem);
+      //listLanguage.innerText = JSON.stringify(obj[Object.keys(obj)[i]]);
     }
   }
-viewImages(coverPic);
+  generateListModification(bookInformation);
+
+  //1.7
+  let bookCovers = {
+    a_tale_of_two_cities: "./img/two_cities.png",
+    the_lord_of_the_rings: "./img/The_Lord.png",
+    the_little_prince: "./img/Litte_Prince.png",
+    the_da_vinci_code: "./img/DaVinciCode.jpg",
+    the_hobbit: "./img/Thehobbit.png",
+    alice_adventures_in_wonderland: "./img/Alice_Adventures.jpg",
+    dream_of_the_redchamber: "./img/Dream_Chamber.jpg",
+    and_then_there_were_none: "./img/There_were_None.jpg",
+    the_lion_the_witch_and_the_wardrobe: "./img/Lion_Witch_Wardobe.jpg",
+    the_adventures_of_pinocchio: "./img/Pinocchio.jpg"
+  };
+
+  function viewImages(obj) {
+    for (let i = 0; i < Object.keys(obj).length; i++) {
+      let test = document.getElementById(Object.keys(obj)[i]);
+      let picture = document.createElement("img");
+      picture.setAttribute("src", obj[Object.keys(obj)[i]]);
+      picture.setAttribute("alt", "books cover");
+      picture.setAttribute("class", "bookCovers");
+
+      test.appendChild(picture);
+      //console.log("Test: " + picture);
+    }
+  }
+  viewImages(bookCovers);
 }
