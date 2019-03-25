1b5eee43ba7aaaf2a0cefaf2ab960d40a217b021
WadeeaKiwan <wadeea.kiwan@hotmail.com>
2019-03-25 19:18:09 +0100

    JS2 Homework Week2

--- Week2/homework/maartjes-work.js
+++ Week2/homework/maartjes-work.js
@@ -46,8 +46,12 @@ const maartjesTasks = monday.concat(tuesday);
 const maartjesHourlyRate = 20;
 
 function computeEarnings(tasks, hourlyRate) {
-  // Replace this comment and the next line with your code
-  console.log(tasks, hourlyRate);
+  const totalEarnings = tasks
+    .map(task => task.duration / 60)
+    .filter(taskHour => taskHour >= 2)
+    .map(taskHour => taskHour * hourlyRate)
+    .reduce((earnings, taskHourlyRate) => earnings + taskHourlyRate, 0);
+  return totalEarnings;
 }
 
 // eslint-disable-next-line no-unused-vars
@@ -55,7 +59,7 @@ const earnings = computeEarnings(maartjesTasks, maartjesHourlyRate);
 
 // add code to convert `earnings` to a string rounded to two decimals (euro cents)
 
-console.log(`Maartje has earned €${'replace this string with the earnings rounded to euro cents'}`);
+console.log(`Maartje has earned €${earnings}`);
 
 // Do not change or remove anything below this line
 module.exports = {
--- Week2/homework/map-filter.js
+++ Week2/homework/map-filter.js
@@ -1,8 +1,8 @@
 'use strict';
 
 function doubleOddNumbers(numbers) {
-  // Replace this comment and the next line with your code
-  console.log(numbers);
+  const doubledOddNumbers = numbers.filter(number => number % 2 !== 0).map(number => number * 2);
+  return doubledOddNumbers;
 }
 
 const myNumbers = [1, 2, 3, 4];
