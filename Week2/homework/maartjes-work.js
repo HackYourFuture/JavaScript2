3a2815c283e15372cd180cb4e9828c93520fa517
Akif <akbu2709@gmail.com>
2019-03-26 14:24:35 +0100

    minor revision

--- Week2/homework/maartjes-work.js
+++ Week2/homework/maartjes-work.js
@@ -53,21 +53,16 @@ function computeEarnings(tasks, hourlyRate) {
 
   const lessThanTwoHour = duration.filter(elem => elem.duration >= 2);
 
-  return lessThanTwoHour.reduce((acc, elem) => {
-    acc += elem.duration * hourlyRate;
-    return acc;
-  }, 0);
+  return lessThanTwoHour.reduce((acc, elem) => (acc += elem.duration * hourlyRate), 0);
 }
 console.log(computeEarnings(maartjesTasks, maartjesHourlyRate));
 
 const earnings = computeEarnings(maartjesTasks, maartjesHourlyRate);
 
 const formattedEuro = earnings.toFixed(2);
-console.log(formattedEuro);
-
-// add code to convert `earnings` to a string rounded to two decimals (euro cents)
 
 console.log(`Maartje has earned €${formattedEuro}`);
+
 module.exports = {
   maartjesTasks,
   maartjesHourlyRate,
--- Week2/homework/map-filter.js
+++ Week2/homework/map-filter.js
@@ -1,14 +1,5 @@
 'use strict';
 
-// function doubleOddNumbers(arr) {
-//   const chooseOdd = arr.filter(number => number % 2 === 1);
-//   const double = chooseOdd.map(number => number * 2);
-//   return double;
-// }
-
-// const myNumbers = [1, 2, 3, 4];
-// console.log(doubleOddNumbers(myNumbers));
-
 function doubleOddNumbers(numbers) {
   const chooseOdds = numbers.filter(number => number % 2 === 1);
   const doubles = chooseOdds.map(number => number * 2);
@@ -18,7 +9,6 @@ function doubleOddNumbers(numbers) {
 const myNumbers = [1, 2, 3, 4];
 console.log(doubleOddNumbers(myNumbers));
 
-// Do not change or remove anything below this line
 module.exports = {
   myNumbers,
   doubleOddNumbers,
