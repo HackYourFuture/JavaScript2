2e2ab639ff0da5dcb58a47790bc81df0cd787bc9
Loulou Shehadat <louloushehadat@Loulous-MacBook-Air.local>
2019-03-27 19:02:20 +0100

    javascript2week2

---
+++ Week2/homework/kalb3/map-filter (2).js
@@ -0,0 +1,16 @@
+'use strict';
+/* cSpell:disable */
+
+function doubleOddNumbers(numbers) {
+  numbers = numbers.filter(number => number % 2 !== 0).map(number => number * 2);
+  return numbers;
+}
+
+const myNumbers = [1, 2, 3, 4];
+console.log(doubleOddNumbers(myNumbers));
+
+
+module.exports = {
+  myNumbers,
+  doubleOddNumbers,
+};
