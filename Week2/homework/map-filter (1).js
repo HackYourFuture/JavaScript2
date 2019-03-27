3d6733b558d1270400e4c4f58b80e4d09113da50
Alaashhadat <loulousheha5@gmail.com>
2019-03-27 18:33:25 +0100

    Update map-filter (1).js

--- Week2/homework/map-filter (1).js
+++ Week2/homework/map-filter (1).js
@@ -9,7 +9,7 @@ function doubleOddNumbers(numbers) {
 const myNumbers = [1, 2, 3, 4];
 console.log(doubleOddNumbers(myNumbers));
 
-// Do not change or remove anything below this line
+
 module.exports = {
   myNumbers,
   doubleOddNumbers,
