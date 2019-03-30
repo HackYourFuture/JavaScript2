f383e8ee6212ba1f722c3fdc167e5da68f054536
ghifo <41265609+ghifo@users.noreply.github.com>
2019-03-30 09:45:38 +0100

    Update map-filter.js

--- ghufran/map-filter.js
+++ ghufran/map-filter.js
@@ -2,10 +2,14 @@
 
 function doubleOddNumbers(numbers) {
   const newNumbers = numbers.filter(number => number % 2 !== 0); // filter the odd nrs
-  const doubleNumbers = newNumbers.map(number => number * 2); // double the odd nrs
-  return doubleNumbers;
+  numbers = newNumbers.map(number => number * 2); // double the odd nrs
+  console.log(numbers);
 }
 
 const myNumbers = [1, 2, 3, 4];
 console.log(doubleOddNumbers(myNumbers)); // [2, 6] the double of [1,3]
 
+module.exports = {
+  myNumbers,
+  doubleOddNumbers,
+};
