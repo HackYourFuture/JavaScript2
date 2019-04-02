7d83f7f7846354dca1957a99d320deab698cae1d
mohammadnaefaldarwesh <mohammad_86dr@hotmail.com>
2019-04-02 21:13:56 +0200

    update homework

--- Week3/homework/step2-2.js
+++ Week3/homework/step2-2.js
@@ -5,16 +5,50 @@ function threeFive(startIndex, stopIndex, threeCallback, fiveCallback) {
   for (let counter = startIndex; counter <= stopIndex; counter++) {
     numbers.push(counter);
   }
-  const result = numbers.filter(number => threeCallback(number) || fiveCallback(number));
-  return console.log(result);
+  const three = threeCallback(numbers);
+  const five = fiveCallback(numbers);
+  const both = fiveCallback(threeCallback(numbers));
+
+  return console.log(three, five, both);
+  // const three = threeCallback(numbers) || fiveCallback(threeCallback(numbers));
+  // const five = fiveCallback(numbers);
+  // const three = threeCallback(numbers);
+  // const five = fiveCallback(numbers);
+  // const klab = fiveCallback(threeCallback(numbers));
+  // const klab2 = [threeCallback(fiveCallback(numbers))];
+
+  // console.log(three, five);
+  // const threefive2 = three || five;
+  // console.log(three, five, threefive2);
+  // const sayThreeFive = three.concat(five).sort();
+  // console.log(sayThreeFive);
+  // const result = [];
+  // for (let i = 0; i < sayThreeFive.length; i++) {
+  //   if (result.indexOf(sayThreeFive[i]) === -1) {
+  //     result.push(sayThreeFive[i]);
+  // const result = threeCallback(numbers);
+  // const result2 = fiveCallback(numbers);
+  // const result3 = result.concat(result2);
+  // console.log(result3);
+  // const result4 = threeCallback(result3) && fiveCallback(result3);
+  // console.log(result, result2, result4);
+  // }
+  // }
+  // console.log(result);
 }
+//   const result = threeCallback(numbers);
+//   const result2 = fiveCallback(numbers);
+//   const result3 = threeCallback(numbers) || fiveCallback(numbers);
+
+//   console.log(result, result2, result3);
+// }
 
 function sayThree(number) {
-  return number % 3 === 0;
+  return number.filter(num => num % 3 === 0);
 }
 
 function sayFive(number) {
-  return number % 5 === 0;
+  return number.filter(num => num % 5 === 0);
 }
 
 threeFive(10, 15, sayThree, sayFive);
