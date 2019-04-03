3abae3a3c875a830c32838b4bf4daf6c0c5ed13e
WadeeaKiwan <wadeea.kiwan@hotmail.com>
2019-04-03 19:45:05 +0200

    update step 2-2

--- Week3/homework/step2-2.js
+++ Week3/homework/step2-2.js
@@ -3,25 +3,25 @@
 function threeFive(startIndex, stopIndex, threeCallback, fiveCallback) {
   const numbers = [];
   for (let number = startIndex; number <= stopIndex; number++) {
-    // numbers.push(number);
+    numbers.push(number);
     if (number % 3 === 0 && number % 5 === 0) {
-      numbers.push(threeCallback(number));
-      numbers.push(fiveCallback(number));
+      threeCallback(number);
+      fiveCallback(number);
     } else if (number % 3 === 0) {
-      numbers.push(threeCallback(number));
+      threeCallback(number);
     } else if (number % 5 === 0) {
-      numbers.push(fiveCallback(number));
+      fiveCallback(number);
     }
   }
-  return numbers;
+  console.log(numbers);
 }
 
 function sayThree(number) {
-  return `${number} is divisible by 3`;
+  console.log(`${number} is divisible by 3`);
 }
 
 function sayFive(number) {
-  return `${number} is divisible by 5`;
+  console.log(`${number} is divisible by 5`);
 }
 
 threeFive(10, 15, sayThree, sayFive);
