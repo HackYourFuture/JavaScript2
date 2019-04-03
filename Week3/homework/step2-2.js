42ca7e9b46f77191da3975f12b54423879718589
Alaashhadat <loulousheha5@gmail.com>
2019-04-03 13:27:14 +0200

    Update step2-2.js

--- Week3/homework/step2-2.js
+++ Week3/homework/step2-2.js
@@ -2,19 +2,22 @@
 
 function threeFive(startIndex, stopIndex, threeCallback, fiveCallback) {
   const numbers = [];
-
-  // Replace this comment and the next line with your code
-  console.log(startIndex, stopIndex, threeCallback, fiveCallback, numbers);
+  for (let num = startIndex; num <= stopIndex; num++) {
+    numbers.push(num);
+    if (num % 3 === 0) {
+      threeCallback(num);
+    }
+    if (num % 5 === 0) {
+      fiveCallback(num);
+    }
+  }
 }
-
 function sayThree(number) {
-  // Replace this comment and the next line with your code
-  console.log(number);
+  return number % 3 === 0;
 }
 
 function sayFive(number) {
-  // Replace this comment and the next line with your code
-  console.log(number);
+  return number % 5 === 0;
 }
 
 threeFive(10, 15, sayThree, sayFive);
