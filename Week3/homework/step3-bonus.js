7855920e549f12d8b1856745c99d90e744b127a0
WadeeaKiwan <wadeea.kiwan@hotmail.com>
2019-04-03 19:57:53 +0200

    update step3-bonus

--- Week3/homework/step3-bonus.js
+++ Week3/homework/step3-bonus.js
@@ -3,11 +3,6 @@
 const values = ['a', 'b', 'c', 'd', 'a', 'e', 'f', 'c'];
 
 function makeUnique(arr) {
-  // return arrArg => {
-  //   arrArg.filter((elem, pos, arr) => {
-  //     return arr.indexOf(elem) === pos;
-  //   });
-  // };
   const arrUnique = [...new Set(arr.map(elem => elem))];
   return arrUnique;
 }
