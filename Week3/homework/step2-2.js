f007933549b540a153deecfe929259a5cd65d8e2
unknown <muhannadjudah@gmail.com>
2019-04-03 20:42:04 +0200

    fix the file step2.2.js

--- Week3/homework/step2-2.js
+++ Week3/homework/step2-2.js
@@ -2,27 +2,30 @@
 
 function threeFive(startIndex, stopIndex, threeCallback, fiveCallback) {
   const numbers = [];
-  const result = [];
+  //const result = [];
 
   for (startIndex; startIndex <= stopIndex; startIndex++) {
     numbers.push(startIndex);
   }
   for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] % 3 === 0) {
-      result.push(threeCallback(numbers[i]));
+    if (numbers[i] % 3 === 0 && numbers[i] % 5 === 0) {
+      threeCallback(numbers[i]);
+      fiveCallback(numbers[i]);
+    } else if (numbers[i] % 3 === 0) {
+      threeCallback(numbers[i]);
     } else if (numbers[i] % 5 === 0) {
-      result.push(fiveCallback(numbers[i]));
+      fiveCallback(numbers[i]);
     }
   }
-  console.log(result);
+  console.log(numbers);
 }
 
 function sayThree(number) {
-  return number;
+  return console.log(number + ' is divisible by 3');
 }
 
 function sayFive(number) {
-  return number;
+  return console.log(number + ' is divisible by 5');
 }
 
 threeFive(10, 15, sayThree, sayFive);
