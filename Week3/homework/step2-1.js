695da8dc671647627a291b082acb712f0e83be8d
ghifo <41265609+ghifo@users.noreply.github.com>
2019-04-06 19:32:30 +0200

    Update step2-1.js

--- Week3/homework/step2-1.js
+++ Week3/homework/step2-1.js
@@ -1,16 +1,14 @@
 'use strict';
 
 function foo(func) {
-  setTimeout(function() {
-    func(); // it delays the applying of another func when it is called
-  }, 2000);
+    func(); 
 }
 
 function bar() {
   console.log('Hello, I am bar!');
 }
 
-foo(bar); // after 2 seconds it is consoled
+foo(bar);
 
 // Do not change or remove anything below this line
 module.exports = foo;
