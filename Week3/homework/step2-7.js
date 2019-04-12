69c9ace6b6a95611737abdeb73d1e7c5e9a8c810
Akif <akbu2709@gmail.com>
2019-04-12 22:51:48 +0200

    adds more explanation

--- Week3/homework/step2-7.js
+++ Week3/homework/step2-7.js
@@ -25,6 +25,11 @@ console.log(y);
 as a argument, it is approached with its reference/address. And when we change object in  function, we are changing 
 the referenced/original object. Dont ask why, it is Javascript : )
 
+if we look the codes above, at the first example f1(x) function takes the value of x and using this value and it is 9. This function 
+doesnt take x itself and doesnt give it new value. So after function we see the value of x is still 9.
+But the second function f2(y) takes a object as an argument and rendering this object itself because of approaching with its reference. 
+Reference is different than to take a variables value only. Reference is a bridge which makes the original value changeable. So at the end 
+of function f2(y), objects value is changing and being 10.
 
 
 
