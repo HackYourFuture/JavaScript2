97e625eaac159b7c57b3acdef3daa5004420302f
Robbert Segers <robbert.segers@klarrio.com>
2019-04-27 14:39:16 +0200

    add week 1 exercise

---
+++ Week1/exercise/w1/index.js
@@ -0,0 +1,30 @@
+console.log('Hack your future, right?!');
+
+const header = document.querySelector('h1');
+console.log('header: ', header);
+
+// EXERCISE:
+// select "change image" button (querySelector)
+// add click event listener
+// execute changeImage() function on click event
+// define changeImage() function
+// in this function:
+//    get image element
+//    get imageInput element
+//    log input value
+//    set image src to imageInput element value
+
+// ====================================== //
+
+// BONUS:
+// select "add todo" button (querySelector)
+// add click event listener
+// execute addTodo() function on click event
+// define addTodo() function
+// in this function:
+//    get todoList element
+//    get todoInput element
+//    log todoInput element value
+//    create <li> element
+//    set <li> element innerHtml to todoInput value
+//    add <li> element to todoList
