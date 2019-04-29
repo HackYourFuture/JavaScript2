75b3ee30dd5a4180c42d6c8e8fdcc73eadca1770
Fatih Tutar <47762263+fatihtutar@users.noreply.github.com>
2019-04-29 15:46:13 +0200

    Update index.js

--- Week1/exercise/w1/index.js
+++ Week1/exercise/w1/index.js
@@ -3,6 +3,31 @@ console.log('Hack your future, right?!');
 const header = document.querySelector('h1');
 console.log('header: ', header);
 
+// select "change image" button (querySelector)
+const changeImageButton = document.querySelector("#btn-changeImage");
+console.log("changeImageButton",changeImageButton);
+
+// define changeImage() function
+const changeImage= () => {
+
+    //    get image element
+    const image = document.getElementById("imageToChange");
+    console.log("image:" + image);
+
+    //    get imageInput element
+    const imageInput = document.getElementById("imageInput");
+
+    //    set image src to imageInput element value
+    image.src = imageInput.value;
+
+    //    log input value
+    console.log("imageInput:" + imageInput.value);
+}
+
+// add click event listener
+// execute changeImage() function on click event
+changeImageButton.addEventListener("click", changeImage)
+
 // EXERCISE:
 // select "change image" button (querySelector)
 // add click event listener
