26128f237b8629e655e663a3d349f881ba121e58
Fatih Tutar <47762263+fatihtutar@users.noreply.github.com>
2019-04-30 15:09:03 +0200

    Update index.js

--- Week1/exercise/w1/index.js
+++ Week1/exercise/w1/index.js
@@ -43,6 +43,39 @@ changeImageButton.addEventListener("click", changeImage)
 
 // BONUS:
 // select "add todo" button (querySelector)
+const addTodoButton = document.querySelector("#btn-addTodo");
+//console.log("addTodoButton",addTodoButton);
+
+// define addTodo() function
+// in this function:
+const addTodo = () =>{
+
+    //    get todoList element
+    const todoList = document.getElementById("todoList");
+    //console.log("todoList:" + todoList); 
+
+    //    get todoInput element
+    const todoInput = document.getElementById("todoInput");
+
+    //    log todoInput element value
+    console.log("todoInput:" + todoInput.value); 
+    
+    //    create <li> element
+    const creatLi = document.createElement('li');
+
+    //    set <li> element innerHtml to todoInput value
+    creatLi.innerHTML = todoInput.value;
+    //console.log(creatLi);
+
+    //    add <li> element to todoList
+    todoList.appendChild(creatLi);
+};
+
+// add click event listener
+// execute addTodo() function on click event
+addTodoButton.addEventListener("click", addTodo);
+
+// select "add todo" button (querySelector)
 // add click event listener
 // execute addTodo() function on click event
 // define addTodo() function
