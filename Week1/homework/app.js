cae834d8b5690ae98ac9dbd5e9df900448dfac05
berry <48694358+berrytu@users.noreply.github.com>
2019-05-03 00:21:04 +0200

    Update app.js

--- Week1/homework/app.js
+++ Week1/homework/app.js
@@ -3,7 +3,57 @@
 {
   const bookTitles = [
     // Replace with your own book titles
-    'harry_potter_chamber_secrets',
+    'lisa_halliday_asymmetry',{
+      title : 'Asymmetry',
+      author : 'Lisa Halliday',
+      language : 'English'
+    },
+    'rebecca_makkai_the_great_believers',{
+      title : 'the great believers ',
+      author : 'rebecca makkai',
+      language : 'English'
+    },
+    'leila_slimani_the_perfect_nanny',{
+      title : 'the perfect nanny',
+      author : 'leila slimani',
+      language : 'English'
+    },
+    'tommy_orange_there_there',{
+      title : 'there there',
+      author : 'tommy orange',
+      language : 'English'
+    },
+    'esi_edugyan_washington_black',{
+      title : 'washington black' ,
+      author : 'esi edugyan',
+      language : 'English'    
+    },
+    'shane_baoer_american_prison',{
+      title : 'american prison',
+      author : 'shane bauer',
+      language : 'English'
+    },
+    'tara_westover_educated',{
+      title : 'educated' ,
+      author : 'tara westover',
+      language : 'English'
+    },
+    'david_w.blight_frederick_douglass',{
+      title : 'frederick douglass' ,
+      author : 'david w.blight',
+      language : 'English'  
+    },
+    'michael_pollan_how_to_change_your_mind',{
+      title : 'how to change your mind',
+      author : 'michael pollan',
+      language : 'English'
+    },
+    'lisa_brennan_jobs_small_fry',{
+      title : 'small fry' ,
+      author : 'lisa brennan jobs',
+      language : 'English'
+    },
+    
   ];
 
   // Replace with your own code
