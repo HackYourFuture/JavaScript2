628df332a07d0de1fc7833636daa007c0d6e295f
Yousef <46559177+yousefnk@users.noreply.github.com>
2019-05-03 21:38:59 +0200

    Update index.js

--- Week1/exercise/w1/index.js
+++ Week1/exercise/w1/index.js
@@ -14,6 +14,15 @@ console.log('header: ', header);
 //    log input value
 //    set image src to imageInput element value
 
+const changeImageButton = document.querySelector('#btn-changeImage')
+const changeImage = () => {
+    const image = document.getElementById("imageToChange");
+    const imageInput = document.getElementById("imageInput");
+    image.src = 'https://www.nieuwsbladtransport.nl/wp-content/uploads/2019/03/big-data-cyber-security-hack.jpg';
+}
+
+changeImageButton.addEventListener("click", changeImage)
+
 // ====================================== //
 
 // BONUS:
@@ -28,3 +37,15 @@ console.log('header: ', header);
 //    create <li> element
 //    set <li> element innerHtml to todoInput value
 //    add <li> element to todoList
+
+const addTodoButton = document.querySelector("#btn-addTodo");
+const addTodo = () => {
+
+    const todoList = document.getElementById("todoList");
+    const todoInput = document.getElementById("todoInput");
+    const creatLi = document.createElement('li');
+    creatLi.innerHTML = todoInput.value;
+    todoList.appendChild(creatLi);
+};
+
+addTodoButton.addEventListener("click", addTodo);
