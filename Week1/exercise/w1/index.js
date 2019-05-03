35f49563f7f55ddd87c77947a9a9407dcb1c1416
whiteststar <48384895+whiteststar@users.noreply.github.com>
2019-05-03 21:56:51 +0200

    exercise 1 and 2 is done
    but i still don't get git bash. not even close...

--- Week1/exercise/w1/index.js
+++ Week1/exercise/w1/index.js
@@ -3,6 +3,42 @@ console.log('Hack your future, right?!');
 const header = document.querySelector('h1');
 console.log('header: ', header);
 
+// document.querySelector('button id="btn-changeImage"');
+//exercise 1:
+const botton1 = document.getElementById("btn-changeImage")
+
+botton1.addEventListener('click', function() {
+    console.log('it is selected');
+    }
+)
+
+const changeIt = () => {
+    const old = document.getElementById("imageToChange");
+    console.log(old);
+    const newurl = document.getElementById("imageInput").value;
+    console.log(newurl);
+    old.src = newurl;
+}
+
+botton1.addEventListener('click' , changeIt)
+
+// Exercise 2:
+
+const botton2 = document.querySelector('#btn-addTodo');
+
+const addtodo = () => {
+    var todoList = document.getElementById('todoList');
+    const todoInput = document.querySelector('#todoInput');
+    console.log(todoInput.value);   
+    var todoElement = document.createElement('li');
+    todoElement.innerHTML = todoInput.value;
+    todoList.appendChild(todoElement);
+    todoInput.value = ''; //after clicking' the text input should disappear, i think...
+}
+
+botton2.addEventListener('click' , addtodo)
+
+
 // EXERCISE:
 // select "change image" button (querySelector)
 // add click event listener
@@ -13,8 +49,6 @@ console.log('header: ', header);
 //    get imageInput element
 //    log input value
 //    set image src to imageInput element value
-
-// ====================================== //
 
 // BONUS:
 // select "add todo" button (querySelector)
@@ -28,3 +62,4 @@ console.log('header: ', header);
 //    create <li> element
 //    set <li> element innerHtml to todoInput value
 //    add <li> element to todoList
+// ====================================== //
