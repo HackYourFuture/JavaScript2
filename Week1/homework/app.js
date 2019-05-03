68ed1e084f0028a31edd4073253a3e2d7fca0a4d
whiteststar <48384895+whiteststar@users.noreply.github.com>
2019-05-04 00:34:22 +0200

    doesnt work, stucked at ul-li

--- Week1/homework/app.js
+++ Week1/homework/app.js
@@ -3,9 +3,31 @@
 {
   const bookTitles = [
     // Replace with your own book titles
-    'harry_potter_chamber_secrets',
+    'the_great_gatsby',
+    'to_kill_a_mockingbird', 
+    'harry_potter_and_the_sorcerers_stone', 
+    '1984', 
+    'the_catcher_in_the_rye', 
+    'the_hobbit', 
+    'fahrenheit_451', 
+    'pride_and_prejudice', 
+    'brave_new_world', 
+    'a_wrinkle_in_time', 
   ];
 
-  // Replace with your own code
-  console.log(bookTitles);
+  
+  var listContainer = document.createElement('div');
+  document.getElementsByTagName('body')[0].appendChild(listContainer);
+
+  var booklist = document.createElement('ul');
+
+  
+  var itemCount = bookTitles.length;
+  for (var i = 0; i < itemCount; ++i) {
+    var listItem = document.createElement('li');
+    listItem.innerText = bookTitles[i];
+    booklist.appendchild(listItem);
+  }
+ listContainer.appendChild(booklist);  
+
 }
