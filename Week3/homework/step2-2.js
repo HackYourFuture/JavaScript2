df402ecc6f74a510a2988c214178cddeae1d6054
ghifo <41265609+ghifo@users.noreply.github.com>
2019-05-11 23:58:23 +0200

    Update step2-2.js

--- Week3/homework/step2-2.js
+++ Week3/homework/step2-2.js
@@ -1,29 +1,38 @@
-'use strict';
+('use strict');
 
 function threeFive(startIndex, stopIndex, threeCallback, fiveCallback) {
   const numbers = [];
 
   for (let i = startIndex; i <= stopIndex; i++) {
-    numbers.push(i);
+    numbers.push(i); // push all nrs between the startIndex & the stopIndex
     if (i % 3 === 0 && i % 5 === 0) {
-      threeCallback(i), fiveCallback(i);
+      threeCallback(i);
+      fiveCallback(i);
     } else if (i % 3 === 0) {
       threeCallback(i);
     } else if (i % 5 === 0) {
       fiveCallback(i);
     }
   }
-  return numbers;
+  return numbers; // returns all the nrs between the startIndex & the stopIndex
 }
 
 function sayThree(number) {
   // Replace this comment and the next line with your code
-  console.log(number + ' can be divided by 3');
+  console.log(number + ' can be divided by 3'); // adds some text to show how the threeCallback func works
 }
 
 function sayFive(number) {
   // Replace this comment and the next line with your code
-  console.log(number + ' can be divided by 5');
+  console.log(number + ' can be divided by 5'); // adds some text to show how the fiveCallback func works
 }
 
 threeFive(10, 15, sayThree, sayFive);
+// it applies first the threeCallback func then the fiveCallback func
+// 10 can be divided by 5
+// 12 can be divided by 3
+// 15 can be divided by 3
+// 15 can be divided by 5
+
+// Do not change or remove anything below this line
+module.exports = threeFive;
