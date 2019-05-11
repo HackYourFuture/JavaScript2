c454439811c78ff6740ff68e274c7d466a15e2d8
ghifo <41265609+ghifo@users.noreply.github.com>
2019-05-11 13:16:02 +0200

    Update app.js

--- homework_week1/homework_week1/app.js
+++ homework_week1/homework_week1/app.js
@@ -69,50 +69,64 @@
     },
   };
 
-  const booksImg = {
-    pride_and_prejudice: './img/pride_and_prejudice.jpg',
-    robinson_crusoe: './img/robinson_crusoe.jpg',
-    the_old_man_and_the_sea: './img/the_old_man_and_the_sea.jpg',
-    thirteen_short_stories: './img/thirteen_short_stories.jpg',
-    narnia: './img/narnia.jpg',
-    as_you_like_it: './img/as_you_like_it.jpg',
-    hamlet: './img/hamlet.jpg',
-    pinocchio: './img/pinocchio.jpg',
-    julius_caesar: './img/julius_caesar.jpg',
-    romeo_and_juliet: './img/romeo_and_juliet.jpg',
+  const cover = {
+    pride_and_prejudice: 'https://images.penguinrandomhouse.com/cover/9780307950901',
+    robinson_crusoe: 'https://images.gr-assets.com/books/1505055646l/832554.jpg',
+    the_old_man_and_the_sea:
+      'https://images-na.ssl-images-amazon.com/images/I/411pakPjvdL._SX326_BO1,204,203,200_.jpg',
+    thirteen_short_stories:
+      'https://images-na.ssl-images-amazon.com/images/I/51uCL6Yc%2BGL._SX333_BO1,204,203,200_.jpg',
+    narnia:
+      'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1449868701i/11127._UY500_SS500_.jpg',
+    as_you_like_it:
+      'https://i.pinimg.com/236x/3b/1a/de/3b1adec055539160278d0bd1658cd733--as-you-like-it-shakespeare-festival.jpg',
+    hamlet:
+      'https://images-na.ssl-images-amazon.com/images/I/51VQaOw9dKL._SX331_BO1,204,203,200_.jpg',
+    pinocchio: 'https://pictures.abebooks.com/isbn/9781402745812-us.jpg',
+    julius_caesar: 'https://images-eu.ssl-images-amazon.com/images/I/51cgd89I9qL.jpg',
+    romeo_and_juliet:
+      'https://images-na.ssl-images-amazon.com/images/I/51dWHaLM2yL._SX331_BO1,204,203,200_.jpg',
   };
-
-  if (booksImg.key === bookTitles.key) {
-    const cover = booksImg.value;
-    console.log(cover);
-  }
 
   function render(ul) {
     for (let i = 0; i < bookTitles.length; i++) {
+      // list items
       const liItem = document.createElement('li');
-      liItem.setAttribute('id', `${bookTitles[i]}`);
-      const details = Object.keys(booksInfo);
-      const titling = document.createElement('h3');
-      titling.innerText = booksInfo[details[i]].title;
-      liItem.appendChild(titling);
       ul.appendChild(liItem);
+
+      // book title
+      const titling = document.createElement('h3');
+      liItem.appendChild(titling);
+      titling.innerText = booksInfo[bookTitles[i]].title;
+
+      // book author
       const bookAuthor = document.createElement('h4');
-      bookAuthor.innerText = booksInfo[details[i]].author;
       liItem.appendChild(bookAuthor);
+      bookAuthor.innerText = booksInfo[bookTitles[i]].author;
+
+      // book language
       const bookLanguage = document.createElement('p');
-      bookLanguage.innerText = booksInfo[details[i]].language;
       liItem.appendChild(bookLanguage);
+      bookLanguage.innerText = booksInfo[bookTitles[i]].language;
+
+      // book image
       const image = document.createElement('img');
-      // image.setAttribute('src', `${booksImg[i]}`);
-      image.setAttribute('alt', `${bookTitles[i]}` + ' cover');
-      bookLanguage.appendChild(image);
+      liItem.appendChild(image);
+      if (cover.key === bookTitles.key) {
+        image.setAttribute('src', cover[bookTitles[i]]);
+        image.setAttribute('alt', `${bookTitles[i]} cover`);
+      }
     }
   }
   function main() {
     const root = document.getElementById('root');
+
+    // the title
     const h1 = document.createElement('h1');
     h1.innerText = 'My Favorite Books';
     root.appendChild(h1);
+
+    // create a list
     const ul = document.createElement('ul');
     root.appendChild(ul);
     render(ul);
@@ -120,3 +134,4 @@
 
   window.addEventListener('load', main);
 }
+
