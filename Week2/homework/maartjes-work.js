d8d27430da79eaebff3c22f0f563a101c2658a36
whiteststar <48384895+whiteststar@users.noreply.github.com>
2019-05-11 11:16:01 +0200

    week 2 exercise 2 complete

--- Week2/homework/maartjes-work.js
+++ Week2/homework/maartjes-work.js
@@ -1,61 +1,69 @@
 'use strict';
 
+//exercise 2
+
 const monday = [
-  {
-    name: 'Write a summary HTML/CSS',
-    duration: 180,
-  },
-  {
-    name: 'Some web development',
-    duration: 120,
-  },
-  {
-    name: 'Fix homework for class10',
-    duration: 20,
-  },
-  {
-    name: 'Talk to a lot of people',
-    duration: 200,
-  },
-];
-
-const tuesday = [
-  {
-    name: 'Keep writing summary',
-    duration: 240,
-  },
-  {
-    name: 'Some more web development',
-    duration: 180,
-  },
-  {
-    name: 'Staring out the window',
-    duration: 10,
-  },
-  {
-    name: 'Talk to a lot of people',
-    duration: 200,
-  },
-  {
-    name: 'Look at application assignments new students',
-    duration: 40,
-  },
-];
-
-const maartjesTasks = monday.concat(tuesday);
-const maartjesHourlyRate = 20;
-
-function computeEarnings(tasks, hourlyRate) {
-  // Replace this comment and the next line with your code
-  console.log(tasks, hourlyRate);
-}
-
-// eslint-disable-next-line no-unused-vars
-const earnings = computeEarnings(maartjesTasks, maartjesHourlyRate);
-
-// add code to convert `earnings` to a string rounded to two decimals (euro cents)
-
-console.log(`Maartje has earned €${'replace this string with the earnings rounded to euro cents'}`);
+    {
+      name: 'Write a summary HTML/CSS',
+      duration: 180
+    },
+    {
+      name: 'Some web development',
+      duration: 120
+    },
+    {
+      name: 'Fix homework for class10',
+      duration: 20
+    },
+    {
+      name: 'Talk to a lot of people',
+      duration: 1.0
+    }
+  ];
+  
+  const tuesday = [
+    {
+      name: 'Keep writing summary',
+      duration: 1.0
+    },
+    {
+      name: 'Some more web development',
+      duration: 180
+    },
+    {
+      name: 'Staring out the window',
+      duration: 10
+    },
+    {
+      name: 'Talk to a lot of people',
+      duration: 1.0
+    },
+    {
+      name: 'Look at application assignments new students',
+      duration: 40
+    }
+  ];
+  
+     const allTasks = monday.concat(tuesday);
+  
+    const MaartjesHourlyPayment = 20;
+  
+    function salary(tasks, hourlyPayment) {
+  
+      const taskDuration = tasks.map(x =>x.duration);
+      const payableHours = taskDuration.filter(y => y > 119);  //exclude less then 2 hours. No special codding added for 1.0 hours format, because there are no 2.0 hours:)
+      const total = (accumulator, currentValue) => accumulator + currentValue;
+      const totalInHours = payableHours.reduce(total);
+      const paymentRaw = totalInHours/60*20;
+      var payment = paymentRaw.toFixed(2);
+      return payment;
+    }
+  
+    const earned = salary(allTasks, MaartjesHourlyPayment);
+    
+    console.log(`Maartje has made €${earned}. Good Job.`);
+   
+ 
 
 // Do not change or remove anything below this line
 module.exports = {
