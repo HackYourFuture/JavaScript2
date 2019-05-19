d9ddd1ffcb20629e25692eafbc3d7402e38e4f03
Wilgert Velinga <wilgert@wilgert.nl>
2019-05-19 15:55:39 +0200

    add a nice book

--- Week1/homework/app.js
+++ Week1/homework/app.js
@@ -4,6 +4,7 @@
   const bookTitles = [
     // Replace with your own book titles
     'harry_potter_chamber_secrets',
+    'the_nature_of_software_development',
   ];
 
   // Replace with your own code
