0da768cea2855ed766a2d819c23897240265fe0f
lizozo <hamza.erguc@gmail.com>
2019-05-27 00:54:13 +0300

    hamza-week2

--- Week2/homework/maartjes-work.js
+++ Week2/homework/maartjes-work.js
@@ -48,14 +48,18 @@ const maartjesHourlyRate = 20;
 function computeEarnings(tasks, hourlyRate) {
   // Replace this comment and the next line with your code
   console.log(tasks, hourlyRate);
+  return tasks
+    .filter(taskInHour => taskInHour.duration / 60 >= 2)
+    .map(taskInMinute => (taskInMinute.duration / 60) * hourlyRate)
+    .reduce((totalAmount, incomePerTask) => totalAmount + incomePerTask);
 }
 
 // eslint-disable-next-line no-unused-vars
 const earnings = computeEarnings(maartjesTasks, maartjesHourlyRate);
 
 // add code to convert `earnings` to a string rounded to two decimals (euro cents)
-
-console.log(`Maartje has earned €${'replace this string with the earnings rounded to euro cents'}`);
+const roundedEarnings = earnings.toFixed(2);
+console.log(`Maartje has earned €${roundedEarnings}`);
 
 // Do not change or remove anything below this line
 module.exports = {
--- Week2/homework/map-filter.js
+++ Week2/homework/map-filter.js
@@ -2,7 +2,7 @@
 
 function doubleOddNumbers(numbers) {
   // Replace this comment and the next line with your code
-  console.log(numbers);
+  return numbers.filter(number => number % 2 === 1).map(number => number * 2);
 }
 
 const myNumbers = [1, 2, 3, 4];
