330c79c7797a50ce08874978aad1a073fafa31e7
heartwhite <abdullahakyurek@gmail.com>
2019-05-27 23:51:15 +0200

    homework files edited

--- Week2/homework/maartjes-work.js
+++ Week2/homework/maartjes-work.js
@@ -46,16 +46,16 @@ const maartjesTasks = monday.concat(tuesday);
 const maartjesHourlyRate = 20;
 
 function computeEarnings(tasks, hourlyRate) {
-  // Replace this comment and the next line with your code
-  console.log(tasks, hourlyRate);
+  const durations = tasks.filter(task => task.duration >= 120).map(task => task.duration / 60);
+  return durations.reduce((totalRate, duration) => totalRate + duration * hourlyRate, 0);
 }
 
 // eslint-disable-next-line no-unused-vars
 const earnings = computeEarnings(maartjesTasks, maartjesHourlyRate);
 
 // add code to convert `earnings` to a string rounded to two decimals (euro cents)
-
-console.log(`Maartje has earned €${'replace this string with the earnings rounded to euro cents'}`);
+const roundedEarnings = earnings.toFixed(2);
+console.log(`Maartje has earned €${roundedEarnings}`);
 
 // Do not change or remove anything below this line
 module.exports = {
