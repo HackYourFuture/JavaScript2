60b3427b8de4fddd721fb5e46ff247852de4cf05
Shvan <48798783+Shvan1@users.noreply.github.com>
2019-06-09 00:57:12 +0200

    Update step2-4.js

--- Week3/homework/step2-4.js
+++ Week3/homework/step2-4.js
@@ -1,11 +1,14 @@
 'use strict';
 
-// sorry I just studied for tge test.
-function Dog() {
+function Dog(name, color, numLegs) {
   // add your code here
-}
 
-const hound = new Dog();
+  this.name = name;
+  this.color = color;
+  this.numLegs = numLegs;
+}
+const hound = new Dog('Alex', 'Black', 4);
+console.log(hound);
 
 // Do not change or remove anything below this line
 module.exports = hound;
