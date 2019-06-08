4cbb76b6ac63fc071acad1651eb15b4a0d1286d1
heartwhite <abdullahakyurek@gmail.com>
2019-06-09 00:23:01 +0200

    i added my idea about K dimentional array. it soesnt work but it is how i think for now

--- Week3/homework/step2-6.js
+++ Week3/homework/step2-6.js
@@ -24,10 +24,24 @@ function flattenArray3d(arr) {
   }
   return flattenArr;
 }
+// under this line i added my (in my mind) function.
+// it doesn't work but it is what i can think for now.
+function flattenArrayKd(arr) {
+  const flattenArr = [];
+  arr.forEach(element => {
+    if (Array.isArray(element)) {
+      flattenArrayKd(element);
+    } else {
+      flattenArr.push(element);
+    }
+  });
+  return flattenArr;
+}
 
 console.log(flattenArray2d(arr2d)); // -> [1, 2, 3, 4, 5, 6]
 console.log(flattenArray3d(arr3d)); // -> [1, 2, 3, 4, 5, 6, 7, 8]
 
+console.log(flattenArrayKd(arr3d));
 // Do not change or remove anything below this line
 module.exports = {
   flattenArray2d,
