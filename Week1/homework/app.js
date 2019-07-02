2696c6e7cdd459b7f9ea1c3da5014006e523c5f6
Adham Elias Botrous <50738429+Adham-Elias-Botours@users.noreply.github.com>
2019-07-02 17:25:22 +0200

    until 1.3 (in the page: button, array, ul)

--- Week1/homework/app.js
+++ Week1/homework/app.js
@@ -1,11 +1,31 @@
 'use strict';
 
-{
-  const bookTitles = [
-    // Replace with your own book titles
-    'harry_potter_chamber_secrets',
-  ];
 
-  // Replace with your own code
-  console.log(bookTitles);
-}
+const booksTitles = [
+    // Replace with your own book titles
+    'still_me',
+    'the_outsider',
+    'the_great_alone',
+    'circe',
+    'the_hate_u_give',
+    'the_kiss_quotient',
+    'vengeful',
+    'elevation',
+    'the_last_black_unicorn',
+    'i_will_be_gone_in_the_dark',
+];
+
+// Replace with your own code
+// console.log(bookTitles.length + " books Titles: \n" + bookTitles);
+const injectListBooksTitles = () => {
+    let lisContainer = document.getElementById('books');
+    let ul = document.createElement('ul');
+    lisContainer.appendChild(ul);
+    booksTitles.forEach(element => {
+        let li = document.createElement('li');
+        ul.appendChild(li);
+        let titleNoUnderScore = element.replace(/_/g, " ");
+        let titleFirstCapital = titleNoUnderScore.charAt(0).toUpperCase() + titleNoUnderScore.slice(1);
+        li.textContent = titleFirstCapital;
+    });
+};
