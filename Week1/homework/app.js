4b60eb7f41e2c1aa448bf25e33220238be01e2c8
Adham Elias Botrous <50738429+Adham-Elias-Botours@users.noreply.github.com>
2019-07-05 18:40:11 +0200

    Prettier format

--- Week1/homework/app.js
+++ Week1/homework/app.js
@@ -10,68 +10,68 @@ const booksTitles = [
     'vengeful',
     'elevation',
     'the_last_black_unicorn',
-    'i_will_be_gone_in_the_dark'
+    'i_will_be_gone_in_the_dark',
 ];
 const booksFullInfo = {
     still_me: {
         title: 'Still Me',
         writer: 'Jojo Moyes',
         category: 'FICTION',
-        price: 11.4
+        price: 11.4,
     },
     the_outsider: {
         title: 'The Outsider',
         writer: 'Stephen King',
         category: 'MYSTERY, THRILLER',
-        price: 17.17
+        price: 17.17,
     },
     the_great_alone: {
         title: 'The Great Alone',
         writer: 'Kristin Hannah',
         category: 'HISTORICAL FICTION',
-        price: 15.83
+        price: 15.83,
     },
     circe: {
         title: 'Circe',
         writer: 'Madeline Miller',
         category: 'FANTASY',
-        price: 14.82
+        price: 14.82,
     },
     the_hate_u_give: {
         title: 'The Hate U Give',
         writer: 'Angie Thomas',
         category: 'ROMANCE',
-        price: 10.43
+        price: 10.43,
     },
     the_kiss_quotient: {
         title: 'The Kiss Quotient',
         writer: 'Helen Hoang',
         category: 'ROMANCE',
-        price: 10.2
+        price: 10.2,
     },
     vengeful: {
         title: 'Vengeful',
         writer: 'V.E. Schwab',
         category: 'SCIENCE FICTION',
-        price: 14.27
+        price: 14.27,
     },
     elevation: {
         title: 'Elevation',
         writer: 'Stephen King, Mark Edward Geyer ',
         category: 'HORROR',
-        price: 13.96
+        price: 13.96,
     },
     the_last_black_unicorn: {
         title: 'The Last Black Unicorn',
         writer: 'Tiffany Haddish',
         category: 'HUMOR',
-        price: 14.86
+        price: 14.86,
     },
     i_will_be_gone_in_the_dark: {
         title: "I'll Be Gone in the Dark: One Woman's Obsessive Search for the Golden State Killer",
         writer: 'Michelle McNamara,  Gillian Flynn, Patton Oswalt',
         category: 'NONFICTION',
-        price: 10.21
+        price: 10.21,
     },
 };
 
@@ -85,7 +85,7 @@ const booksCovers = {
     vengeful: 'images/vengeful.jpg',
     elevation: 'images/elevation.jpg',
     the_last_black_unicorn: 'images/the_last_black_unicorn.jpg',
-    i_will_be_gone_in_the_dark: 'images/i_will_be_gone_in_the_dark.jpg'
+    i_will_be_gone_in_the_dark: 'images/i_will_be_gone_in_the_dark.jpg',
 };
 
 // console.log(bookTitles.length + " books Titles: \n" + bookTitles);
@@ -107,17 +107,17 @@ const ListBooksTitles = () => {
     const checkButton = document.getElementById('books_array').style.display;
     if (checkButton == 'block') {
         document.getElementById('books_array').style.display = 'none';
-        document.getElementById('btn_books_array').textContent = "Show Array list";
+        document.getElementById('btn_books_array').textContent = 'Show Array list';
     } else {
         document.getElementById('books_array').style.display = 'block';
-        document.getElementById('btn_books_array').textContent = "Hide Array list";
+        document.getElementById('btn_books_array').textContent = 'Hide Array list';
     }
 };
 
 const injectListBooksFullInfo = () => {
     const listContainer = document.getElementById('books_object');
     const pageTitle = document.createElement('h2');
-    pageTitle.textContent = "The 10 best books of 2018";
+    pageTitle.textContent = 'The 10 best books of 2018';
     listContainer.appendChild(pageTitle);
     const ul = document.createElement('ul');
     listContainer.appendChild(ul);
