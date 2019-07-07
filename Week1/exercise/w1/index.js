15262ebfe31f322d0dba1dc68b7fda5fa28c8bb9
Robbert Segers <robbert.segers@klarrio.com>
2019-07-07 11:23:25 +0200

    update week 1 exercise

--- Week1/exercise/w1/index.js
+++ Week1/exercise/w1/index.js
@@ -1,30 +1,50 @@
-console.log('Hack your future, right?!');
+console.log('Hack your future Belgium!');
 
-const header = document.querySelector('h1');
-console.log('header: ', header);
+// EXERCISE 1
 
-// EXERCISE:
-// select "change image" button (querySelector)
-// add click event listener
-// execute changeImage() function on click event
-// define changeImage() function
-// in this function:
-//    get image element
-//    get imageInput element
-//    log input value
-//    set image src to imageInput element value
+// 1a: create a function called "changeHeader", put a console.log() inside this function to test
+
+// 1d: add an event listener to the "Change header" button 
+//     and call the "changeHeader" function when clicked ( you should see your console.log() )
+
+// 1b: inside this function: select the header element and assign that to a variable called "header"
+
+// 1c: change the inner html of the header element to your name
+
 
 // ====================================== //
 
-// BONUS:
-// select "add todo" button (querySelector)
-// add click event listener
-// execute addTodo() function on click event
-// define addTodo() function
-// in this function:
-//    get todoList element
-//    get todoInput element
-//    log todoInput element value
-//    create <li> element
-//    set <li> element innerHtml to todoInput value
-//    add <li> element to todoList
+
+// EXERCISE 2
+
+// 2a: create a function called "changeImage", put a console.log() inside this function to test
+
+// 1b: add an event listener to the "Change image" button and call the "changeImage" function when clicked
+
+// inside this function:
+
+// 2c: select the "imageInput" element and assign to a variable called "imageInputValue"
+
+// 2d: select the image element and assign to a variable called "imageToChange"
+
+// 2e: to change the image: assign the imageInputValue to the image src
+
+
+// ====================================== //
+
+
+// Exercise 3:
+
+// 3a: select "add todo" button & add click event listener to execute addTodo() function on click event
+
+// 3b: define addTodo() function, in this function:
+
+// 3c: get todoList element
+
+// 3d: get todoInput element & log todoInput value
+
+// 3e: create a <li> element
+
+// 3f: set created <li> element innerHtml to todoInput value
+
+// 3g: add <li> element to todoList
