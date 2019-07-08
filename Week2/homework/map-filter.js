e5b1d788d459022e81085c6df75b6d2371f17280
Adham Elias Botrous <50738429+Adham-Elias-Botours@users.noreply.github.com>
2019-07-08 12:28:16 +0200

    solve the question

--- Week2/homework/map-filter.js
+++ Week2/homework/map-filter.js
@@ -1,13 +1,18 @@
 'use strict';
 
 function doubleOddNumbers(numbers) {
-  // Replace this comment and the next line with your code
-  console.log(numbers);
+  const filterCondition = number => number % 2 !== 0;
+  const filterResult = numbers.filter(filterCondition);
+  console.log(`The Filter Result is: [${filterResult}]`); /* array of odd numbers */
+  const numberDuplicate = number => number * 2;
+  const numbersDuplicate = filterResult.map(numberDuplicate);
+  return numbersDuplicate;
 }
 
 const myNumbers = [1, 2, 3, 4];
-console.log(doubleOddNumbers(myNumbers));
-
+console.log(`The original array is: [${myNumbers}]`);
+const myNewNumbers = doubleOddNumbers(myNumbers);
+console.log(`The new array is (map result): [${myNewNumbers}]`);
 // Do not change or remove anything below this line
 module.exports = {
   myNumbers,
