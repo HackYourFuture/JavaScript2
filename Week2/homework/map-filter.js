3f2fae5c10105b91fa444f4afe8b73aafb151c9a
Oguzhan <oguzhanturgut@outlook.com>
2019-07-08 15:42:22 +0200

    Add reduce solution to doubleOddNumbers

--- Week2/homework/map-filter.js
+++ Week2/homework/map-filter.js
@@ -1,7 +1,14 @@
 'use strict';
 
 function doubleOddNumbers(numbers) {
+  // the best practice
   return numbers.filter(n => n % 2 !== 0).map(n => n * 2);
+
+  // another solution with reduce instead og filter and map chaining
+  // return numbers.reduce((acc, current) => {
+  //   if (current % 2 !== 0) acc.push(current * 2);
+  //   return acc;
+  // }, []);
 }
 
 const myNumbers = [1, 2, 3, 4];
