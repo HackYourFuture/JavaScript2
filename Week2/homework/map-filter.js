3d54b6b24c70664c6b048aa9e85bd9082dde3b6b
20twa07 <48910851+20twa07@users.noreply.github.com>
2019-07-09 10:36:28 +0200

    homework for week2 submission

--- Week2/homework/map-filter.js
+++ Week2/homework/map-filter.js
@@ -1,13 +1,10 @@
 'use strict';
-
 function doubleOddNumbers(numbers) {
-  // Replace this comment and the next line with your code
+  return numbers.filter(number => number % 2 !== 0).map(number => number * 2);
   console.log(numbers);
 }
-
 const myNumbers = [1, 2, 3, 4];
 console.log(doubleOddNumbers(myNumbers));
-
 // Do not change or remove anything below this line
 module.exports = {
   myNumbers,
