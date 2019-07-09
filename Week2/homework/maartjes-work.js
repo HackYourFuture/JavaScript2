e477f879ed1622de2bd36fad27965100f7504856
ShyarK <shyr.khalil@gmail.com>
2019-07-09 13:59:39 +0200

    hyf-js2-week2 homework submission

--- Week2/homework/maartjes-work.js
+++ Week2/homework/maartjes-work.js
@@ -42,12 +42,25 @@ const tuesday = [
   },
 ];
 
+// Write a program that computes how much Maartje has earned by completing these tasks, using map and filter.
+// For the 'summing part' you can try your luck with reduce; alternatively, you may use forEach or a for loop.
+// Follow these steps. Each step should build on the result of the previous step.
+// Map the tasks to durations in hours.
+// Filter out everything that took less than two hours (i.e., remove from the collection)
+// Multiply the each duration by a per-hour rate for billing (use €20/hour) and sum it all up.
+// Output a formatted Euro amount, rounded to Euro cents, e.g: €11.34.
+// Choose variable and parameters names that most accurately describe their contents or purpose. When naming an array, use a plural form, e.g. durations. For a single item, use a singular form, e.g. duration. For details, see Naming Conventions.
+// Don't forget to use =>.
+
 const maartjesTasks = monday.concat(tuesday);
 const maartjesHourlyRate = 20;
 
 function computeEarnings(tasks, hourlyRate) {
   // Replace this comment and the next line with your code
-  console.log(tasks, hourlyRate);
+  const twoHoursTasks = tasks.map(el => el.duration / 60).filter(duration => duration >= 2);
+  const hourlyRateBilling = twoHoursTasks.map(duration => duration * hourlyRate);
+  const sumAllBillings = hourlyRateBilling.reduce((acc, curr) => acc + curr);
+  return sumAllBillings;
 }
 
 // eslint-disable-next-line no-unused-vars
@@ -55,7 +68,7 @@ const earnings = computeEarnings(maartjesTasks, maartjesHourlyRate);
 
 // add code to convert `earnings` to a string rounded to two decimals (euro cents)
 
-console.log(`Maartje has earned €${'replace this string with the earnings rounded to euro cents'}`);
+console.log(`Maartje has earned €${earnings.toFixed(2)}`);
 
 // Do not change or remove anything below this line
 module.exports = {
--- Week2/homework/map-filter.js
+++ Week2/homework/map-filter.js
@@ -2,7 +2,7 @@
 
 function doubleOddNumbers(numbers) {
   // Replace this comment and the next line with your code
-  console.log(numbers);
+  return numbers.filter(num => num % 2 !== 0).map(el => el * 2);
 }
 
 const myNumbers = [1, 2, 3, 4];
