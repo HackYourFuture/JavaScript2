6d7f182fada801b209e6a37cad1d858b5c75860a
Adham Elias Botrous <50738429+Adham-Elias-Botours@users.noreply.github.com>
2019-07-10 17:25:55 +0200

    2-2 done, test faild.

--- Week3/homework/step2-2.js
+++ Week3/homework/step2-2.js
@@ -4,17 +4,43 @@ function threeFive(startIndex, stopIndex, threeCallback, fiveCallback) {
   const numbers = [];
 
   // Replace this comment and the next line with your code
-  console.log(startIndex, stopIndex, threeCallback, fiveCallback, numbers);
+  let i;
+  for (i = startIndex; i < stopIndex + 1; i++) {
+    numbers.push(i);
+  }
+  let ii;
+  for (ii = 0; ii < numbers.length; ii++) {
+    if (numbers[ii] % 5 === 0 && numbers[ii] % 3 === 0) {
+      console.log(`Hi: I am ${numbers[ii]},\n my index is ${numbers.indexOf(numbers[ii])}`);
+      threeCallback(numbers[ii]);
+      fiveCallback(numbers[ii]);
+      console.log(`Bye.`);
+    } else if (numbers[ii] % 3 === 0) {
+      console.log(`Hi: I am ${numbers[ii]},\n my index is ${numbers.indexOf(numbers[ii])}`);
+      threeCallback(numbers[ii]);
+      console.log(`Bye.`);
+    } else if (numbers[ii] % 5 === 0) {
+      console.log(`Hi: I am ${numbers[ii]},\n my index is ${numbers.indexOf(numbers[ii])}`);
+      fiveCallback(numbers[ii]);
+      console.log(`Bye.`);
+    } else {
+      console.log(
+        `Hi: I am ${numbers[ii]}\n my index is ${numbers.indexOf(
+          numbers[ii],
+        )} . \n I can not be divided on 3 neither on 5 \n Bye.`,
+      );
+    }
+  }
 }
 
 function sayThree(number) {
   // Replace this comment and the next line with your code
-  console.log(number);
+  console.log(`I can be divided on 3 `);
 }
 
 function sayFive(number) {
   // Replace this comment and the next line with your code
-  console.log(number);
+  console.log(`I can be divided on 5 `);
 }
 
 threeFive(10, 15, sayThree, sayFive);
