c6af6f9a75bee4f04d1b4d6dd4564e37ac913cd5
Adham Elias Botrous <50738429+Adham-Elias-Botours@users.noreply.github.com>
2019-07-11 08:32:53 +0200

    2.4 done & Test Pass

--- Week3/homework/step2-4.js
+++ Week3/homework/step2-4.js
@@ -1,10 +1,12 @@
 'use strict';
 
-function Dog() {
-  // add your code here
+function Dog(name, color, numLegs) {
+  this.name = name;
+  this.color = color;
+  this.numLegs = numLegs;
 }
 
-const hound = new Dog();
-
+const hound = new Dog('Kiki', 'Brown', 4);
+// console.log(hound);
 // Do not change or remove anything below this line
 module.exports = hound;
