80989a83d08ac4c44e9416bafaa569d5d6c32cd1
Adham Elias Botrous <50738429+Adham-Elias-Botours@users.noreply.github.com>
2019-07-11 11:48:58 +0200

    step 3 done and Test Pass

--- Week3/homework/step3.js
+++ Week3/homework/step3.js
@@ -1,14 +1,17 @@
 'use strict';
 
 function createBase(base) {
-  // Replace this comment and the next line with your code
-  console.log(base);
+  return function(add) {
+    return base + add;
+  };
 }
 
 const addSix = createBase(6);
+// const addSix = createBase(6);
 
 console.log(addSix(10)); // returns 16
+console.log(createBase(6)(10)); // the same (16)
 console.log(addSix(21)); // returns 27
-
+console.log(createBase(6)(21)); // the same(27)
 // Do not change or remove anything below this line
 module.exports = createBase;
