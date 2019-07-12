d20572020c291c6256ba6a66239e193944e08dbd
Salih <salihsert18@gmail.com>
2019-07-13 00:06:56 +0200

    add hw2 map filter 1.1

--- Week2/homework/map-filter.js
+++ Week2/homework/map-filter.js
@@ -1,8 +1,8 @@
 'use strict';
 
 function doubleOddNumbers(numbers) {
-  // Replace this comment and the next line with your code
-  console.log(numbers);
+  const arr = numbers.filter(num => num % 2 !== 0).map(num => num * 2);
+  return arr;
 }
 
 const myNumbers = [1, 2, 3, 4];
