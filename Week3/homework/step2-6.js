511449e5f89d10667827d738e89b30831c5f8e4c
elicarcar <elisabetta.carcar@hotmail.com>
2019-07-13 15:56:53 +0200

    Finish half of  step 2-6 Homework

--- Week3/homework/step2-6.js
+++ Week3/homework/step2-6.js
@@ -4,8 +4,13 @@ const arr2d = [[1, 2], [3, 4], [5, 6]];
 const arr3d = [[[1, 2], [3, 4]], [[5, 6], [7, 8]]];
 
 function flattenArray2d(arr) {
-  // Replace this comment and the next line with your code
-  console.log(arr);
+  const newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    for (let n = 0; n < arr.length - 1; n++) {
+      newArr.push(arr[i][n]);
+    }
+  }
+  return newArr;
 }
 
 function flattenArray3d(arr) {
@@ -14,7 +19,7 @@ function flattenArray3d(arr) {
 }
 
 console.log(flattenArray2d(arr2d)); // -> [1, 2, 3, 4, 5, 6]
-console.log(flattenArray3d(arr3d)); // -> [1, 2, 3, 4, 5, 6, 7, 8]
+// console.log(flattenArray3d(arr3d)); // -> [1, 2, 3, 4, 5, 6, 7, 8]
 
 // Do not change or remove anything below this line
 module.exports = {
