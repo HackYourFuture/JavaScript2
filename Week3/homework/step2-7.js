887cbf33996731f6c2cfc46e3ee064a6d07b28c9
musafir928 <uganoghli@gmail.com>
2019-07-17 13:18:30 +0200

    correction on 2-7

--- Week3/homework/step2-7.js
+++ Week3/homework/step2-7.js
@@ -24,8 +24,8 @@ console.log(y);
 /* My explain:
   when a primitive type assigned to a variable, it means: variable as a container of that value
  but, in a non-primitive type: the variable point to the location, not actually as a container of that value.
- so, in this two function above, when we call the function f1(x), we just copy x's value to the val, so after running function x not changed.
- but, in f2(y), when the function get y as a parameter val also gets the location(address) of what why gets. Both y and val pointed to same location.
+ so, in this two function above, when we call the function f1(x), we just copy x's value to the val, so after running function, x not changed.
+ but, in f2(y), when the function get y as a parameter val also gets the location(address) of what y gets. Both y and val pointed to same location.
  so, changing one of them means changing the another. 
  This is a main difference between primitive type and none-primitive type in javascript   data types.
  */
