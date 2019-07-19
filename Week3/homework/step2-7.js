6ff241609817d51429ac5c9fbe79053e6fe82086
20twa07 <48910851+20twa07@users.noreply.github.com>
2019-07-19 12:46:11 +0200

    add extra explanation

--- Week3/homework/step2-7.js
+++ Week3/homework/step2-7.js
@@ -30,12 +30,20 @@ console.log(y);
 // console.log(Object.prototype.toString.call(9) === '[object Number]'); returns true
 // typeof 9 is Number which is primitive.
 
-// So we are just passing value of x inside of function. Actual x will not change.
+// const x = 9
+// In this code we put x value somewhere in the memory heap.
+// When We call f1(x), this function uses that variable which is in the memory heap.Actual x will not change.
+// So we are just passing value of x inside of function.
+// functions have their own scope and execution context So x will be 10 inside of function.
+// At the end we are logging x. not f1(x). They are both different scope and execution context.
 
 // Second Example
+
 // This example shows us "Pass by reference in Js". Because;
 // console.log(Object.prototype.toString.call(y) === '[object Object]'); returns true
 // means typeof y is Object
 
-// When we want to update/do sth. inside of function with objects, actual object will change. so;
+// When we want to update/do sth. inside of function with objects, actual object will change.
+// Because parameter of f2(y) which is y and const y are both referencing the same object.
+// If we change reference object both of them will be affected from that code.
 // console.log(y) returns {x: 10}
