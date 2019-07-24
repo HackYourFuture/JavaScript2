afd78866237a0416207febbd4f65505313c6fdf3
musafir928 <uganoghli@gmail.com>
2019-07-24 18:06:43 +0200

    add parameters to Dog function

--- Week3/homework/step2-4.js
+++ Week3/homework/step2-4.js
@@ -1,14 +1,14 @@
 'use strict';
 
-function Dog() {
+function Dog(name, color, food, numLegs) {
   // add your code here
-  this.name = 'kuchuk';
-  this.color = 'black and white';
-  this.food = 'meat';
-  this.numLegs = 4;
+  this.name = name;
+  this.color = color;
+  this.food = food;
+  this.numLegs = numLegs;
 }
 
-const hound = new Dog();
+const hound = new Dog('kuchuk', 'black and white', 'meat', 4);
 
 // Do not change or remove anything below this line
 module.exports = hound;
