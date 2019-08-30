fb743c6646e9060290eda369074b8c426a71d761
FarukTercan <47561416+FarukTercan@users.noreply.github.com>
2019-08-30 23:21:35 +0200

    Latest Update
    I have a merge issue on Git so I edited my file directly in the Github.com.

--- Week1/homework/app.js
+++ Week1/homework/app.js
@@ -1,57 +1,19 @@
 
 {
-  'use strict';
-
-  //  1.1
+  ('use strict');
 
   const myBooks = [
-    'notes_from_underground',
     'brave_new_world',
+    'notes_from_underground',
+    'blindness',
     'nineteen_eighty_four',
-    'animal_farm',
     'the_little_prince',
+    'animal_farm',
     'the_alchemist',
     'essays_montaigne',
-    'blindness',
-    'a_confession',
     'sherlock_holmes',
+    'a_confession',
   ];
-
-  // 1.2
-  console.log(myBooks);
-
-  //  1.3
-
-  function makeList() {
-    const myBooks = [
-      'notes_from_underground',
-      'brave_new_world',
-      'nineteen_eighty_four',
-      'animal_farm',
-      'the_little_prince',
-      'the_alchemist',
-      'essays_montaigne',
-      'blindness',
-      'a_confession',
-      'sherlock_holmes',
-    ];
-
-    const listDiv = document.createElement('div');
-    document.getElementsByTagName('body')[0].appendChild(listDiv);
-
-    const ulTag = document.createElement('ul');
-    listDiv.appendChild(ulTag);
-
-    for (let i = 0; i < myBooks.length; i++) {
-      const liTag = document.createElement('li');
-      liTag.innerHTML = myBooks[i];
-      ulTag.appendChild(liTag);
-    }
-  }
-
-  // makeList(); 
-  //I canceled function calling to avoid showing on page
-
 
   const infoBookList = {
     notes_from_underground: {
@@ -65,65 +27,60 @@
     the_little_prince: { title: 'The Little Prince', release: '(1943)', author: 'Saint Exupery' },
     the_alchemist: { title: 'The Alchemist', release: '(1988)', author: 'Paulo Coelho' },
     essays_montaigne: { title: 'Essays', release: '(1580)', author: 'Montaigne' },
-    blindness_saramago: { title: 'Blindness', release: '(1995)', author: 'Saramago' },
-    a_confession: { title: 'A Confession', release: '(1884)', author: 'Tolstoy' },
     sherlock_holmes: { title: 'Sherlock Holmes', release: '(1892)', author: 'AC Doyle' },
+    blindness: { title: 'Blindness', release: '(1995)', author: 'Saramago' },
+    a_confession: { title: 'A Confession', release: '(1884)', author: 'Tolstoy' },
   };
 
+  const coverBookList = {
+    notes_from_underground:
+      'https://s.s-bol.com/imgbase0/imagebase3/large/FC/0/1/3/2/9200000024772310.jpg',
+    brave_new_world:
+      'https://upload.wikimedia.org/wikipedia/en/thumb/6/62/BraveNewWorld_FirstEdition.jpg/220px-BraveNewWorld_FirstEdition.jpg',
+    nineteen_eighty_four: 'https://www.studystore.nl/images/9780141036144/3/1',
+    animal_farm: 'https://s.s-bol.com/imgbase0/imagebase3/large/FC/6/2/8/8/1001004005988826.jpg',
+    the_little_prince:
+      'https://s.s-bol.com/imgbase0/imagebase3/large/FC/7/5/0/8/1001004000638057.jpg',
+    the_alchemist: 'https://s.s-bol.com/imgbase0/imagebase3/large/FC/9/3/2/3/9200000041223239.jpg',
+    essays_montaigne:
+      'https://s.s-bol.com/imgbase0/imagebase3/large/FC/1/0/6/6/1001004000636601.jpg',
+    blindness: 'https://s.s-bol.com/imgbase0/imagebase3/large/FC/0/6/8/0/9200000001310860.jpg',
+    a_confession: 'https://images-na.ssl-images-amazon.com/images/I/71i8kz3FrZL.jpg',
+    sherlock_holmes: 'https://images-na.ssl-images-amazon.com/images/I/91743TsDedL.jpg',
+  };
 
   function makeBookList() {
     const divBookList = document.createElement('div');
-    document.getElementsByTagName('body')[0].appendChild(divBookList);
+    document.body.appendChild(divBookList);
 
-    const ulBookList = document.createElement('ul');
-    divBookList.appendChild(ulBookList);
+    const bookListElement = document.createElement('ul');
+    divBookList.appendChild(bookListElement);
 
-    const values = Object.values(infoBookList);
+    for (let i = 0; i < myBooks.length; i++) {
+      const key = myBooks[i];
+      const book = infoBookList[key];
 
-    for (let i = 0; i < values.length; i++) {
-      const liBookList = document.createElement('li');
-      ulBookList.appendChild(liBookList);
-
-      const covers = document.createElement('img');
-      liBookList.appendChild(covers);
+      const bookListItem = document.createElement('li');
+      bookListElement.appendChild(bookListItem);
 
       const titleh2 = document.createElement('h2');
-      liBookList.appendChild(titleh2);
-      titleh2.innerHTML = values[i].title;
+      bookListItem.appendChild(titleh2);
+      titleh2.innerHTML = book.title;
 
       const p1 = document.createElement('p');
-      liBookList.appendChild(p1);
-      p1.innerHTML = values[i].release;
+      bookListItem.appendChild(p1);
+      p1.innerHTML = book.release;
 
       const p2 = document.createElement('p');
-      liBookList.appendChild(p2);
-      p2.innerHTML = values[i].author;
+      bookListItem.appendChild(p2);
+      p2.innerHTML = book.author;
 
-    }
-
-    const coverBookList = {
-      notes_from_underground:
-        https://s.s-bol.com/imgbase0/imagebase3/large/FC/0/1/3/2/9200000024772310.jpg',
-      brave_new_world: 'https://upload.wikimedia.org/wikipedia/en/thumb/6/62/BraveNewWorld_FirstEdition.jpg/220px-BraveNewWorld_FirstEdition.jpg',
-      nineteen_eighty_four: 'https://www.studystore.nl/images/9780141036144/3/1',
-      animal_farm: 'https://s.s-bol.com/imgbase0/imagebase3/large/FC/6/2/8/8/1001004005988826.jpg',
-      the_little_prince: 'https://s.s-bol.com/imgbase0/imagebase3/large/FC/7/5/0/8/1001004000638057.jpg',
-      the_alchemist: 'https://s.s-bol.com/imgbase0/imagebase3/large/FC/9/3/2/3/9200000041223239.jpg',
-      essays_montaigne: 'https://s.s-bol.com/imgbase0/imagebase3/large/FC/1/0/6/6/1001004000636601.jpg',
-      blindness_saramago: 'https://s.s-bol.com/imgbase0/imagebase3/large/FC/0/6/8/0/9200000001310860.jpg',
-      a_confession: 'https://images-na.ssl-images-amazon.com/images/I/71i8kz3FrZL.jpg',
-      sherlock_holmes: 'https://images-na.ssl-images-amazon.com/images/I/91743TsDedL.jpg',
-    };
-
-    const coverValues = Object.values(coverBookList);
-
-    for (let i = 0; i < coverValues.length; i++) {
-      document.getElementsByTagName('img')[i].setAttribute('src', coverValues[i]);
-      document.getElementsByTagName('img')[i].setAttribute('alt', myBooks[i]);
+      const covers = document.createElement('img');
+      bookListItem.appendChild(covers);
+      covers.setAttribute('src', coverBookList[key]);
+      covers.setAttribute('alt', book.title);
     }
   }
 
   makeBookList();
-
-
 }
