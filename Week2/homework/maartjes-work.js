2a6146d64c4403b1b74c7d1ae6c622faf62e0146
mosleh <mosleh.alnaqeeb@gmail.com>
2019-09-01 20:25:03 +0200

    JS2-Week2

--- Week2/homework/maartjes-work.js
+++ Week2/homework/maartjes-work.js
@@ -46,16 +46,31 @@ const maartjesTasks = monday.concat(tuesday);
 const maartjesHourlyRate = 20;
 
 function computeEarnings(tasks, hourlyRate) {
-  // Replace this comment and the next line with your code
-  console.log(tasks, hourlyRate);
+  let result = tasks.map(toHours => toHours.duration / 60);
+  console.log(result); // just for testing.
+  result = result.filter(twoHoursOrMore => twoHoursOrMore >= 2);
+  console.log(result); // just for testing.
+  result = result.reduce((sum, twoHoursOrMore) => sum + twoHoursOrMore * hourlyRate, 0);
+  return result;
 }
+
+/* or it could be solved with chaining method as following
+function computeEarnings(tasks, hourlyRate) {
+  const result = tasks
+    .map(toHours => toHours.duration / 60)
+    .filter(twoHoursOrMore => twoHoursOrMore >= 2)
+    .reduce((sum, twoHoursOrMore) => sum + twoHoursOrMore * hourlyRate, 0);
+  console.log(result);
+}
+ (but now i prefer the previous method because it allow me to log each stage and help me to more understand). */
 
 // eslint-disable-next-line no-unused-vars
 const earnings = computeEarnings(maartjesTasks, maartjesHourlyRate);
+console.log(typeof earnings); // (note for me) in the function computeEarnings i used console log instead of return so when i tried to apply the toFixed method i got NaN and to know what happened i used (typeof earnings) then the result was undefined but when i used return the result was number.
 
 // add code to convert `earnings` to a string rounded to two decimals (euro cents)
 
-console.log(`Maartje has earned €${'replace this string with the earnings rounded to euro cents'}`);
+console.log(`Maartje has earned €${earnings.toFixed(2)}`);
 
 // Do not change or remove anything below this line
 module.exports = {
--- Week2/homework/map-filter.js
+++ Week2/homework/map-filter.js
@@ -1,10 +1,11 @@
 'use strict';
 
 function doubleOddNumbers(numbers) {
-  // Replace this comment and the next line with your code
-  console.log(numbers);
+  let result = numbers.filter(elem => elem % 2 !== 0);
+  console.log(result); // [1,3]
+  result = result.map(x => x * 2);
+  return result; // [2,6]
 }
-
 const myNumbers = [1, 2, 3, 4];
 console.log(doubleOddNumbers(myNumbers));
 
