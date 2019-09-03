d5c42e8b895b35a848da47a2ae850d59ce166eec
ihk2018 <karatasibrahimhalil@gmail.com>
2019-09-03 11:18:17 +0200

    JS2Week2 Homework

--- Week2/homework/maartjes-work.js
+++ Week2/homework/maartjes-work.js
@@ -46,16 +46,17 @@ const maartjesTasks = monday.concat(tuesday);
 const maartjesHourlyRate = 20;
 
 function computeEarnings(tasks, hourlyRate) {
-  // Replace this comment and the next line with your code
-  console.log(tasks, hourlyRate);
+  return tasks
+    .map(taskToDurations => taskToDurations.duration / 60)
+    .filter(lessThanTwoHours => lessThanTwoHours >= 2)
+    .map(durationPerHour => durationPerHour * hourlyRate)
+    .reduce((totalAmount, amount) => totalAmount + amount, 0);
 }
 
 // eslint-disable-next-line no-unused-vars
 const earnings = computeEarnings(maartjesTasks, maartjesHourlyRate);
 
-// add code to convert `earnings` to a string rounded to two decimals (euro cents)
-
-console.log(`Maartje has earned €${'replace this string with the earnings rounded to euro cents'}`);
+console.log(`Maartje has earned €${earnings.toFixed(2)}`);
 
 // Do not change or remove anything below this line
 module.exports = {
