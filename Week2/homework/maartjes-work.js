5c81b9a42602ddfbb3b7dc8a8a04c8283d4f2de3
mehmetekinci <mehmetekinci1989@gmail.com>
2019-09-03 20:16:54 +0200

    add javaScript2-week2 homework

--- Week2/homework/maartjes-work.js
+++ Week2/homework/maartjes-work.js
@@ -46,8 +46,10 @@ const maartjesTasks = monday.concat(tuesday);
 const maartjesHourlyRate = 20;
 
 function computeEarnings(tasks, hourlyRate) {
-  // Replace this comment and the next line with your code
-  console.log(tasks, hourlyRate);
+  return tasks
+    .map(hourlyTasks => hourlyTasks.duration / 60)
+    .filter(moreThanTwoHours => moreThanTwoHours >= 2)
+    .reduce((totalIncome, taskDurations) => totalIncome + taskDurations * hourlyRate, 0);
 }
 
 // eslint-disable-next-line no-unused-vars
@@ -55,7 +57,7 @@ const earnings = computeEarnings(maartjesTasks, maartjesHourlyRate);
 
 // add code to convert `earnings` to a string rounded to two decimals (euro cents)
 
-console.log(`Maartje has earned €${'replace this string with the earnings rounded to euro cents'}`);
+console.log(`Maartje has earned €${earnings.toFixed(2)}`);
 
 // Do not change or remove anything below this line
 module.exports = {
--- Week2/homework/map-filter.js
+++ Week2/homework/map-filter.js
@@ -1,8 +1,8 @@
 'use strict';
 
 function doubleOddNumbers(numbers) {
-  // Replace this comment and the next line with your code
-  console.log(numbers);
+  const oddNums = numbers.filter(data => data % 2 !== 0).map(data => data * 2);
+  return oddNums;
 }
 
 const myNumbers = [1, 2, 3, 4];
