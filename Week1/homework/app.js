2117adf861fc576d15e0698fe788b4c4c0c7c905
Ann-Grabetski <ann.grabetski@gmail.com>
2019-09-07 17:59:47 +0200

    implementation of feedback to JS2 Week1 homework from Ann-Grabetski

--- Week1/homework/app.js
+++ Week1/homework/app.js
@@ -1,7 +1,6 @@
 'use strict';
 
 {
-
   const booksArray = [
     `The_Goldfinch`,
     `A_Garden_of_Earthly_Delights`,
@@ -12,72 +11,62 @@
     `The_Unwomanly_Face_of_War`,
     `Chernobyl_Prayer`,
     `Runaway`,
-    `The_Year_of_Magical_Thinking`
+    `The_Year_of_Magical_Thinking`,
   ];
 
   const booksObject = {
     The_Goldfinch: {
       title: `The Goldfinch`,
       author: `Donna Tartt`,
-      original_language: `English`
+      original_language: `English`,
     },
     A_Garden_of_Earthly_Delights: {
       title: `A Garden of Earthly Delights`,
       author: `Carol Oates`,
-      original_language: `English`
+      original_language: `English`,
     },
     Beloved: {
       title: `Beloved`,
       author: `Toni Morrison`,
-      original_language: `English`
+      original_language: `English`,
     },
     There_Once_Lived_a_Woman_Who_Tried_to_Kill_Her_Neighbors_Baby: {
       title: `There Once Lived a Woman Who Tried to Kill Her Neighbo\'s Baby`,
       author: `Lyudmila Petrushevskaya`,
-      original_language: `Russian`
+      original_language: `Russian`,
     },
     The_House_of_the_Spirits: {
       title: `The House of the Spirits`,
       author: `Isabel Allende`,
-      original_language: `Spanish`
+      original_language: `Spanish`,
     },
     The_Left_Hand_of_Darkness: {
       title: `The Left Hand of Darkness`,
       author: `Ursula K. Le Guin`,
-      original_language: `English`
+      original_language: `English`,
     },
     The_Unwomanly_Face_of_War: {
       title: `The Unwomanly Face of War`,
       author: `Svetlana Alexievich`,
-      original_language: `Russian`
+      original_language: `Russian`,
     },
     Chernobyl_Prayer: {
       title: `Chernobyl Prayer`,
       author: `Svetlana Alexievich`,
-      original_language: `Russian`
+      original_language: `Russian`,
     },
     Runaway: {
       title: `Runaway`,
       author: `Alice Munro`,
-      original_language: `English`
+      original_language: `English`,
     },
     The_Year_of_Magical_Thinking: {
       title: `The Year of Magical Thinking`,
       author: `Joan Didion`,
-      original_language: `English`
-    }
+      original_language: `English`,
+    },
   };
 
-  for (const book of booksArray) {
-    const title = booksObject[book].title;
-    const author = booksObject[book].author;
-    const original_language = booksObject[book].original_language;
-
-    console.log(title);
-    console.log(author);
-    console.log(original_language);
-  };
-  
   const booksList = document.createElement(`ul`);
   document.body.insertBefore(booksList, document.body.childNodes[2]);
 
@@ -85,7 +74,7 @@
     const listItem = document.createElement(`li`);
     booksList.appendChild(listItem);
     listItem.setAttribute(`id`, book);
-    
+
     const title = document.createElement(`h3`);
     listItem.appendChild(title);
     const titleContent = document.createTextNode(booksObject[book].title);
@@ -99,8 +88,43 @@
 
     const original_language = document.createElement(`p`);
     listItem.appendChild(original_language);
-    const original_languageContent = document.createTextNode(`Original language: ${booksObject[book].original_language}`);
+    const original_languageContent = document.createTextNode(
+      `Original language: ${booksObject[book].original_language}`,
+    );
     original_language.appendChild(original_languageContent);
+  }
+
+  const imagesObject = {
+    The_Goldfinch: {
+      url: `images/The_Goldfinch.jpg`,
+    },
+    A_Garden_of_Earthly_Delights: {
+      url: `images/A_Garden_of_Earthly_Delights.jpg`,
+    },
+    Beloved: {
+      url: `images/Beloved.jpg`,
+    },
+    There_Once_Lived_a_Woman_Who_Tried_to_Kill_Her_Neighbors_Baby: {
+      url: `images/There_Once_Lived_a_Woman_Who_Tried_to_Kill_Her_Neighbors_Baby.jpg`,
+    },
+    The_House_of_the_Spirits: {
+      url: `images/The_House_of_the_Spirits.jpg`,
+    },
+    The_Left_Hand_of_Darkness: {
+      url: `images/The_Left_Hand_of_Darkness.jpg`,
+    },
+    The_Unwomanly_Face_of_War: {
+      url: `images/The_Unwomanly_Face_of_War.jpg`,
+    },
+    Chernobyl_Prayer: {
+      url: `images/Chernobyl_Prayer.jpg`,
+    },
+    Runaway: {
+      url: `images/Runaway.jpg`,
+    },
+    The_Year_of_Magical_Thinking: {
+      url: `images/The_Year_of_Magical_Thinking.jpg`,
+    },
   };
 
   for (const book of booksArray) {
@@ -109,7 +133,6 @@
     const image = document.createElement(`img`);
     listItem.appendChild(image);
     listItem.insertBefore(image, listItem.childNodes[0]);
-    image.setAttribute(`src`, `images/${book}.jpg`);
-  };
-  
-};
+    image.setAttribute(`src`, imagesObject[book].url);
+  }
+}
