2a7d6d99e1265db10882dc32eda300394a9805e5
msnmz <snmez.muharrem@gmail.com>
2019-09-11 15:56:44 +0200

    Does: step 2-6

--- Week3/homework/step2-6.js
+++ Week3/homework/step2-6.js
@@ -4,13 +4,14 @@ const arr2d = [[1, 2], [3, 4], [5, 6]];
 const arr3d = [[[1, 2], [3, 4]], [[5, 6], [7, 8]]];
 
 function flattenArray2d(arr) {
-  // Replace this comment and the next line with your code
-  console.log(arr);
+  return arr.reduce((flattenedArr, currentArr) => [...flattenedArr, ...currentArr], []);
 }
 
 function flattenArray3d(arr) {
-  // Replace this comment and the next line with your code
-  console.log(arr);
+  return arr.reduce(
+    (flattenedArr, current2dArr) => [...flattenedArr, ...flattenArray2d(current2dArr)],
+    [],
+  );
 }
 
 console.log(flattenArray2d(arr2d)); // -> [1, 2, 3, 4, 5, 6]
