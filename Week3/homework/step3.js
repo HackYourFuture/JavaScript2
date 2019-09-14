ace3469f9d0e43d505d261e462aac98e167aa67d
mehmetekinci <mehmetekinci1989@gmail.com>
2019-09-14 22:13:10 +0200

    add JavaScript2 -week3-second homework

--- Week3/homework/step3.js
+++ Week3/homework/step3.js
@@ -1,8 +1,9 @@
 'use strict';
 
 function createBase(base) {
-  // Replace this comment and the next line with your code
-  console.log(base);
+  return function(newNum) {
+    return base + newNum;
+  };
 }
 
 const addSix = createBase(6);
