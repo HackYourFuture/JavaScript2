2c7b0115eeb6e8c3b8da2d46271bce2eb842e67a
msnmz <snmez.muharrem@gmail.com>
2019-09-20 03:05:40 +0200

    Fix: step 2-4::undefined-parameters

--- Week3/homework/step2-4.js
+++ Week3/homework/step2-4.js
@@ -6,7 +6,7 @@ function Dog(name, color, numLegs) {
   this.numLegs = numLegs;
 }
 
-const hound = new Dog();
+const hound = new Dog('Hound', 'yellow', 4);
 
 // Do not change or remove anything below this line
 module.exports = hound;
