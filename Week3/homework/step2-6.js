52ae2f64e9082b6f43eec633bbeb0b82efb53e3b
msnmz <snmez.muharrem@gmail.com>
2019-09-20 03:36:19 +0200

    Fix: step 2-6::multilevel-flattenning-recursive

--- Week3/homework/step2-6.js
+++ Week3/homework/step2-6.js
@@ -2,6 +2,10 @@
 
 const arr2d = [[1, 2], [3, 4], [5, 6]];
 const arr3d = [[[1, 2], [3, 4]], [[5, 6], [7, 8]]];
+const arrMultid = [
+  [[1, 2, [3, 4]], [5, 6, 7, 8]],
+  [[9, 10], [11, 12, [13, 14, 15, [16, 17, 18, 19, [20, 21]]]]],
+];
 
 function flattenArray2d(arr) {
   return arr.reduce((flattenedArr, currentArr) => [...flattenedArr, ...currentArr], []);
@@ -15,18 +19,20 @@ function flattenArray3d(arr) {
 }
 
 function flattenArrayMultilevel(arr) {
-  return arr.reduce(
-    (flattenedArr, currentElm) => [
-      ...flattenedArr,
-      Array.isArray(currentElm) ? flattenArrayMultilevel(currentElm) : currentElm,
-    ],
-    [],
-  );
+  let flattenedArray = [];
+  if (Array.isArray(arr)) {
+    arr.forEach(elm => {
+      if (Array.isArray(elm)) flattenedArray = [...flattenedArray, ...flattenArrayMultilevel(elm)];
+      else flattenedArray = [...flattenedArray, elm];
+    });
+    return flattenedArray;
+  }
+  return [...flattenedArray, arr];
 }
 
 console.log(flattenArray2d(arr2d)); // -> [1, 2, 3, 4, 5, 6]
 console.log(flattenArray3d(arr3d)); // -> [1, 2, 3, 4, 5, 6, 7, 8]
-console.log(flattenArrayMultilevel(arr3d)); // -> [1, 2, 3, 4, 5, 6, 7, 8]
+console.log(flattenArrayMultilevel(arrMultid)); // -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]
 
 // Do not change or remove anything below this line
 module.exports = {
--- Week3/test/step2-6.test.js
+++ Week3/test/step2-6.test.js
@@ -7,6 +7,12 @@ const expected2d = [1, 2, 3, 4, 5, 6];
 const arr3d = [[[1, 2], [3, 4]], [[5, 6], [7, 8]]];
 const expected3d = [1, 2, 3, 4, 5, 6, 7, 8];
 
+const arrMultid = [
+  [[1, 2, [3, 4]], [5, 6, 7, 8]],
+  [[9, 10], [11, 12, [13, 14, 15, [16, 17, 18, 19, [20, 21]]]]],
+];
+const expectedMultid = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21];
+
 describe('step2-6', () => {
   test('flattenArray2d -> [1, 2, 3, 4, 5, 6]', () => {
     const result = flattenArray2d(arr2d);
@@ -18,8 +24,8 @@ describe('step2-6', () => {
     expect(result).toEqual(expected3d);
   });
 
-  test('flattenArrayMultilevel -> [1, 2, 3, 4, 5, 6, 7, 8]', () => {
-    const result = flattenArrayMultilevel(arr3d);
-    expect(result).toEqual(expected3d);
+  test('flattenArrayMultilevel -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]', () => {
+    const result = flattenArrayMultilevel(arrMultid);
+    expect(result).toEqual(expectedMultid);
   });
 });
