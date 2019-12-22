7a4f6af338147ca27a7a6b48a13e658ed0a6dd43
Jalal Al Attar <jalalalattar89@gmail.com>
2019-12-22 01:34:42 +0100

    Homework for first week js2

---
+++ Week1/homework/js2-exercises/3-hijackGoogleLogo.js
@@ -0,0 +1,7 @@
+function hijackGoogleLogo () {
+    let googleImage=document.querySelector('#hplogo')
+    googleImage.src='https://www.hackyourfuture.dk/static/logo-dark.svg'
+    googleImage.srcset='https://www.hackyourfuture.dk/static/logo-dark.svg'
+
+}
+hijackGoogleLogo ();
---
+++ Week1/homework/js2-exercises/4-showCurrentTime.js
@@ -0,0 +1,6 @@
+function eenklok() {
+    const datum = new Date();
+    const tijd = datum.toLocaleTimeString();
+    document.querySelector('#klok').innerHTML = tijd;
+}
+setInterval(eenklok, 1000);
