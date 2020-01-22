3342e8950be88f03ab326e1f358e6785073f9e18
Nikos Spiropoulos <spiropoulos94@gmail.com>
2020-01-22 02:19:14 +0200

    nikos homework week 1

---
+++ Week1/homework/showCurrentTime/showCurrentTime.js
@@ -0,0 +1,16 @@
+function showCurrentTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('txt').innerHTML =
+    h + ":" + m + ":" + s;
+    var t = setInterval(showCurrentTime, 500);
+  }
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+  }
+
