04a3d2ef3c34de6123ee797beed44b71adf914fb
Nikos Spiropoulos <spiropoulos94@gmail.com>
2020-01-22 12:41:33 +0200

    create oddOnesOut

---
+++ Week2/js-exercises/oddOnesOut.js
@@ -0,0 +1,24 @@
+
+const myNumbers = [1, 2, 3, 4];
+let differentNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+
+function doubleNumbers(number) {
+    return number * 2
+}
+
+function evenNumbers(number) {
+    return number % 2 == 0
+}
+
+//the following function requires an array as an input.
+
+function doubleEvenNumbers(numbers) {
+    let filteredNumbers = numbers.filter(evenNumbers)
+    let doubledNumbers = filteredNumbers.map(doubleNumbers)
+
+    return doubledNumbers
+}
+
+
+
+console.log(doubleEvenNumbers(differentNumbers))
