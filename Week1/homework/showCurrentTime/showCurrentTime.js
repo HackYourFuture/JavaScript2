3c1edae59b4b078e6af1ee38129c5bcd876c462e
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2020-01-22 18:25:19 +0200

    Update Week1/homework/showCurrentTime/showCurrentTime.js
    Co-Authored-By: Nikos Kleidis <nikoskleidis@gmail.com>

--- Week1/homework/showCurrentTime/showCurrentTime.js
+++ Week1/homework/showCurrentTime/showCurrentTime.js
@@ -10,7 +10,8 @@ function showCurrentTime() {
      setInterval(showCurrentTime, 500);
   }
   function checkTime(i) {
-    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    if (i < 10) {
+       i = "0" + i
+    }  // add zero in front of numbers < 10
     return i;
   }
-
