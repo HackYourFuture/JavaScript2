701e0a17790afbcdc883fd2558d7a921e80b577b
Nikos Spiropoulos <spiropoulos94@gmail.com>
2020-01-23 14:10:44 +0200

    Update myFavoriteHobbies.js

--- Week2/js-exercises/myFavoriteHobbies/myFavoriteHobbies.js
+++ Week2/js-exercises/myFavoriteHobbies/myFavoriteHobbies.js
@@ -1,3 +1,5 @@
+
+
 const myHobbies = [
     "Meditation",
     "Reading",
@@ -6,16 +8,35 @@ const myHobbies = [
     "Going to the gym"
 ];
 
-function convertItemToLi(array) {
-    let unorderedList = document.createElement("ul");
-    for (let item of array) {
-        let listItem = document.createElement("li");
-        let listArr = [];
+
+//made WITHOUT using array.map()
+
+/*function convertItemToLi(array) {
+  let unorderedList = document.createElement("ul");
+  for (let item of array) {
+    let listItem = document.createElement("li");
+    let listArr = [];
+    listItem.innerHTML = item;
+    unorderedList.appendChild(listItem);
+  }
+  console.log(unorderedList);
+  document.body.appendChild(unorderedList);
+}*/
+
+//console.log(convertItemToLi(myHobbies));
+
+
+//made WITH array.map()
+
+function convertItemsToLi(array) {
+    let unorderedList = document.createElement("UL");
+
+    array.map(item => {
+        let listItem = document.createElement("LI");
         listItem.innerHTML = item;
         unorderedList.appendChild(listItem);
-    }
-    console.log(unorderedList);
+    });
     document.body.appendChild(unorderedList);
 }
 
-console.log(convertItemToLi(myHobbies));
+convertItemsToLi(myHobbies);
