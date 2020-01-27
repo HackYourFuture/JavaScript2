c5dec5212e7508e3de45b28979203f6524973206
Nikos Spiropoulos <spiropoulos94@gmail.com>
2020-01-27 20:13:28 +0200

    update++

--- Week2/project Pomodoro Clock/myClockApp.js
+++ Week2/project Pomodoro Clock/myClockApp.js
@@ -1,16 +1,23 @@
 
-let displayTimer = document.getElementById('displayTimer')
 
-displayTimer.innerHTML = "fahkbfksabk"
+
+
+
+let displayTimer = document.getElementById('displayTimer')
+//console.log(displayTimer)
 
 function pomodoro(minutes) {
     let interval;
     var seconds = minutes * 60
     interval = setInterval(() => {
         seconds--;
+        displayTimer.innerHTML = seconds
+
         console.log(seconds)
         if (seconds == 0) {
             console.log("yimes up")
+            displayTimer.innerHTML = "Times Up"
+
             clearInterval(interval)
         }
     }, 1000)
@@ -19,4 +26,8 @@ function pomodoro(minutes) {
 
 }
 
-pomodoro(0.1);
+//pomodoro(0.1);
+
+
+
+document.getElementById("startBtn").addEventListener("click", pomodoro(0.2))
