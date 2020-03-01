68d25e869e7536643c5b80f5340b4a6a4a6df259
gajduk-mansystems <andrej.gajduk@mansystems.de>
2020-03-01 13:28:59 +0100

    week1-homework-structure

---
+++ Week1/js-exercises/ex3-hijackLogo.js
@@ -0,0 +1,19 @@
+/**
+ 
+ ** Exercise 3: The logo hijack **
+ 
+ No homepage is safe from the logo bandit!Everytime he sees a Google Logo he replaces it with a logo from HackYourfuture instead: https: //www.hackyourfuture.dk/static/logo-dark.svg.
+
+ In this exercise you 're expected to write a JavaScript function that can be executed in the console of the [Google website](https://www.google.com).
+
+ 
+ 1. Find out how to select the element that contains the Google logo, and store it in a variable.
+ 2. Modify the source and sourceset of the logo so that it 's replaced by the HackYourFuture logo instead.
+
+ */
+
+function hijackGoogleLogo() {
+  // your code goes in here
+}
+
+hijackGoogleLogo();
