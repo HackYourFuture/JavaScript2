1267b3157fa37ee24739f09584f335379bc5eee9
Wasim Othman <wasim1991othman@gmail.com>
2020-08-05 23:33:40 +0200

    added ex-aboutme.html & ex2-aboutme.js

--- Week1/js-exercises/ex2-aboutMe.js
+++ Week1/js-exercises/ex2-aboutMe.js
@@ -9,3 +9,30 @@
  5. See HTML
  6. Create a new img element and set its src attribute to a picture of you.Append that element to the page.
  */
+
+ document.body.style.fontFamily= "Arial, sans-serif";
+
+let nickname = document.querySelector('#nickname');
+nickname.innerText = 'wasim';
+
+let favFood = document.querySelector('#fav-food');
+favFood.innerText = 'pizza';
+
+let hometown = document.querySelector('#hometown');
+hometown.innerText = 'italy';
+
+
+
+ let li = document.querySelectorAll('li');
+ for ( let i = 0; i < li.length; i++){
+    li[i].setAttribute ('class', 'list-item');
+    // li.className = 'list-item';
+    // li[i].style.color= 'red';
+};
+
+
+
+let img = document.createElement('img');
+img.src = 'https://www.refinery29.com/images/9381037.jpg?format=webp&width=340&height=408&quality=85';
+let body = document.querySelector('body');
+body.appendChild(img);
