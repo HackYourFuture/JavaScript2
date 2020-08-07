2b413f81f7dad6a12d7fc5e102ad79f7a9415004
Sadeq Al-Dallal <66870615+sadeq93@users.noreply.github.com>
2020-08-07 10:26:01 +0200

    finished the exercises

--- Week1/js-exercises/ex2-aboutMe.js
+++ Week1/js-exercises/ex2-aboutMe.js
@@ -1,31 +1,14 @@
-/**
- 
- ** Exercise 2: About me **
-
- 1. See HTML
- 2. Change the body tag 's style so it has a font-family of "Arial, sans-serif".
- 3. Replace each of the spans(nickname, fav - food, hometown) with your own information.
- 4. Iterate through each li and change the class to "list-item".
- 5. See HTML
- 6. Create a new img element and set its src attribute to a picture of you.Append that element to the page.
- */
-
-
-//   2.
 const body = document.querySelector('body');
 body.style.fontFamily = "Arial, sans-serif";
 const ul = body.children[1].children;
+let myInfo = ['Sadeq', 'Pizza','Nieuwkoop']
 
-//   3.
-document.getElementById('nickname').innerText = 'Sadeq';
-document.getElementById('fav-food').innerText = 'Pizza';
-document.getElementById('hometown').innerText = 'Nieuwkoop';
-
-//   4.
 for (let i = 0; i < ul.length; i++) {
-  ul[i].setAttribute('class', "list-item")
+  ul[i].setAttribute('class', "list-item");
+  ul[i].children[0].innerText = myInfo[i];
 }
-//   6.
+
 const img = document.createElement('img');
+img.style.width = '300px';
 img.src = "https://babeltechreviews.com/wp-content/uploads/2018/07/rendition1.img_.jpg";
 body.appendChild(img);
--- Week1/js-exercises/ex3-hijackLogo.js
+++ Week1/js-exercises/ex3-hijackLogo.js
@@ -1,17 +1,3 @@
-/**
- 
- ** Exercise 3: The logo hijack **
- 
- No homepage is safe from the logo bandit!Everytime he sees a Google Logo he replaces it with a logo from HackYourfuture instead: https: //www.hackyourfuture.dk/static/logo-dark.svg.
-
- In this exercise you 're expected to write a JavaScript function that can be executed in the console of the [Google website](https://www.google.com).
-
- 
- 1. Find out how to select the element that contains the Google logo, and store it in a variable.
- 2. Modify the source and sourceset of the logo so that it 's replaced by the HackYourFuture logo instead.
-
- */
-
 function hijackGoogleLogo() {
   let logo = document.querySelectorAll("img")[1];
   logo.src = "https://www.hackyourfuture.dk/static/logo-dark.svg";
