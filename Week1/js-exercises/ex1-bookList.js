868cddc1d401123f64590c468d9f884168338752
Vaida <93vaida@gmail.com>
2020-09-25 22:04:28 +0200

    function that loops through array of books

--- Week1/js-exercises/ex1-bookList.js
+++ Week1/js-exercises/ex1-bookList.js
@@ -18,25 +18,67 @@
 
 function createBookList(books) {
   // your code goes in here, return the ul element
+  const newUl = document.createElement('ul');
+  document.querySelector('#bookList').appendChild(newUl);
+  newUl.style.cssText = 'display: flex; flex-wrap: wrap;';
+
+  books.forEach((element) => {
+    //iterate through array
+    const bookTitle = element.title;
+    const bookAuthor = element.author;
+    const read = element.alreadyRead;
+
+    //create elements and append them
+    const newList = document.createElement('li');
+    newUl.appendChild(newList);
+    const newPar = document.createElement('p');
+    newList.appendChild(newPar);
+    const newImg = document.createElement('img');
+    newList.appendChild(newImg);
+
+    //styles
+    newImg.style.cssText = 'height: 350px; max-width: 300px;';
+    newList.style.cssText = 'margin: 1rem; list-style: none; padding: 1rem 3rem; flex: 1;';
+    newPar.style.cssText = 'font-family: monospace; text-align: center; font-size: 2rem';
+
+    //adding text to paragraph
+    newPar.innerText = `${bookTitle} by ${bookAuthor}`;
+
+    //book covers
+    if (bookAuthor === 'Don Norman') {
+      newImg.src = './book covers/norman.jpg';
+    } else if (bookAuthor === 'Brian Christian') {
+      newImg.src = './book covers/christian.jpg';
+    } else {
+      newImg.src = './book covers/hunt.jpg';
+    }
+    //background color change
+    if (read === true) {
+      newList.style.backgroundColor = 'green';
+    } else {
+      newList.style.backgroundColor = 'red';
+    }
+  });
 }
 
-const books = [{
+const books = [
+  {
     title: 'The Design of Everyday Things',
     author: 'Don Norman',
-    alreadyRead: false
+    alreadyRead: false,
   },
   {
     title: 'The Most Human Human',
     author: 'Brian Christian',
-    alreadyRead: true
+    alreadyRead: true,
   },
   {
     title: 'The Pragmatic Programmer',
     author: 'Andrew Hunt',
-    alreadyRead: true
-  }
+    alreadyRead: true,
+  },
 ];
 
 let ulElement = createBookList(books);
 
-document.querySelector("#bookList").appendChild(ulElement);
+document.querySelector('#bookList').appendChild(ulElement);
