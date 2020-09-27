f071a9ce3b5f1ec147b093e0104c3617f7ae5b48
Vaida <93vaida@gmail.com>
2020-09-27 17:35:13 +0200

    about me js file

--- Week1/js-exercises/ex2-aboutMe.js
+++ Week1/js-exercises/ex2-aboutMe.js
@@ -9,3 +9,28 @@
  5. See HTML
  6. Create a new img element and set its src attribute to a picture of you.Append that element to the page.
  */
+
+'use strict';
+const title = document.querySelector('h1');
+document.body.style.fontFamily = 'Arial, sans-serif';
+
+const name = document.getElementById('nickname');
+const favouriteFood = document.getElementById('fav-food');
+const hometown = document.getElementById('hometown');
+
+name.innerText = 'Vaida';
+favouriteFood.innerText = 'Cheese';
+hometown.innerText = 'Svencionys';
+
+const list = document.getElementsByTagName('li');
+for (let i = 0; i < list.length; i++) {
+  list[i].classList.add('list-item');
+}
+const div = document.createElement('div');
+const picture = document.createElement('img');
+picture.src = './picture/vaida.jpg';
+picture.style.height = '250px';
+title.appendChild(div);
+div.appendChild(picture);
+
+//created a div so img would be under the h1, not next to it
