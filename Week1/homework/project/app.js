bef6dcf50721fac17d84f04b37db6e846d1fe950
Nizami Mursudlu <nizamimursudlu@gmail.com>
2020-11-29 02:03:39 +0100

    project Random Quote Generator is finished

---
+++ Week1/homework/project/app.js
@@ -0,0 +1,34 @@
+const objectWithQuotes = [{
+  Quote: "Life is what happens when you're busy making other plans.",
+  Author: "- John Lennon"
+},
+{
+  Quote: "The way to get started is to quit talking and begin doing.",
+  Author: "- Walt Disney"
+},
+{
+  Quote: "If life were predictable it would cease to be life, and be without flavor.",
+  Author: "- Eleanor Roosevelt"
+},
+{
+  Quote: "It is during our darkest moments that we must focus to see the light.",
+  Author: "- Aristotle"
+},
+{
+  Quote: "Tell me and I forget. Teach me and I remember. Involve me and I learn.",
+  Author: "- Benjamin Franklin"
+},
+{
+  Quote: "Whoever is happy will make others happy too.",
+  Author: "- Anne Frank"
+}]
+let newQuoteBtn = document.getElementById("new-quote-btn")
+let chosenQuote = document.querySelector("#new-quote")
+let chosenAuthor = document.querySelector("#author > input")
+function quotePicker() {
+  let quoteNum = Math.floor((Math.random()) * 6)
+  let newQuote = objectWithQuotes[quoteNum]
+  chosenQuote.value = newQuote.Quote
+  chosenAuthor.value = newQuote.Author
+}
+newQuoteBtn = addEventListener("click", quotePicker)
