59b1ecb5fb4377bf33f21761549cbf4ccfa5857f
atabek-bakh <coderonline@icloud.com>
2020-12-06 23:35:27 +0100

    the project is set

--- Week2/project/index.js
+++ Week2/project/index.js
@@ -10,3 +10,62 @@
  */
 
 'use strict';
+
+const startButton = document.getElementById("start");
+const pauseButton = document.getElementById("pause");
+const minutesLeft = document.getElementById("mins-left");
+const secondsLeft = document.getElementById("secs-left");
+const minutesMore = document.getElementById("more");
+const minutesLess = document.getElementById("less");
+const setMinutes = document.getElementById("set-minutes");
+
+// a callback function for the setInterval
+const countDown = function () {
+  if (secondsLeft.innerText <= 0) {
+    secondsLeft.innerText = 60;
+    minutesLeft.innerText--;
+  };
+  secondsLeft.innerText--;
+  // in case both minutes and seconds are zero, then stop countdown
+  if (secondsLeft.innerText <= 0 && minutesLeft.innerText <= 0) {
+    clearInterval(window.interval);
+    alert("hehe");
+  };
+};
+
+const startTimer = function () {
+  interval = setInterval(countDown, 1000);
+  startButton.disabled = true;
+  pauseButton.disabled = false;
+  minutesMore.disabled = true;
+  minutesLess.disabled = true;
+};
+
+const pauseTimer = function () {
+  clearInterval(window.interval);
+  pauseButton.disabled = true;
+  startButton.disabled = false;
+}
+
+const resetTimer = function () {
+  clearInterval(window.interval);
+  secondsLeft.innerText = 00;
+  minutesLeft.innerText = setMinutes.innerText;
+  pauseButton.disabled = true;
+  startButton.disabled = false;
+  minutesMore.disabled = false;
+  minutesLess.disabled = false;
+}
+
+const more = function () {
+  setMinutes.innerText++;
+  minutesLeft.innerText = setMinutes.innerText;
+}
+
+const less = function () {
+  setMinutes.innerText--;
+  if (setMinutes.innerText <= 1) {
+    setMinutes.innerText = 1;
+  }
+  minutesLeft.innerText = setMinutes.innerText;
+}
